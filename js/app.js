709f4e583671fde33f0626e12686bf2e629c080b
Parkjoopika <parkjoohan0603@gmail.com>
2020-06-18 02:05:15 +0900

    add edit mod

--- js/app.js
+++ js/app.js
@@ -2,6 +2,27 @@ const todo_ul = document.querySelector(".todo-list")
 const todo_input = document.querySelector(".new-todo") 
 const todo_list = []
 
+function editingTodo(event) {
+    event.preventDefault()
+    const li = event.target.parentElement
+    const label = li.querySelector(".label")
+    if (event.key === "Escape") {
+        li.classList.remove("editing")
+        event.target.value = label.innerText
+    } else if (event.key === "Enter") {
+        todo = findTodo(li)
+        label.innerText = event.target.value
+        li.classList.remove("editing")
+        todo.todo = label.innerText
+    }
+}
+
+function editTodo(event) {
+    event.preventDefault()
+    const li = event.target.parentElement.parentElement
+    li.classList.add("editing")
+}
+
 function findTodo(li) {
     const label = li.querySelector(".label")
     for (const todo of todo_list) {
@@ -23,7 +44,6 @@ function todoComplete(event) {
         li.classList.remove("completed")
         todo.complete = false
     }
-    console.log(todo_list)
 }
 
 function save(current_todo, boolean) {
@@ -46,13 +66,18 @@ function inputTodo(event) {
 function drawTodo(current_todo, complete) {
     const li = document.createElement("li")
     const div = document.createElement("div")
-    const input = document.createElement("input") 
+    const input = document.createElement("input")
+    const edit_input = document.createElement("input") 
     const label = document.createElement("label")
     const btn = document.createElement("button")
     div.classList.add("view")
     input.classList.add("toggle")
     input.type = "checkbox"
     input.addEventListener("change", todoComplete)
+    edit_input.classList.add("edit")
+    edit_input.value = current_todo
+    edit_input.addEventListener("keyup", editingTodo)
+    label.addEventListener("dblclick", editTodo)
     label.classList.add("label")
     label.innerText = current_todo
     btn.classList.add("destroy")
@@ -63,6 +88,7 @@ function drawTodo(current_todo, complete) {
     div.appendChild(label)
     div.appendChild(btn)
     li.appendChild(div)
+    li.appendChild(edit_input)
     todo_ul.appendChild(li)
     save(current_todo, complete)
 }
