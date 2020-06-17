9b3053d938133e11f0373de966459bbb5d08affe
Jeesoo Min <kuongee@gmail.com>
2020-06-18 02:02:46 +0900

    Add item edit function

--- js/app.js
+++ js/app.js
@@ -3,22 +3,52 @@ import TodoInput from './components/TodoInput.js';
 
 class App {
   constructor() {
-    this.todoItems = [];
+    this.todoItems = [
+      {
+        content: 'ABCD',
+        isCompleted: false,
+        editing: false
+      },
+      {
+        content: 'EFGH',
+        isCompleted: true,
+        editing: false
+      },
+      {
+        content: 'qwerqwer',
+        isCompleted: false,
+        editing: false
+      }
+    ];
+
+    const getNewItem = (content, isCompleted = false, editing = false) => {
+      return {
+        content: content,
+        isCompleted: isCompleted,
+        editing: editing
+      };
+    };
 
     this.todoList = new TodoList({
       $element: document.getElementById('todo-list'),
       items: this.todoItems,
       onClickToggle: id => {
         const newTodoItems = [...this.todoItems];
-        newTodoItems[id] = {
-          content: this.todoItems[id].content,
-          isCompleted: !this.todoItems[id].isCompleted
-        };
+        newTodoItems[id] = getNewItem(this.todoItems[id].content, !this.todoItems[id].isCompleted);
         this.setState(newTodoItems);
       },
       onClickDestroy: id => {
         const newTodoItems = [...this.todoItems];
         newTodoItems.splice(id, 1);
+        this.setState(newTodoItems);
+      },
+      onToggleEdit: (id, newContent, editing) => {
+        const newTodoItems = [...this.todoItems];
+        newTodoItems[id] = getNewItem(
+          newContent ? newContent.trim() : this.todoItems[id].content,
+          this.todoItems[id].isCompleted,
+          editing
+        );
         this.setState(newTodoItems);
       }
     });
@@ -26,7 +56,7 @@ class App {
     this.todoInput = new TodoInput({
       $element: document.getElementById('new-todo-title'),
       onEnter: newContent => {
-        this.setState([...this.todoItems, { content: newContent, isCompleted: false }]);
+        this.setState([...this.todoItems, getNewItem(newContent)]);
       }
     });
   }
--- js/components/TodoList.js
+++ js/components/TodoList.js
@@ -1,38 +1,104 @@
-const Item = ({ id, content, isCompleted }) => {
-  return `<li ${isCompleted ? 'class=completed' : ''}>
-  <input class="toggle" type="checkbox" value="${id}" ${isCompleted ? 'checked' : ''}>
-  <label>${content}</label>
-  <button class="destroy" name="${id}"></button>
-  </li>`;
+const ContentWrapper = ({ id, content, isCompleted }) => {
+  const checked = isCompleted ? 'checked' : '';
+  return `<div class="view">
+    <input class="toggle" type="checkbox" name="${id}" ${checked}/>
+    <label for="${id}">${content}</label>
+    <button class="destroy" name="${id}"></button>
+  </div>`;
+};
+
+const EditInput = ({ id, content }) => {
+  return `<input class="edit" name="${id}" value="${content}" autofocus/>`;
+};
+
+const Item = props => {
+  const { isCompleted, editing } = props;
+  const completed = isCompleted ? 'completed' : '';
+  const edit = editing ? 'editing' : '';
+  return `<li class="${completed} ${edit}">${ContentWrapper(props)}${EditInput(props)}</li>`;
 };
 
 export default class TodoList {
   constructor(props) {
-    const { $element, items, onClickToggle, onClickDestroy } = props;
+    const { $element, items, onClickToggle, onClickDestroy, onToggleEdit } = props;
+
     this.$element = $element;
-    this.items = items;
+    this.todoItems = items;
+    this.isEditing = -1; // 현재 편집 중인 아이템의 id 저장
 
     this.render();
+
+    const handleFinishEdit = save => {
+      onToggleEdit(
+        this.isEditing,
+        save ? document.getElementsByClassName('edit')[this.isEditing].value : '',
+        false
+      );
+      this.isEditing = -1;
+    };
+
+    // 마우스 클릭 이벤트
     this.$element.addEventListener('click', e => {
-      if (e.target.nodeName == 'INPUT') {
-        onClickToggle(e.target.value);
+      // 편집 중인 아이템 외의 기능 선택 시 편집 완료
+      if (this.isEditing !== -1 && e.target.className !== 'edit') {
+        handleFinishEdit(true);
+        return;
       }
 
-      if (e.target.nodeName == 'BUTTON') {
-        console.log(e.target.name);
+      // 아이템 완료/미완료 선택
+      if (e.target.nodeName === 'INPUT' && e.target.className === 'toggle') {
+        onClickToggle(e.target.name);
+        return;
+      }
+
+      // 아이템 삭제
+      if (e.target.nodeName === 'BUTTON' && e.target.className === 'destroy') {
         onClickDestroy(e.target.name);
+        return;
+      }
+    });
+
+    // 마우스 더블 클릭 이벤트
+    this.$element.addEventListener('dblclick', e => {
+      // 아이템 편집
+      if (e.target.nodeName === 'LABEL') {
+        const editId = e.target.htmlFor;
+        this.isEditing = editId;
+        onToggleEdit(editId, '', true);
+      }
+    });
+
+    // 키보드 입력 이벤트
+    this.$element.addEventListener('keydown', e => {
+      if (this.isEditing !== -1) {
+        if (e.key === 'Escape') {
+          handleFinishEdit(false);
+          return;
+        }
+
+        if (e.key === 'Enter') {
+          handleFinishEdit(true);
+          return;
+        }
       }
     });
   }
 
   render() {
-    this.$element.innerHTML = `${this.items
-      .map((item, id) => Item({ id: id, content: item.content, isCompleted: item.isCompleted }))
+    this.$element.innerHTML = `${this.todoItems
+      .map((item, id) =>
+        Item({
+          id: id,
+          content: item.content,
+          isCompleted: item.isCompleted,
+          editing: item.editing
+        })
+      )
       .join('')}`;
   }
 
   setState(newItems) {
-    this.items = newItems;
+    this.todoItems = newItems;
     this.render();
   }
 }
