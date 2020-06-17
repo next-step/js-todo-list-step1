6663990cb865dd5f67ef10e306bd1658de4582c5
Hosek Na <hsna7024@gmail.com>
2020-06-17 15:45:49 +0900

    refactor: Add errorMessageMap

---
+++ utils/constants.js
@@ -0,0 +1,9 @@
+export const errorMessageMap = {
+  SHOULD_BE_ELEMENT: "It Should be Element.",
+  SHOULD_BE_ARRAY: "It Should be Array",
+  SHOuLD_BE_CREATED_BY_NEW: "It Should be created by new",
+  SHOULD_BE_OBJECT: "It Should be object type",
+  SHOULD_BE_STRING: "It Should be string",
+  SHOULD_NOT_BE_ZERO_LENGTH: "It Should not be 0 length",
+  SHOULD_BE_BOOLEAN: "It Should be Boolean",
+};
--- utils/validator.js
+++ utils/validator.js
@@ -1,43 +1,45 @@
+import { errorMessageMap } from "./constants.js";
+
 export const validator = {
   isNewInstance(it, targetInstance) {
     if (!(it instanceof targetInstance)) {
-      throw new Error("It Should be created by new");
+      throw new Error(errorMessageMap.SHOuLD_BE_CREATED_BY_NEW);
     }
     return true;
   },
   isObject(it) {
     if (typeof it !== "object") {
-      throw new Error("It Should be object type");
+      throw new Error(errorMessageMap.SHOULD_BE_OBJECT);
     }
     return true;
   },
   isElement(it) {
     if (!(it instanceof Element)) {
-      throw new Error("It Should be Element");
+      throw new Error(errorMessageMap.SHOULD_BE_ELEMENT);
     }
     return true;
   },
   isArray(it) {
     if (!(it instanceof Array)) {
-      throw new Error("It Should be Array");
+      throw new Error(errorMessageMap.SHOULD_BE_ARRAY);
     }
     return true;
   },
   isBoolean(it) {
     if (typeof it !== "boolean") {
-      throw new Error("It Should be Boolean");
+      throw new Error(errorMessageMap.SHOULD_BE_BOOLEAN);
     }
     return true;
   },
   isString(it) {
     if (typeof it !== "string") {
-      throw new Error("It Should be string");
+      throw new Error(errorMessageMap.SHOULD_BE_STRING);
     }
     return true;
   },
   isNotZeroLengthString(it) {
     if (it.length === 0) {
-      throw new Error("It Should not be 0 length");
+      throw new Error(errorMessageMap.SHOULD_NOT_BE_ZERO_LENGTH);
     }
     return true;
   },
