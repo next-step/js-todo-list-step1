21ff9fdb391a6c93bf0a04905e088788e67cb000
soyoung <soyoungp02@gmail.com>
2020-06-20 18:03:06 +0900

    ♻️separate component function

--- js/app.js
+++ js/app.js
@@ -1,13 +1,35 @@
 const KEYCODE_ENTER = 13;
 
-const $newTodo = document.getElementById('new-todo-title');
-const $todoList = document.getElementById('todo-list');
+function TodoList(element) {
+  this.$ul = element;
+  this.todoList = [];
 
-$newTodo.addEventListener('keypress', e => {
-  if (e.keyCode === KEYCODE_ENTER) {
-    const size = $todoList.childElementCount;
-    const list = `${$todoList.innerHTML}<li><input type="checkbox" id="${size}" class="toggle"><label for="${size}">${e.target.value}</label></li>`;
-    $todoList.innerHTML = list;
-    $newTodo.value = '';
+  this.addListItem = value => {
+    this.todoList = [{ text: value }, ...this.todoList];
+    this.render();
+  };
+
+  this.render = () => {
+    this.$ul.innerHTML = this.todoList.map((item, index) => `<li><input type="checkbox" id="${index}" class="toggle"><label for="${index}">${item.text}</label></li>`).join('');
+  };
+}
+
+function TodoInput(element, { addTodo }) {
+  this.$input = element;
+  this.$input.addEventListener('keypress', addTodo);
+  this.setText = text => {
+    this.$input.value = text;
   }
+}
+
+const addTodo = e => {
+  if (e.keyCode === KEYCODE_ENTER) {
+    todoList.addListItem(e.target.value);
+    todoInput.setText('');
+  }
+};
+
+const todoList = new TodoList(document.getElementById('todo-list'));
+const todoInput = new TodoInput(document.getElementById('new-todo-title'), {
+  addTodo
 });
