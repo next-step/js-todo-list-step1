056a7a262230cb1e04bb158ca7c33faf762a23af
Hosek Na <hsna7024@gmail.com>
2020-07-01 13:18:54 +0900

    refactor: Modify error message name

--- utils/validator.js
+++ utils/validator.js
@@ -3,43 +3,43 @@ import { errorMessageMap } from "./constants.js";
 export const validator = {
   isNewInstance(it, targetInstance) {
     if (!(it instanceof targetInstance)) {
-      throw new Error(errorMessageMap.SHOuLD_BE_CREATED_BY_NEW);
+      throw new Error(errorMessageMap.NOT_CREATED_BY_NEW);
     }
     return true;
   },
   isObject(it) {
     if (typeof it !== "object") {
-      throw new Error(errorMessageMap.SHOULD_BE_OBJECT);
+      throw new Error(errorMessageMap.NOT_OBJECT);
     }
     return true;
   },
   isElement(it) {
     if (!(it instanceof Element)) {
-      throw new Error(errorMessageMap.SHOULD_BE_ELEMENT);
+      throw new Error(errorMessageMap.NOT_ELEMENT);
     }
     return true;
   },
   isArray(it) {
     if (!(it instanceof Array)) {
-      throw new Error(errorMessageMap.SHOULD_BE_ARRAY);
+      throw new Error(errorMessageMap.NOT_ARRAY);
     }
     return true;
   },
   isBoolean(it) {
     if (typeof it !== "boolean") {
-      throw new Error(errorMessageMap.SHOULD_BE_BOOLEAN);
+      throw new Error(errorMessageMap.NOT_BOOLEAN);
     }
     return true;
   },
   isString(it) {
     if (typeof it !== "string") {
-      throw new Error(errorMessageMap.SHOULD_BE_STRING);
+      throw new Error(errorMessageMap.NOT_STRING);
     }
     return true;
   },
   isNotZeroLengthString(it) {
     if (it.length === 0) {
-      throw new Error(errorMessageMap.SHOULD_NOT_BE_ZERO_LENGTH);
+      throw new Error(errorMessageMap.NOT_ZERO_LENGTH);
     }
     return true;
   },
