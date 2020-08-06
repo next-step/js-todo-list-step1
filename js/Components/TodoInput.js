db8d3e03f1f7f27ea2301a4a56023dd09004ed69
2SOOY <1401660@naver.com>
2020-08-06 21:47:07 +0900

    Add - TodoInput Component
    - todo를 입력하고, 입력한 todo에 대해서 TodoList에 추가하는 역할을 함.
    - 단일 데이터 흐름을 위해, todo를 추가하는 함수를 넘겨받음.

--- js/Components/TodoInput.js
+++ js/Components/TodoInput.js
@@ -1,6 +1,29 @@
-function TodoInput({ $target }) {
+import { MESSAGE } from '../utils/constant.js';
+
+function TodoInput({ $target, onAddTodo }) {
   this.init = () => {
     this.$target = $target;
+
+    this.bindEvents();
+  };
+
+  this.bindEvents = () => {
+    this.$target.addEventListener('keydown', this.onKeydown);
+  };
+
+  this.onKeydown = (e) => {
+    if (e.key !== 'Enter') return;
+
+    const keyword = e.target.value.trim();
+
+    if (keyword.length === 0) {
+      alert(MESSAGE.NO_INPUT_KEYWORD);
+      e.target.value = '';
+      return;
+    }
+
+    onAddTodo(keyword);
+    e.target.value = '';
   };
 
   this.init();
