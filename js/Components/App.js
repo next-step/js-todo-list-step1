eaf44782f8d161ce5ebe91f5c11306641b686400
2SOOY <1401660@naver.com>
2020-08-06 21:55:32 +0900

    Add - TodoInput component in App component
    - TodoInput 컴포넌트를 생성을 위한 선택자 상수화 SELECTOR.TODO_INPUT
    - TodoInput 컴포넌트에게 DOM Node와 검색어 처리 로직 함수 전달
    - setState 로직 업데이트 : todo 추가에 따른 state값 변경하기

--- js/Components/App.js
+++ js/Components/App.js
@@ -1,17 +1,41 @@
-import TodoCount from './TodoInput.js';
+import TodoInput from './TodoInput.js';
 import TodoList from './TodoList.js';
 import TodoCount from './TodoCount.js';
+
+import { SELECTOR } from '../utils/constant.js';
 
 function App($target) {
   this.init = () => {
     this.$target = $target;
     this.state = {
-      todoCounts: {},
       todos: [],
     };
+
+    this.todoInput = new TodoInput({
+      $target: document.querySelector(SELECTOR.TODO_INPUT),
+      onAddTodo: this.onAddTodo,
+    });
   };
 
-  this.setState = (nextState) => {};
+  this.onAddTodo = (keyword) => {
+    const todoItem = {
+      id: Date.now(),
+      title: keyword,
+      isCompleted: false,
+    };
+
+    const nextState = {
+      todos: [...this.state.todos, todoItem],
+    };
+
+    this.setState(nextState);
+  };
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+
+    // console.log(nextState);
+  };
 
   this.init();
 }
--- js/utils/constant.js
+++ js/utils/constant.js
@@ -1,3 +1,8 @@
 export const SELECTOR = {
   TODO_APP: '#todo-app',
+  TODO_INPUT: '#new-todo-title',
+};
+
+export const MESSAGE = {
+  NO_INPUT_KEYWORD: '🔥할일을 입력해주세요',
 };
