e2d64e96793bb26db5955095ecbd775873433dfc
2SOOY <1401660@naver.com>
2020-08-07 23:24:15 +0900

    Add - todoList dummy data
    - todoList의 render 함수를 구현하기 위해서 dummy data 추가함
    - dummydata => id, title, isCompleted 포함

---
+++ js/utils/data.js
@@ -0,0 +1,17 @@
+export const dummyTodos = [
+  {
+    id: 1,
+    title: '투두',
+    isCompleted: false,
+  },
+  {
+    id: 2,
+    title: '퉁둥',
+    isCompleted: true,
+  },
+  {
+    id: 3,
+    title: '툴둘',
+    isCompleted: false,
+  },
+];
