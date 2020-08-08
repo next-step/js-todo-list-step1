93ce817ddfc6f70c63885e6ebf2f8ebef26fdc5d
2SOOY <1401660@naver.com>
2020-08-09 04:20:23 +0900

    Add - TodoCount component
    - 선택된 탭에 따라 todo 목록의 개수를 보여주는 컴포넌트 설계
    - render 메소드를 최대한 간략하게 하기 위해 getSelectedTabCount라는 별도의 메소드로 구현함

--- js/Components/TodoCount.js
+++ js/Components/TodoCount.js
@@ -1,13 +1,49 @@
-function TodoCount({ $target }) {
+import { TAB, MESSAGE } from '../utils/constant.js';
+
+function TodoCount({ $target, todoCountState }) {
   this.init = () => {
     this.$target = $target;
+    this.state = todoCountState;
 
     this.render();
   };
 
-  this.setState = (nextState) => {};
+  this.getSelectedTabCount = (selectedTab) => {
+    const { todos } = this.state;
+    let renderCount = 0;
 
-  this.render = () => {};
+    switch (selectedTab) {
+      case TAB.ALL:
+        renderCount = todos.length;
+        break;
+
+      case TAB.ACTIVE:
+        renderCount = todos.filter(({ isCompleted }) => !isCompleted).length;
+        break;
+
+      case TAB.COMPLETED:
+        renderCount = todos.filter(({ isCompleted }) => isCompleted).length;
+        break;
+
+      default:
+        console.error(`TodoCount Render Error : ${MESSAGE.UNDEFINED_TAB}`);
+        break;
+    }
+
+    return renderCount;
+  };
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+
+    this.render();
+  };
+
+  this.render = () => {
+    const renderCount = this.getSelectedTabCount(this.state.selectedTab);
+
+    this.$target.innerHTML = `총 <strong>${renderCount}</strong> 개`;
+  };
 
   this.init();
 }
