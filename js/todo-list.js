a4d4d2741a8e5ce198814a48ab901c60dba7aa0d
김경록 <goodboy3421@gmail.com>
2020-08-09 01:15:51 +0900

    feat: todo-list check 버튼을 누르면 Completed 상태로 변경하는 기능
    check 버튼 클릭시(체크o) li에 completed 클래스 추가 (체크x)라면 li의 클래스 제거

--- js/todo-list.js
+++ js/todo-list.js
@@ -1,11 +1,19 @@
 const ENTER_KEY = "Enter";
-let cnt = 0;
 
+const COMPLETED_CLASS_NAME = "completed";
 let todoList = {
     init: function () {
         const _this = this;
         const inputTodo = document.querySelector("#new-todo-title");
         inputTodo.addEventListener("keydown", _this.create);
+
+        let todoList = document.querySelector(".todo-list");
+        todoList.addEventListener("click", (e) => {
+            if (e.target.nodeName === "INPUT") {
+                _this.check(e);
+                return;
+            }
+        });
     },
     create: function (e) {
         if (e.key !== ENTER_KEY) {
@@ -20,15 +28,23 @@ let todoList = {
         }
 
         let todoListItem = document.querySelector("#todo-list-item").innerHTML;
-
         let newTodoListItem = todoListItem.replace("{title}", inputValue)
             .replace("{title}", inputValue);
-        console.log(++cnt + "번 실행");
+
         let todoList = document.querySelector("#todo-list");
         todoList.innerHTML = newTodoListItem + todoList.innerHTML;
 
         inputTodo.value = '';
-    }
+    },
+    check: function (e) {
+        let checkedStatus = e.target.checked;
+        let todoListItem = e.target.offsetParent;
+        if (checkedStatus) {
+            todoListItem.setAttribute("class", COMPLETED_CLASS_NAME);
+            return;
+        }
+        todoListItem.removeAttribute("class");
+    },
 }
 
 todoList.init();
