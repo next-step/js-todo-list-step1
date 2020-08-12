34243504bb93dcbdfde3c030e01d9f3110a1b055
kathy95917 <kathy95917@naver.com>
2020-08-12 18:01:50 +0900

    체크박스, x버튼 기능 추가

--- index.js
+++ index.js
@@ -4,18 +4,51 @@ const toDoList = document.querySelector(".todo-list");
 
 let toDos = [];
 let toDoId = 0;
+
+function doneToDo(event) {
+  const btn = event.target;
+  const selectli = btn.parentNode;
+  const list = selectli.parentNode;
+  list.classList.add("completed");
+  btn.setAttribute("checked", true);
+}
+function deleteToDo(event) {
+  const btn = event.target;
+  const selectli = btn.parentNode;
+  const list = selectli.parentNode;
+  list.removeChild(selectli);
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(list.id);
+  });
+  toDos = cleanToDos;
+}
+
 function AddToDo(inputVal) {
   const title = inputVal;
-  toDoList.innerHTML = `
-    <li>
+  const newId = toDos.length + 1;
+  const li = document.createElement("li");
+  li.classList.add("addToDoList");
+  li.innerHTML = `
         <div class="view">
          <input class="toggle" type="checkbox">
             <label class="label">${title}</label>
             <button class="destroy"></button>
         </div>
         <input class="edit" value="새로운 타이틀">
-    </li>
     `;
+  toDoList.appendChild(li);
+  li.id = newId;
+  const toDoObj = {
+    text: `${title}`,
+    id: newId,
+  };
+  toDoInput.value = "";
+  toDos.push(toDoObj);
+
+  const toggleBtn = document.querySelector(".toggle");
+  toggleBtn.addEventListener("click", doneToDo);
+  const destroyBtn = document.querySelector(".destroy");
+  destroyBtn.addEventListener("click", deleteToDo);
 }
 
 function handleSubmit(event) {
