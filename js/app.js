fd7f3fc09ae20c065186ef1f899070965ca02a9d
hwijun-jeong <hwijun.jeong@navercorp.com>
2020-08-16 19:20:57 +0900

    feat: add new item by press enter

--- js/app.js
+++ js/app.js
@@ -1,12 +1,27 @@
 console.log('script loaded');
 
+const isEmpty = str => {
+    return (!str || 0 === str.length);
+}
 
 const onNewItemSubmit = e => {
     if(e.key === 'Enter') {
-        console.log('enter key pressed')
-    }
+        let inputSection = document.getElementById('new-todo-title');
 
-    console.log(e);
+        let itemTitle = inputSection.value;
+        if(isEmpty(itemTitle)) {
+            return;
+        }
+        
+        let newRow = document.importNode(document.querySelector('#item-new').content, true);
+        let targetListSection = document.querySelector('#todo-list');
+
+        newRow.getElementById('item-label').innerHTML = itemTitle;
+        inputSection.value = '';
+
+        targetListSection.appendChild(newRow);
+        return;
+    }
 }
 
 document.getElementById('new-todo-title').addEventListener('keydown', onNewItemSubmit);
