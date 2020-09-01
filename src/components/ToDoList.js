f8b789b9b0291093765955ba84e77622972f7910
junilHwang <tjsdlf4261@naver.com>
2020-09-02 01:16:51 +0900

    feat: todo list를 더블클릭했을 때 input 모드로 변경 - 이벤트 작업 중

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -13,13 +13,14 @@ export const ToDoList = class {
   constructor (target) {
     this.#target = target;
     this.#setState({
-      items: []
+      items: [],
+      editing: -1,
     })
   }
 
   #render () {
     const { items } = this.#state;
-    this.#target.innerHTML = items.map(({ state, title }) => `
+    this.#target.innerHTML = items.map(({ state, title }, key) => `
       <li ${ getStateClass(state) }>
         <div class="view">
           <input class="toggle"
@@ -28,15 +29,16 @@ export const ToDoList = class {
           <label class="label">${title}</label>
           <button class="destroy"></button>
         </div>
-        ${ state === TODO_STATE.EDITING ? `<input class="edit" value="${title}" />` : '' }
+        ${ state === TODO_STATE.EDITING ? `<input class="edit" value="${title}" data-index="${key}" />` : '' }
       </li>
     `).join('');
   }
 
   #initEventListener () {
-
     this.#addToggleEvent();
     this.#addRemoveEvent();
+    this.#addEditingEvent();
+    this.#addEditedEvent();
   }
 
   #addToggleEvent () {
@@ -57,6 +59,37 @@ export const ToDoList = class {
       items.splice(key, 1);
       this.#setState({ items: [ ...items ] });
     }))
+  }
+
+  #addEditingEvent () {
+    const labels = this.#target.querySelectorAll('.label');
+    const { items } = this.#state;
+    labels.forEach((v, key) => v.addEventListener('dblclick', ({ target }) => {
+      const todoItem = items[key];
+      todoItem.state = TODO_STATE.EDITING;
+      items[key] = { ...todoItem };
+      this.#setState({
+        items: [ ...items ],
+        editing: key
+      });
+    }))
+  }
+
+  #addEditedEvent () {
+    const editors = this.#target.querySelectorAll('.edit');
+    const { editing } = this.#state;
+
+    editors.forEach(v => {
+      const index = Number(v.dataset.index);
+      v.addEventListener('keydown', ({ target, key }) => {
+        switch (key) {
+          case 'Enter':
+            break;
+          case 'Esc':
+            break;
+        }
+      })
+    })
   }
 
   #setState (payload) {
