a611154feb90b34e1ecac6c645f1c58db5e875c4
junilHwang <tjsdlf4261@naver.com>
2020-09-02 00:49:46 +0900

    fix: todo list에 todoItem을 키보드로 입력하여 추가하기 - 인풋이 초기화 되지 않는 현상 수정

--- src/components/ToDoInput.js
+++ src/components/ToDoInput.js
@@ -17,15 +17,21 @@ export const ToDoInput = class {
     this.#target.addEventListener('input', ({ target }) => {
       this.#setState({ toDoItem: target.value });
     })
-    this.#target.addEventListener('keydown', ({ key }) => {
+    this.#target.addEventListener('keydown', ({ key, target }) => {
       if (key === 'Enter') {
         this.#props.addToDoItem(this.#state.toDoItem);
+        this.#reset();
       }
     })
   }
 
   #setState (payload) {
     this.#state = { ...this.#state, ...payload };
+  }
+
+  #reset () {
+    this.#setState({ toDoItem: '' });
+    this.#target.value = '';
   }
 
 }
