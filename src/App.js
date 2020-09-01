5dafeb2e955265507d1b815bce6ec7b15df681e7
junilHwang <tjsdlf4261@naver.com>
2020-09-02 00:41:11 +0900

    feat: todo list에 todoItem을 키보드로 입력하여 추가하기 - ToDoList Component 초기 설정 완료

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ const App = class {
   constructor() {
     const toDoInputTarget = document.querySelector('#new-todo-title');
     const toDoListTarget = document.querySelector('#todo-list');
-    this.#toDoInput = new ToDoInput(toDoInputTarget);
+    this.#toDoInput = new ToDoInput(toDoInputTarget, );
     this.#toDoList = new ToDoList(toDoListTarget);
   }
 
+  addToDoItem (item) {
+    this.#toDoList.addItem(item);
+  }
 
 
 }
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,21 +1,52 @@
+import { TODO_STATE } from "../domain";
+
 export const ToDoList = class {
 
-  #state; #target;
+  #state;
+  #target;
 
   constructor (target) {
     this.#target = target;
+    this.#setState({
+      items: []
+    })
   }
 
   #render () {
-
+    const { items } = this.#state;
+    this.#target.innerHTML = items.map(({ state, title }) => `
+      <li ${ this.#getStateClass(state) }>
+        <div class="view">
+          <input class="toggle" type="checkbox" checked/>
+          <label class="label">${title}</label>
+          <button class="destroy"></button>
+        </div>
+        ${ state === TODO_STATE.EDITING &&
+          `<input class="edit" value="${title}" />` }
+      </li>
+    `).join('');
   }
 
   #initEventListener () {
 
   }
 
-  #setState () {
+  #setState (payload) {
+    this.#state = { ...this.#state, ...payload };
+    this.#render();
+    this.#initEventListener();
+  }
 
+  #getStateClass (state) {
+    return state === TODO_STATE.COMPLETED ? 'class="completed"' :
+           state === TODO_STATE.EDITING   ? 'class="editing"'   :
+           '';
+  }
+
+  addItem (item) {
+    this.#setState({
+      items: [ ...this.#state.items, item ],
+    });
   }
 
 }
