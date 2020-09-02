c21af80d03ee02c02989d7d10cd2765ddcd1cbdb
junilHwang <tjsdlf4261@naver.com>
2020-09-02 15:01:10 +0900

    refactor: Component로 추상화 중 - ToDoInput에서 불필요한 코드 제거

--- src/components/ToDoInput.js
+++ src/components/ToDoInput.js
@@ -4,22 +4,18 @@ export const ToDoInput = class extends Component{
 
   constructor (target, props) {
     super(target, props);
-    super.setState({
-      value: ''
-    })
   }
 
   _initEventListener () {
-    this.$target.addEventListener('input', ({ target }) => {
-      super.setState({ value: target.value });
-    })
-    this.$target.addEventListener('keydown', ({ key }) => {
+    const { $target } = this;
+    $target.addEventListener('keydown', ({ key }) => {
       if (key === 'Enter') {
-        this.$props.addToDoItem(this.$state.value);
-        this.$target.value = '';
-        super.setState({ value: '' });
+        this.$props.addToDoItem($target.value);
+        $target.value = '';
       }
     })
   }
+
+
 
 }
