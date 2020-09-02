6da4fba69cb7038a76794f4e998828987f2c92eb
junilHwang <tjsdlf4261@naver.com>
2020-09-02 16:32:59 +0900

    refactor: Store 적용 - ToDoInput에 적용

--- src/components/ToDoInput.js
+++ src/components/ToDoInput.js
@@ -1,21 +1,29 @@
 import {Component} from "../_core";
 import {debounceOf} from "../utils";
+import {toDoStore} from "../store";
 
 export const ToDoInput = class extends Component{
 
-  constructor (target, props) {
-    super(target, props);
+  constructor (target) {
+    super(target, {});
   }
 
   _initEventListener () {
     const { $target } = this;
     const callback = debounceOf(() => {
-      this.$props.addToDoItem($target.value);
+      this.#addItem($target.value);
       $target.value = '';
     });
     $target.addEventListener('keydown', ({ key }) => {
       if (key === 'Enter') callback(1000 / 60);
     })
+  }
+
+  #addItem (itemTitle) {
+    toDoStore.commit('SET_ITEMS', [
+      ...toDoStore.$state.items,
+      { title: itemTitle, completed: false, editing: false }
+    ]);
   }
 
 }
