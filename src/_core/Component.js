e7ded0bd134d86866a454ec76757435bc993fea8
junilHwang <tjsdlf4261@naver.com>
2020-09-02 14:51:13 +0900

    refactor: Component로 추상화 중 - ToDoList에 적용 완료

--- src/_core/Component.js
+++ src/_core/Component.js
@@ -1,3 +1,21 @@
 export const Component = class {
+  $state; $target; $props;
 
+  constructor(target, props) {
+    this.$target = target;
+    this.$props = props;
+    this.initEventListener();
+  }
+
+  setState (payload) {
+    this.$state = { ...this.$state, ...payload };
+    this._setState(payload);
+    this.render();
+  }
+  _setState(payload) {}
+
+  render () { this._render(); }
+  _render () {}
+  initEventListener () { this._initEventListener(); }
+  _initEventListener () { }
 }
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,30 +1,25 @@
 import {ToDoItemService} from "../services";
+import {Component} from "../_core";
 
 const getToDoItemClass = (completed, editing) =>
   editing   ? 'class="editing"'   :
   completed ? 'class="completed"' :
   '';
 
-export const ToDoList = class {
-
-  #state;
-  #target;
-  #props;
+export const ToDoList = class extends Component{
 
   constructor (target, props) {
-    this.#target = target;
-    this.#props = props;
-    this.#setState({
+    super(target, props);
+    super.setState({
       items: ToDoItemService.fetchAll(),
       editingIndex: -1,
       type: 'all'
     })
-    this.#initEventListener();
   }
 
-  #render () {
-    const { items, type, editingIndex } = this.#state;
-    this.#target.innerHTML =
+  _render () {
+    const { items, type, editingIndex } = this.$state;
+    this.$target.innerHTML =
       Object.entries(items)
         .filter(entry => {
           const completed = entry[1].completed;
@@ -46,90 +41,88 @@ export const ToDoList = class {
         `).join('');
 
     if (editingIndex !== -1) {
-      this.#target.querySelector(`.edit[data-index="${editingIndex}"]`).focus();
+      this.$target.querySelector(`.edit[data-index="${editingIndex}"]`).focus();
     }
   }
 
-  #initEventListener () {
-    this.#target.addEventListener('change', ({ target }) => {
+  _initEventListener () {
+    const { $target } = this;
+    $target.addEventListener('change', ({ target }) => {
       if (target.classList.contains('toggle')) this.#toggle(target)
     })
-    this.#target.addEventListener('click', ({ target }) => {
+    $target.addEventListener('click', ({ target }) => {
       if (target.classList.contains('destroy')) this.#remove(target)
     })
-    this.#target.addEventListener('dblclick', ({ target }) => {
+    $target.addEventListener('dblclick', ({ target }) => {
       if (target.classList.contains('label')) this.#editing(target)
     })
-    this.#target.addEventListener('keydown', ({ target, key }) => {
+    $target.addEventListener('keydown', ({ target, key }) => {
       if (target.classList.contains('edit')) this.#edited(target, key)
     })
   }
 
   #toggle (target) {
-    const { items } = this.#state;
+    const { items } = this.$state;
     const index = Number(target.parentNode.dataset.index);
     const todoItem = items[index];
     todoItem.completed = target.checked;
     items[index] = { ...todoItem };
-    this.#setState({ items: [ ...items ] });
+    super.setState({ items: [ ...items ] });
   }
 
   #remove (target) {
-    const { items } = this.#state;
+    const { items } = this.$state;
     const index = Number(target.parentNode.dataset.index);
     items.splice(index, 1);
-    this.#setState({ items: [ ...items ] });
+    super.setState({ items: [ ...items ] });
   }
 
   #editing (target) {
-    const { items } = this.#state;
+    const { items } = this.$state;
     const index = Number(target.parentNode.dataset.index);
     const todoItem = items[index];
     todoItem.editing = true;
     items[index] = { ...todoItem };
-    this.#setState({ items: [ ...items ], editingIndex: index });
+    super.setState({ items: [ ...items ], editingIndex: index });
   }
 
   #edited (target, key) {
-    const { items } = this.#state;
+    if (!['Enter', 'Escape'].includes(key)) return;
+    const { items } = this.$state;
     const index = Number(target.dataset.index);
     const todoItem = items[index];
     if (key === 'Enter') {
       todoItem.title = target.value;
     }
-    if (['Enter', 'Escape'].includes(key)) {
-      todoItem.editing = false;
-      items[index] = { ...todoItem };
-      this.#setState({
-        items: [ ...items ],
-        editingIndex: -1
-      });
-    }
+    todoItem.editing = false;
+    items[index] = { ...todoItem };
+    super.setState({
+      items: [ ...items ],
+      editingIndex: -1
+    });
   }
 
   count () {
-    return this.#state.items.length;
+    return this.$state.items.length;
   }
 
-  #setState (payload) {
-    this.#state = { ...this.#state, ...payload };
+  _setState (payload) {
     if (payload.items !== undefined) {
       ToDoItemService.put(payload.items);
     }
-    this.#render();
-    requestAnimationFrame(() => this.#props.countUpdate());
+    requestAnimationFrame(() => this.$props.countUpdate());
   }
 
   addItem (itemTitle) {
-    this.#setState({
+    super.setState({
       items: [
-        ...this.#state.items,
+        ...this.$state.items,
         { title: itemTitle, completed: false, editing: false }
       ],
     });
   }
 
   selectType (type) {
-    this.#setState({ type });
+    super.setState({ type });
   }
 }
