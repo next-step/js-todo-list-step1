1573c210b2aecd9374a72e34c421407d540b2b8b
junilHwang <tjsdlf4261@naver.com>
2020-09-02 16:30:20 +0900

    feat: Store에 Getter 추가

--- src/_core/Store.js
+++ src/_core/Store.js
@@ -1,26 +1,35 @@
 export const Store = class {
 
-  $state; $mutations; $observing;
+  $state; #mutations; #observing; #getters
 
-  constructor({ state, mutations }) {
+  constructor({ state, mutations, getters }) {
     this.$state = state;
-    this.$mutations = mutations;
-    this.$observing = new Set();
+    this.#mutations = mutations;
+    this.#getters = getters;
+    this.#observing = new Set();
   }
 
   addObserver (...components) {
-    components.forEach(component => this.$observing.add(component));
+    components.forEach(component => this.#observing.add(component));
   }
 
   commit (mutationKey, payload) {
     const newState = { ...this.$state };
-    this.$mutations[mutationKey](newState, payload);
+    this.#mutations[mutationKey](newState, payload);
     this.#setState(newState);
   }
 
+  get $getters () {
+    return Object.entries(this.#getters)
+                 .reduce((getters, [key, getter]) => {
+                   getters[key] = getter(this.$state);
+                   return getters;
+                 }, {});
+  }
+
   #setState (newState) {
-    this.$state = { ...this.$state };
-    this.$observing.forEach(component => component.render());
+    this.$state = { ...newState };
+    this.#observing.forEach(component => component.render());
   }
 
 }
