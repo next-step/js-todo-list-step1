837a787ff12b8df8d22e6ac47b874a1ae480a638
catsbi <killfine1988@gmail.com>
2020-09-03 20:35:50 +0900

    addItem() 구현중

--- js/todolist.js
+++ js/todolist.js
@@ -1,15 +1,33 @@
 /* 1. todo list에 todoItem을 키보드로 입력하여 추가하기 */ 
 function addItem() {
-    //  enter키 동작 시 inputText 받아오기 
+    //  enter키 동작 시 inputText 받아오기
 
-    const $inputText = document.getElementById('new-todo-title').value;
+    const make = todoItem =>{
+        const frame = `<li>
+                        <div class="view">
+                          <input class="toggle" type="checkbox" ${todoItem.checked ? 'checked': ''}>
+                          <label class="label">${todoItem.text}</label>
+                          <button class="destroy"></button>
+                        </div>
+                        <input class="edit" value="${todoItem.text}" />
+                      </li>`;
+        let $div = document.createElement('div');
+        $div.innerHTML = frame;
+        return $div.firstElementChild;
+    }
+    let $todoInput = document.getElementById('new-todo-title');
+    const todoText = $todoInput.value;
 
     // todo-list 추가
     let $ul = document.getElementById('todo-list');
-    let $li = document.createElement("li");
-
-    let $liText = document.createTextNode($inputText);
-    $li.appendChild($liText);
-    $ul.appendChild($li);
-
+    $ul.append(make({text:todoText, checked: false}));
+    $todoInput.value = '';
 }
+
+
+
+
+window.addEventListener("DOMContentLoaded", ()=>{
+
+
+});
