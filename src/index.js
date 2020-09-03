0664651c0006777c3d89450c73bfe80b875d94f2
김종완 <oen142@naver.com>
2020-09-03 20:58:26 +0900

    newTodoList add CheckBox

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 const inputNewTodo = document.getElementById('new-todo-title');
+const ulCreated = document.getElementById('todo-list');
+
 
 const handleInput = (e) => {
-  if (e.key === 'Enter') {
-    //
+
+  if (e.key === 'Enter' && inputNewTodo.value.length !== 0) {
+    let newLi = document.createElement('li');
+    let newLiInDiv = document.createElement('div');
+    let newTodoText = document.createTextNode(inputNewTodo.value);
+
+    let newInputCheckBox = document.createElement('input');
+    newInputCheckBox.type = 'checkBox';
+    newInputCheckBox.classList.add('tttt');
+
+    newLiInDiv.appendChild(newTodoText);
+    newLi.appendChild(newInputCheckBox);
+    newLi.appendChild(newLiInDiv);
+    ulCreated.appendChild(newLi);
+    inputNewTodo.value = '';
   }
 };
 
