7713d8cd00420cb9473774591d985ef2d5f71721
jjiyeon <ejy1105@gmail.com>
2020-09-05 16:46:12 +0900

    chore : class 구조 커밋

---
+++ js/class/ToDo.js
@@ -0,0 +1,33 @@
+class ToDo{
+    constructor(_dom){
+        this.init(_dom);
+        console.log(_dom);
+    }
+    init(_dom){
+        this.addTodoItem(_dom);
+    }
+    addTodoItem(){
+        
+    }
+    event(){
+        
+    }
+    setComplete(){
+
+    }
+    deleteTodoItem(){
+
+    }
+    updateTodoItem(){
+
+    }
+    itemCount(){
+
+    }
+    showList(){
+
+    }
+    saveLocal(){
+
+    }
+}
