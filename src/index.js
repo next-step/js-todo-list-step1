ad2caf9788aff382a75e62d0fecb4ae174ea0d97
EastjunDev <eastjun.dev@gmail.com>
2020-09-05 21:26:03 +0900

    Revert "[ADD]InputNewTodo"
    This reverts commit 3c0e702f327546ce1fac9d670ca154632752c51f.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const inputNewTodo = document.getElementById('new-todo-title');
-
-const handleInput = (e) => {
-  if (e.key === 'Enter') {
-    //
-  }
-};
-
-inputNewTodo.addEventListener('keypress', handleInput);
