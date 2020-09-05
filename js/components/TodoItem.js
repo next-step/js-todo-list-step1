83df1f2663582d2526bf19724b845164c8c75b13
kdongha <kdongha1597@gmail.com>
2020-09-06 00:03:04 +0900

    todoItem 구조 변경

--- js/components/TodoItem.js
+++ js/components/TodoItem.js
@@ -1,9 +1,11 @@
 const TodoItem = ({id, text, done, edit}) => `
 	<li class="${done ? 'completed' : ''} ${edit ? 'editing' : ''}">
-		<input type="checkbox" class="toggle" data-id=${id} data-role="toggle" ${done ? 'checked' : ''} />
-		<label class="view" data-id=${id} data-role="edit">${text}</label>
+		<div class="view">
+			<input type="checkbox" class="toggle" data-id=${id} data-role="toggle" ${done ? 'checked' : ''} />
+			<label data-id=${id} data-role="edit">${text}</label>
+			<button class="destroy" data-id=${id} data-role="remove"/>
+		</div>
 		<input class="edit" value=${text} data-id=${id} data-role="change" />
-		<i class="destroy" data-id=${id} data-role="remove"/>
 	</li>
 `;
 
