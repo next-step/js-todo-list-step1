04b9b0f5ab738331f8631f665c917da303a094d1
김휘겸 <breakstorm00@gmail.com>
2020-09-06 22:27:41 +0900

    입력 이벤트 처리

--- js/app.js
+++ js/app.js
@@ -1,23 +1,39 @@
 import todoItem from './todoItem.js';
-const DATA = []
+let ID = 0;
+const DATA = [];
+// {id, context, complete}
 
-const todoInputElement = document.getElementById('new-todo-title')
-const todoListElement = document.getElementById('todo-list')
 
-todoInputElement.addEventListener('keyup', (event) => {
-    const key = event.key 
+/************* element select & event setting ****************/ 
+const $todoInputElement = document.getElementById('new-todo-title');
+const $todoListElement = document.getElementById('todo-list');
+const $filtersElement = document.querySelector('.filters');
+
+$todoInputElement.addEventListener('keyup', (event) => {
+	const key = event.key;
+	const valueTrim = $todoInputElement.value.trim()
     if (key === "Enter") {
         let data = {
-            id: DATA.length + 1,
-            context: 'test',// input value
+            id: ID + 1,
+            context: valueTrim,// input value
             complete: false
-        }
+		}
+		inputEvent(data)
+		render()
+		$todoInputElement.value = '';
     }
 })
 
 
-todoListElement.addEventListener('dblclick', (event) => {
+$todoListElement.addEventListener('dblclick', (event) => {
     // 입력된 값 수저하는 기능
 })
-console.log('app.js' );
-// todoItem();
+
+/**************** App logic ****************/
+const inputEvent = (newData) => {
+	DATA.push(newData)
+	console.log('inputEvent', newData, DATA);
+}
+const render =  () => {
+	$todoListElement.innerHTML = DATA.map( (item) => todoItem(item) ).join('')
+}
--- js/todoItem.js
+++ js/todoItem.js
@@ -1,5 +1,12 @@
-const todoItem = () => {
-    console.log('todoItem', );
+const todoItem = ({id, context, complete}) => {
+    return `
+    <li>
+        <input type="text" class="edit" value="${context}">
+        <input type="checkbox" class="toggle" value="${complete}">
+        <label for="toggle" class="">${context}</label>
+        <button class="destroy"></button>
+    </li>
+    `
 }
 
 export default todoItem;
