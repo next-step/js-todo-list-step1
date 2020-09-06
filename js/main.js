9ae56f2ad6ef83f46f2ef55344c9371ea098b07b
kingdotol <oh@dotol.net>
2020-09-07 04:29:11 +0900

    상태관리 예제를 사용한 구조변화(입력->데이터저장->렌더)

--- js/main.js
+++ js/main.js
@@ -1,91 +1,71 @@
-const $todoInput = document.querySelector("#new-todo-title");
-const $toggleInput = document.querySelector(".toggle");
+function TodoApp() {
+    this.todoItems = [];
+    this.id = 0;
 
-$todoInput.addEventListener("keyup", onAddTodoItem);
+    this.setState = updatedItems => {
+        this.todoItems = updatedItems;
+        todoList.setState(this.todoItems);
+      };
 
-document.getElementById("todo-list").addEventListener("click", function (e) {            
-    if (e.target && e.target.nodeName == "INPUT" && e.target.classList == "toggle") {    
-        e.target.closest("li").classList.toggle("completed");
-    }
-    else if(e.target && e.target.nodeName == "BUTTON" && e.target.classList == "destroy"){
-        if(confirm("정말로 삭제하시겠습니까?")){ 
-            e.target.closest("li").remove();
-            countList();
+    new TodoInput({
+        onAdd: contents => {
+          const newTodoItem = new TodoItem(contents,this.id++);
+          this.todoItems.push(newTodoItem);
+          this.setState(this.todoItems);
         }
-    }
-});
+    });
+    
+    const todoList = new TodoList();
 
-document.getElementById("todo-list").addEventListener("dblclick", function(e){
-    if (e.target && e.target.nodeName == "LABEL" && e.target.classList == "label"){
-        e.target.closest("li").classList.add("editing");
-        e.target.closest("li").lastElementChild.focus();
-    }
-});
+}  
 
-document.getElementById("todo-list").addEventListener("keyup", function(e){
-    if (e.target && e.target.nodeName == "INPUT" && e.target.classList == "edit" && e.key == "Escape"){
-        e.target.closest("li input").value = e.target.closest("li").querySelector("label").innerText;
-        e.target.closest("li").classList.remove("editing");
-    }
-});
-
-document.getElementById("todo-list").addEventListener("focusout", function(e){
-    if (e.target && e.target.nodeName == "INPUT" && e.target.classList == "edit"){
-        e.target.closest("li").querySelector("label").innerText = e.target.closest("li input").value;
-        e.target.closest("li").classList.remove("editing");
-    }
-});
-
-document.querySelector(".filters").addEventListener("click", function(e){
-    if (e.target && e.target.nodeName == "A"){
-        document.querySelector(".filters .selected").classList.remove("selected");
-        e.target.classList.add("selected");
-        if (e.target.classList.contains("all")){
-            document.querySelectorAll(".todo-list li").forEach( e => {
-                e.style.display = "";
-            });
-        }
-        else if (e.target.classList.contains("active")){
-            document.querySelectorAll(".todo-list li").forEach( function(e){
-                if(e.closest("li").classList == "completed") e.style.display = "none";
-                else e.style.display = "";
-            });
-        }
-        else if (e.target.classList.contains("completed")){
-            document.querySelectorAll(".todo-list li").forEach(function(e){
-                if(e.closest("li").classList != "completed") e.style.display = "none";
-                else e.style.display = "";
-            });
-        }
-    }
-});
-
-function countList(){
-    document.querySelector(".todo-count strong").innerText = 
-      document.querySelector("#todo-list").childElementCount
-}
-
-function onAddTodoItem(event) {
-    const todoTitle = event.target.value;
-    const todoList = document.getElementById("todo-list");
-    if (event.key === "Enter" && todoTitle !== "") {
-        todoList.insertAdjacentHTML("beforeend", renderTodoItemTemplate(todoTitle));
-        event.target.value = "";
-        countList();
+function TodoInput({onAdd}){
+    const $todoInput = document.querySelector("#new-todo-title");
+  
+    $todoInput.addEventListener("keydown", event => this.addTodoItem(event));
+  
+    this.addTodoItem = event => {
+      const $newTodoTarget = event.target;
+      if (this.isValid(event, $newTodoTarget)) {
+        onAdd($newTodoTarget.value);
+        $newTodoTarget.value = "";
+      }
+    };
+  
+    this.isValid = (event,input) => {
+      if(event && input.value && event.key == "Enter")
+        return true;
     }
 }
 
-function onToggleTodoItem(event) {
-  event.target.closest("li").classList.toggle("completed");
+function TodoList() {
+    const $todoList = document.querySelector("#todo-list");
+    this.setState = (updatedTodoItems,viewMode) => {
+      this.todoItems = updatedTodoItems;
+      this.render(this.todoItems);
+    };
+  
+    this.render = items => {
+       const template = items.map(todoItemTemplate);
+       $todoList.innerHTML = template.join("");
+    };
+    
+    function todoItemTemplate({title,id}){
+        return ` <li data-id="${id}">
+                        <div class="view">
+                            <input class="toggle" type="checkbox">
+                            <label class="label">${title}</label>
+                            <button class="destroy"></button>
+                        </div>
+                        <input class="edit" value="${title}">
+                    </li>`;
+      }
+  }
+  
+function TodoItem(title,id){
+    this.id = id;
+    this.title = title;
+    this.completed = false;
 }
 
-function renderTodoItemTemplate(title) {
-  return ` <li>
-                  <div class="view">
-                      <input class="toggle" type="checkbox">
-                      <label class="label">${title}</label>
-                      <button class="destroy"></button>
-                  </div>
-                  <input class="edit" value="${title}">
-              </li>`;
-}
+new TodoApp();
