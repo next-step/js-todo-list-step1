a620a36b9146b4c23dd46faca720bdd1866099bf
kingdotol <oh@dotol.net>
2020-09-07 03:25:02 +0900

    X버튼을 이용해서 해당 엘리먼트를 삭제

--- js/main.js
+++ js/main.js
@@ -5,10 +5,15 @@ $todoInput.addEventListener("keyup", onAddTodoItem);
 
 document.getElementById("todo-list").addEventListener("click", function (e) {            
     if (e.target && e.target.nodeName == "INPUT" && e.target.classList == "toggle") {    
-       e.target.closest("li").classList.toggle("completed");
-     }
-  });
-  
+        e.target.closest("li").classList.toggle("completed");
+    }
+    else if(e.target && e.target.nodeName == "BUTTON" && e.target.classList == "destroy"){
+        if(confirm("정말로 삭제하시겠습니까?")){ 
+            e.target.closest("li").remove();
+        }
+    }
+});
+
 function onAddTodoItem(event) {
   const todoTitle = event.target.value;
   const todoList = document.getElementById("todo-list");
