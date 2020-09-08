fa7202148316a9b50b150b7907a4345a52dbbaf1
kyungso <cks678928@gmail.com>
2020-09-08 16:16:37 +0900

    feat: todo list의 item갯수를 count한 갯수를 리스트의 하단에 보여주기

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,16 +1,22 @@
 const $todoInput = document.querySelector("#new-todo-title");
 const $todoList = document.getElementById("todo-list");
+const $todoCount = document.querySelector(".todo-count strong");
 
 $todoInput.addEventListener("keyup", onAddTodoItem);
 $todoList.addEventListener("click", onToggleTodoItem);
 $todoList.addEventListener("dblclick", onEditTodoItem);
 $todoList.addEventListener("keyup", onCancelEditTodoItem);
 
+function onCountTodoList() {
+    $todoCount.innerText = $todoList.childElementCount
+}
+
 function onAddTodoItem(event) {
     const todoTitle = event.target.value;
     if (event.key === "Enter" && todoTitle !== "") {
         $todoList.insertAdjacentHTML("beforeend", renderTodoItemTemplate(todoTitle));
         event.target.value = "";
+        onCountTodoList();
     }
 }
 
@@ -19,6 +25,7 @@ function onToggleTodoItem(event) {
         event.target.closest("li").classList.toggle("completed");
     else if (event.target.classList == "destroy") {
         event.target.closest("li").remove();
+        onCountTodoList();
     }
 }
 
