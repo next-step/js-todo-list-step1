1978221776b0c42570b77f0c15cba48948f2c7b0
kyungso <cks678928@gmail.com>
2020-09-08 16:27:57 +0900

    feat: todo list의 상태값을 확인하여, 해야할 일과, 완료한 일을 클릭하면 해당 상태의 아이템만 보여주기

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,11 +1,13 @@
 const $todoInput = document.querySelector("#new-todo-title");
 const $todoList = document.getElementById("todo-list");
 const $todoCount = document.querySelector(".todo-count strong");
+const $todoFilter = document.querySelector(".filters");
 
 $todoInput.addEventListener("keyup", onAddTodoItem);
 $todoList.addEventListener("click", onToggleTodoItem);
 $todoList.addEventListener("dblclick", onEditTodoItem);
 $todoList.addEventListener("keyup", onCancelEditTodoItem);
+$todoFilter.addEventListener("click", onFilterTodoList);
 
 function onCountTodoList() {
     $todoCount.innerText = $todoList.childElementCount
@@ -52,4 +54,29 @@ function renderTodoItemTemplate(title) {
                   </div>
                   <input class="edit" value="${title}">
               </li>`;
+}
+
+function onFilterTodoList(event) {
+    const selectedFilter = document.querySelector(".filters .selected");
+    const allTodoList = document.querySelectorAll(".todo-list li");
+
+    selectedFilter.classList.remove("selected");
+    event.target.classList.add("selected");
+
+    if (event.target.classList.contains("all")) {
+        allTodoList.forEach(e => {
+            e.style.display = "block";
+        });
+    } else if (event.target.classList.contains("active")) {
+        allTodoList.forEach(e => {
+            if (e.closest("li").classList == "completed") e.style.display = "none";
+            else e.style.display = "block";
+        });
+    } else if (event.target.classList.contains("completed")) {
+        allTodoList.forEach(e => {
+            if (e.closest("li").classList != "completed") e.style.display = "none";
+            else e.style.display = "block";
+        });
+    }
+
 }
