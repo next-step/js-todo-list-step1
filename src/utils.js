04b19af1459177d52ccf63e81afe153ee703bdf1
eyabc <bey4314@naver.com>
2020-09-09 11:08:01 +0900

    refactor: parentNode -> closest(황준일님께 도움받은 부분)

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export const addEventBubblingListener = ($target, action, className, callback) => {
     $target.addEventListener(action, event => {
         const { target } = event;
-        const index = target?.parentNode?.dataset?.index;
+        const index = target?.closest('[data-index]')?.dataset?.index;
         target.classList.contains(className) && callback({ index, event });
     });
 };
