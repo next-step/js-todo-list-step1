181ac93ef362af7155335a93913ed0b7c27b7ffd
kingdotol <oh@dotol.net>
2020-09-13 01:45:33 +0900

    TodoState생성

---
+++ js/TodoState.js
@@ -0,0 +1,15 @@
+export default new class TodoState {
+    constructor(){
+        this.todoItems = [];
+        this.id = 0;
+        this.view = "all";
+    }
+}
+// class TodoState {
+//     constructor(){
+//         this.todoItems = [];
+//         this.id = 0;
+//         this.view = "all";
+//     }
+// }
+//  const TodoState  = new TodoState();
