c565c04a5ec719212334e6ec0cccc5464a56c79a
kingdotol <oh@dotol.net>
2020-09-14 05:41:44 +0900

    function->class

--- js/TodoCount.js
+++ js/TodoCount.js
@@ -1,26 +1,29 @@
 import TodoState from "./TodoState.js"
 import ItemController from "./ItemController.js"
-export function TodoCount({onChangeView}){
-    const $todoFilter = document.querySelector(".filters");
-    const $todoFilters = $todoFilter.querySelectorAll(".filters a");
-    const $count = document.querySelector(".todo-count strong");
-  
-    this.changeView = ({classList:$selectedMode}) => {
-        onChangeView($selectedMode[0]);
+export class TodoCount{
+    constructor({onChangeView}){
+        this.$todoFilter = document.querySelector(".filters");
+        this.$todoFilters = $todoFilter.querySelectorAll(".filters a");
+        this.$count = document.querySelector(".todo-count strong");
+        this.onChangeView = onChangeView;
+        
+        this.$todoFilter.addEventListener("click", ({target}) => {
+            if(target.nodeName == "A" && !target.classList.contains("selected")) {
+                this.changeView(target);
+            }
+        });
+    }
+    
+    changeView = ({classList:$selectedMode}) => {
+        this.onChangeView($selectedMode[0]);
         TodoState.view = $selectedMode[0];
-        $todoFilters.forEach((node)=>{ node.classList.remove("selected"); });
+        this.$todoFilters.forEach((node)=>{ node.classList.remove("selected"); });
         $selectedMode.add("selected");
         this.count($selectedMode[0]);
     }
     
-    this.count = (view = TodoState.view) =>{
-        $count.innerText = ItemController.getItemsByState(view).length;//굳이 아이템불러와야하나! state에 상태별갯수 저장?
-        //$count.innerText = $todoList.childElementCount
+    count = (view = TodoState.view) =>{
+        this.$count.innerText = ItemController.getItemsByState(view).length;//굳이 아이템불러와야하나! state에 상태별갯수 저장?
+        //this.$count.innerText = $todoList.childElementCount
     }
-
-    $todoFilter.addEventListener("click", ({target}) => {
-        if(target.nodeName == "A" && !target.classList.contains("selected")) {
-            this.changeView(target);
-        }
-    });
 }
--- js/TodoInput.js
+++ js/TodoInput.js
@@ -1,16 +1,18 @@
-export function TodoInput({addItem}){
-  const $todoInput = document.querySelector("#new-todo-title");
-
-  this.addTodoItem = (target) => {
+export class TodoInput{
+  constructor({addItem}){
+    this.$todoInput = document.querySelector("#new-todo-title");
+    this.addItem = addItem;
+    
+    this.$todoInput.addEventListener("keydown", ({target, key}) => {
+      if(key === "Enter") this.addTodoItem(target);
+    });
+  }
+  
+  addTodoItem(target) {
     const $newTodoTarget = target;
-    if (!!$newTodoTarget.value.trim()) {
-      addItem($newTodoTarget.value);
+    if(!!$newTodoTarget.value.trim()) {
+      this.addItem($newTodoTarget.value);
       $newTodoTarget.value = "";
     }
-  };
-  
-  $todoInput.addEventListener("keydown", ({target, key}) => {
-    if(key === "Enter") this.addTodoItem(target);
-  });
-
+  }
 }
