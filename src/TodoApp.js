b8042b490ecf128dd1c5f4f91118e42149ab1481
kyungso <cks678928@gmail.com>
2020-10-21 20:25:09 +0900

    [3기 kingso] TodoList with CRUD (#42)
    * feat: todo list에 todoItem을 키보드로 입력하여 추가하기
    
    * feat: todo list의 체크박스를 클릭하여 complete 상태로 변경.
    
    * feat: todo list의 x버튼을 이용해서 해당 엘리먼트를 삭제
    
    * feat: todo list를 더블클릭했을 때 input 모드로 변경
    
    * feat: esc키를 누르면 수정되지 않은 채로 다시 view 모드로 복귀
    
    * feat: todo list의 item갯수를 count한 갯수를 리스트의 하단에 보여주기
    
    * feat: todo list의 상태값을 확인하여, 해야할 일과, 완료한 일을 클릭하면 해당 상태의 아이템만 보여주기
    
    * feat: todo list의 item 수정을 완료하고 enter키를 누르면 수정된 값으로 view 모드로 복귀

---
+++ src/TodoApp.js
@@ -0,0 +1,85 @@
+const $todoInput = document.querySelector("#new-todo-title");
+const $todoList = document.getElementById("todo-list");
+const $todoCount = document.querySelector(".todo-count strong");
+const $todoFilter = document.querySelector(".filters");
+
+$todoInput.addEventListener("keyup", onAddTodoItem);
+$todoList.addEventListener("click", onToggleTodoItem);
+$todoList.addEventListener("dblclick", onEditTodoItem);
+$todoList.addEventListener("keyup", onCancelEditTodoItem);
+$todoFilter.addEventListener("click", onFilterTodoList);
+
+function onCountTodoList() {
+    $todoCount.innerText = $todoList.childElementCount
+}
+
+function onAddTodoItem(event) {
+    const todoTitle = event.target.value;
+    if (event.key === "Enter" && todoTitle !== "") {
+        $todoList.insertAdjacentHTML("beforeend", renderTodoItemTemplate(todoTitle));
+        event.target.value = "";
+        onCountTodoList();
+    }
+}
+
+function onToggleTodoItem(event) {
+    if (event.target.classList == "toggle")
+        event.target.closest("li").classList.toggle("completed");
+    else if (event.target.classList == "destroy") {
+        event.target.closest("li").remove();
+        onCountTodoList();
+    }
+}
+
+function onEditTodoItem(event) {
+    if (event.target.classList == "label") {
+        event.target.closest("li").classList.add("editing");
+        event.target.closest("li").lastElementChild.focus();
+    }
+}
+
+function onCancelEditTodoItem(event) {
+    if (event.target.classList == "edit" && event.key == "Escape") {
+        event.target.closest("li input").value = event.target.closest("li").querySelector("label").innerText;
+        event.target.closest("li").classList.remove("editing");
+    } else if (event.target.classList == "edit" && event.key == "Enter") {
+        event.target.closest("li").querySelector("label").innerText = event.target.value;
+        event.target.closest("li").classList.remove("editing");
+    }
+}
+
+function renderTodoItemTemplate(title) {
+    return ` <li>
+                  <div class="view">
+                      <input class="toggle" type="checkbox">
+                      <label class="label">${title}</label>
+                      <button class="destroy"></button>
+                  </div>
+                  <input class="edit" value="${title}">
+              </li>`;
+}
+
+function onFilterTodoList(event) {
+    const selectedFilter = document.querySelector(".filters .selected");
+    const allTodoList = document.querySelectorAll(".todo-list li");
+
+    selectedFilter.classList.remove("selected");
+    event.target.classList.add("selected");
+
+    if (event.target.classList.contains("all")) {
+        allTodoList.forEach(e => {
+            e.style.display = "block";
+        });
+    } else if (event.target.classList.contains("active")) {
+        allTodoList.forEach(e => {
+            if (e.closest("li").classList == "completed") e.style.display = "none";
+            else e.style.display = "block";
+        });
+    } else if (event.target.classList.contains("completed")) {
+        allTodoList.forEach(e => {
+            if (e.closest("li").classList != "completed") e.style.display = "none";
+            else e.style.display = "block";
+        });
+    }
+
+}
