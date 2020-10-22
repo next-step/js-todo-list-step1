dd6bdcd75e7ee4ef236f90c533063c991ab2d8b2
DevRappers <devrappers@gmail.com>
2020-10-22 22:15:14 +0900

    [ADD] remove, toggle

--- src/Components/App.js
+++ src/Components/App.js
@@ -13,9 +13,39 @@ export default store => {
   const toggleTodoItem = id => store.dispatch(toggleTodo(id));
   const updateTodoItem = (id, text) => store.dispatch(updateTodo(id, text));
 
+  const list = document.getElementById("todo-list");
+
   TodoInput(insertTodoItem);
 
+  const onClick = e => {
+    const {
+      dataset: { index }
+    } = e.target.parentElement;
+
+    const id = parseInt(index, 10);
+
+    if (e.target.className === "destroy") {
+      removeTodoItem(parseInt(index, 10));
+      return;
+    }
+    if (e.target.className === "toggle") {
+      toggleTodoItem(id);
+    }
+  };
+
+  const setCount = state => {
+    const { todo } = state;
+
+    const todoCount = document.querySelector(".todo-count");
+    const count = todoCount.querySelector("strong");
+
+    count.innerHTML = todo.length;
+  };
+
+  list.addEventListener("click", onClick);
+
   store.subscribe(() => {
+    setCount(store.getState());
     TodoList(store.getState());
   });
 };
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,18 +1,51 @@
-const getClassName = completed => (completed ? "class='completed'" : "");
+const getClassName = isCompleted => (isCompleted ? "completed" : "");
 
-const addList = ({ key, text, completed }) => `
-    <li key={key} ${getClassName(completed)} >
-        <div class="view" data-index="${key}">
-          <input class="toggle" type="checkbox" ${completed ? " checked" : ""}/>
-          <label class="label">${text}</label>
-          <button class="destroy"></button>
-        </div>
-        <input class="edit" value="${text}" data-index="${key}"/>
-    </li>
-`;
+const makeElement = (tagName, options) => {
+  const tag = document.createElement(tagName);
+
+  Object.entries(options).forEach(([key, value]) => (tag[key] = value));
+
+  return tag;
+};
+
+const createListItem = ({ id, text, completed }) => {
+  const toDo = makeElement("li", { className: getClassName(completed) });
+
+  const viewDiv = makeElement("div", { className: "view" });
+  viewDiv.dataset.index = id;
+
+  const toggle = makeElement("input", {
+    type: "checkbox",
+    className: "toggle"
+  });
+
+  if (completed) {
+    toggle.setAttribute("checked", completed);
+  }
+
+  const label = makeElement("label", {
+    className: "label",
+    innerHTML: text
+  });
+
+  const removeButton = makeElement("button", {
+    className: "destroy",
+    type: "button"
+  });
+
+  viewDiv.appendChild(toggle);
+  viewDiv.appendChild(label);
+  viewDiv.appendChild(removeButton);
+
+  toDo.appendChild(viewDiv);
+
+  return toDo;
+};
 
 export default ({ todo: todoList }) => {
   const list = document.getElementById("todo-list");
 
-  list.innerHTML = todoList.map(addList).join("");
+  list.innerHTML = todoList
+    .map(todo => createListItem(todo).outerHTML)
+    .join("");
 };
