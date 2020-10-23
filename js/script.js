fda3446cf9dc66d38eabf6f095d07662fad6c3d8
rsj2003 <rsj2003@naver.com>
2020-10-24 00:44:58 +0900

    refactor : 변수 이름 변경, 함수뒤 세미콜론 붙임

--- js/script.js
+++ js/script.js
@@ -1,78 +1,85 @@
 window.onload = function() {
   const mainInput = document.getElementById("new-todo-title");
   const mainList = document.getElementById("todo-list");
+  const listCount = document.querySelector(".todo-count strong");
 
   mainInput.addEventListener("keydown", e => {
     // console.log(e.key);
     if(e.key === "Enter" && mainInput.value.replace(/ /g,"") != "") {
       const liElement = document.createElement("li");
       const viewDivElement = document.createElement("div");
-      const inputEditElement = document.createElement("input");
-      const inputToggleElement = document.createElement("input");
+      const editInputElement = document.createElement("input");
+      const toggleInputElement = document.createElement("input");
       const labelLabelElement = document.createElement("label");
-      const buttonDestroyElement = document.createElement("button");
+      const destroyButtonElement = document.createElement("button");
 
       viewDivElement.setAttribute("class", "view");
-      inputEditElement.setAttribute("class", "edit");
-      inputToggleElement.setAttribute("class", "toggle");
-      inputToggleElement.setAttribute("type", "checkbox");
+      editInputElement.setAttribute("class", "edit");
+      toggleInputElement.setAttribute("class", "toggle");
+      toggleInputElement.setAttribute("type", "checkbox");
       labelLabelElement.setAttribute("class", "label");
-      buttonDestroyElement.setAttribute("class", "destroy");
+      destroyButtonElement.setAttribute("class", "destroy");
 
-      inputToggleElement.addEventListener("input", e => {
+      toggleInputElement.addEventListener("input", e => {
         const target = e.target.parentNode.parentNode;
         if(e.target.checked) return target.classList.add("completed");
         target.classList.remove("completed");
-      })
+      });
       
-      buttonDestroyElement.addEventListener("click", e => {
+      destroyButtonElement.addEventListener("click", e => {
         e.target.parentNode.parentNode.remove();
-      })
+        
+        const count = mainList.childNodes.length;
+        listCount.innerHTML = count;
+      });
 
       liElement.addEventListener("dblclick", e => {
         const parentLi = parent(e.target, "li");
         parentLi.classList.add("editing");
         parentLi.childNodes[1].focus();
-      })
+      });
 
-      inputEditElement.addEventListener("blur", e => {
+      editInputElement.addEventListener("blur", e => {
         const parentLi = parent(e.target, "li");
         const siblingDiv = e.target.previousSibling.childNodes[1];
         parentLi.classList.remove("editing");
         siblingDiv.innerHTML = e.target.value;
-      })
+      });
 
-      inputEditElement.addEventListener("keydown", e => {
+      editInputElement.addEventListener("keydown", e => {
         if(e.key === "Enter") {
           const parentLi = parent(e.target, "li");
           const siblingDiv = e.target.previousSibling.childNodes[1];
           parentLi.classList.remove("editing");
           siblingDiv.innerHTML = e.target.value;
-        }
+        };
         if(e.key === "Escape") {
           const parentLi = parent(e.target, "li");
           const siblingDiv = e.target.previousSibling.childNodes[1];
           parentLi.classList.remove("editing");
           e.target.value = siblingDiv.innerHTML;
-        }
-      })
+        };
+      });
 
       labelLabelElement.innerHTML = mainInput.value;
-      inputEditElement.value = mainInput.value;
+      editInputElement.value = mainInput.value;
 
-      viewDivElement.append(inputToggleElement, labelLabelElement, buttonDestroyElement);
-      liElement.append(viewDivElement, inputEditElement);
+      viewDivElement.append(toggleInputElement, labelLabelElement, destroyButtonElement);
+      liElement.append(viewDivElement, editInputElement);
       mainList.append(liElement);
 
-      mainInput.value = "";
-    }
-  })
+      const count = mainList.childNodes.length;
+      listCount.innerHTML = count;
 
-}
+      mainInput.value = "";
+    };
+  });
+
+};
 
 function parent(target, tag) {
   while(target.tagName.toLowerCase() != tag) {
     target = target.parentNode;
-  }
+  };
   return target;
-}
+};
