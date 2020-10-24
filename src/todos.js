bf14ed0f606fc4975431a8cde38e504929299dd1
mavriel <mavriel@gmail.com>
2020-10-24 23:56:35 +0900

    item갯수를 count한 갯수를 리스트의 하단에 보여주기

--- src/todos.js
+++ src/todos.js
@@ -2,6 +2,7 @@ import { setTodoHandler } from "./store.js";
 import { makeTodo } from "./todo.js";
 
 const todoList = document.getElementById("todo-list");
+const todoCount = document.getElementById("todo-count");
 
 const removeAllChildren = () => {
   while (todoList.firstChild) {
@@ -11,9 +12,12 @@ const removeAllChildren = () => {
 
 const addTodo = (todo) => todoList.appendChild(todo);
 
+const setTodoCount = R.pipe(R.length, (count) => (todoCount.innerHTML = count));
+
 const renderTodos = R.pipe(
   R.tap(removeAllChildren),
   R.values,
+  R.tap(setTodoCount),
   R.reverse,
   R.map(makeTodo),
   R.forEach(addTodo)
