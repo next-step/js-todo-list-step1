70c224a2cd2ab0100433a6290615fea2ecb6613b
breakstorm <breakstorm00@gmail.com>
2020-10-25 12:03:32 +0900

    아이템 추가 하기 작성

--- js/TodoList/head.js
+++ js/TodoList/head.js
@@ -1,6 +1,15 @@
 class Head {
-    constructor () {
+    constructor (parent, item) {
         console.log('TEST: TodoList Head', );
+        this.$TodoInput = document.getElementById('new-todo-title')
+        this.$TodoInput.addEventListener('keyup', (e) => {
+            console.log('TEST: input keyup event', parent, item);
+            if (e.key === "Enter" ) {
+                console.log('TEST: Enter', this.$TodoInput);
+                let input = this.$TodoInput.value.trim()
+                parent.addItem(input)
+            }
+        })
     }
 }
 export { Head }
--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ console.log('TEST: TodoList init', );
 let ID = 0;
 class app {
     constructor () {
-        this.item = [{id: ID++, context: 'test', complete: false}] // {id, context, complete}
+        this.item = [] // {id, context, complete}, {id: ID++, context: 'test', complete: false}
         console.log('TEST: app.js', this.item, ID);
         this.Head = new Head(this, this.item);
         this.Body = new Body(this, this.item);
@@ -16,13 +16,26 @@ class app {
         this.Template = Template
         this.$TodoInput = document.getElementById('new-todo-title')
         this.$TodoList = document.getElementById('todo-list')
-        this.render();
+        // this.render();
     }
 
     render() {
+        /**  body.js로 옮길것  */
         let result = this.item.map(v => Template(v)).join()
+        this.$TodoList.innerHTML = '';
         this.$TodoList.insertAdjacentHTML('beforeend', result)
+        /** bottom.js로 옮길것  */
     }
+    
+    addItem (inputValue) {
+        let eachItem = {id: ID++, context: inputValue, complete: false}
+        console.log('TEST: addItem', eachItem);
+        this.item.push(eachItem)
+        this.render()
+    }
+    updateItem () {}
+    deleteItem () {}
+
 }
 
 let TodoList = new app();
