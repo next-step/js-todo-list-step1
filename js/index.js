1570477d20b68ef3a59b46f83a39363dcdee1ff1
wonsunil <seonil6352@gmail.com>
2020-10-27 22:17:06 +0900

    feat: add filter button function

--- js/index.js
+++ js/index.js
@@ -3,16 +3,38 @@ window.onload = () => {
 
 	const all = document.querySelector(".all");
 	addBubblingEvent("click", all, function(e) {
+		selectFilter("all");
+
 		app.renderAsState("all");
 	});
 
 	const active = document.querySelector(".active");
 	addBubblingEvent("click", active, function(e) {
+		selectFilter("active");
+
 		app.renderAsState("active");
 	});
 
 	const completed = document.querySelector(".completed");
 	addBubblingEvent("click", completed, function(e) {
+		selectFilter("completed");
+
 		app.renderAsState("completed");
 	});
+
+	const filters = document.querySelector(".filters");
+
+	function selectFilter(filter) {
+		objectForEach(filters.children, item => {
+			if(typeof item === "object") {
+				if(item.children[0].classList[0] == filter) {
+					item.children[0].classList.add("selected");
+
+					return;
+				};
+
+				item.children[0].classList.remove("selected");
+			};
+		});
+	};
 };
