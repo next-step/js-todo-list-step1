e6b9bc756734474529064d6711567716b12e4b25
sarah1446 <lemonview2@gmail.com>
2020-10-28 15:38:48 +0900

    todo 삭제,수정,개수까지 구현완료

--- App.js
+++ App.js
@@ -1,25 +1,16 @@
 import TodoInput from './components/TodoInput.js';
 import TodoList from './components/TodoList.js';
+import TodoCount from './components/TodoCount.js';
 import newId from './components/utils/newId.js'
 
 function App() {
-    this.data = [
-        // {
-        //     text: 'study!!',
-        //     id: 1,
-        //     completed: false
-        // },
-        // {
-        //     text: '운동 헛둘헛둘',
-        //     id: 2,
-        //     completed: true
-        // }
-    ];
+    this.data = [];
     this.app = this;
 
     //컴포넌트 인스턴스 생성
     const todoInput = new TodoInput(this.app);
     const todoList = new TodoList(this.data, this.app);
+    const todoCount = new TodoCount(this.app, this.data)
     
    
     this.addTodo = newTodo => {
@@ -27,7 +18,8 @@ function App() {
         data.push({
             text: newTodo,
             id: newId(),
-            completed: false
+            completed: false,
+            modifyMode: false
         })
         this.setState(data);
     }
@@ -42,6 +34,23 @@ function App() {
         this.setState(data);
     }
    
+    this.deleteTodo = deleteId => {
+        // 왜 const data = this.data.slice()복제해서 하면 안되지???
+        const data =  this.data.filter(todo => todo.id !== deleteId);
+        this.setState(data)
+    }
+    
+    this.editTodo = (editedText, id) => {
+        const data = this.data.slice();
+        for(let i =0; i < data.length; i++) {
+            if(data[i].id === id ) {
+                data[i].text = editedText;
+            }
+        }
+        this.setState(data)
+    }
+    
+    
     
     this.setState = updatedData => {
         this.data = updatedData;
@@ -49,6 +58,7 @@ function App() {
     }
     this.render = () => {
         todoList.setState(this.data);
+        todoCount.setState(this.data)
     }
     
     
--- components/TodoList.js
+++ components/TodoList.js
@@ -7,17 +7,55 @@ function TodoList(data, app) {
     this.render = () => {
         $todoList.innerHTML = this.data.map((todo, key) => {
             return( 
-                `<li  class="${todo.completed ? `completed` : ' ' }" id=${todo.id}>
+                `<li  class="${todo.completed ? `completed` : ' ' } " id=${todo.id}>
                     <div class="view">
                         <input class="toggle" type="checkbox" ${todo.completed ? `checked` : ''}/>
                         <label class="label">${todo.text}</label>
                         <button class="destroy"></button>
                     </div>
-                    <input class="edit" value="새로운 타이틀" />
+                    <input class="edit" value="${todo.text}" />
                 </li>`
             )
-        })
+        }).join('')
     }
+    
+    
+     
+     //esc누르면 editing 클래스 삭제
+     this.editMode = e => {
+        if(e.target.className === 'label') {
+            e.target.parentNode.parentNode.classList.add('editing');
+        }
+        
+    }
+    $todoList.addEventListener('dblclick', this.editMode);
+    
+    
+    this.editTodo = e => {
+        if(e.target.className === 'edit') {
+            const editedText = e.target.value;
+            const id = e.target.parentNode.id;
+            
+           if(e.key === 'Enter') {
+                this.app.editTodo(editedText, id);
+            }else if(e.key === 'Escape') {
+                e.target.parentNode.classList.remove('editing') 
+            }
+        }
+    }
+    $todoList.addEventListener('keydown', this.editTodo);
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
     
     this.toggleTodo = e => {
         if(e.target.className === 'toggle') {
@@ -26,6 +64,19 @@ function TodoList(data, app) {
         }
     }
     $todoList.addEventListener('click', this.toggleTodo); //다른 코드 참고해보기
+    
+    this.deleteTodo = e => {
+        const deleteId = e.target.parentNode.parentNode.id;
+        if(e.target.className === 'destroy') {
+            this.app.deleteTodo(deleteId)
+        }
+    }
+    $todoList.addEventListener('click', this.deleteTodo);
+    
+   
+    
+    
+    
     
     
     this.setState = newData => {
