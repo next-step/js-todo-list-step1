946223b5011c20694c6c80e9355663692d977c87
wonsunil <seonil6352@gmail.com>
2020-11-01 03:53:43 +0900

    Add filter button template

--- src/components/TodoFilter.js
+++
@@ -1,9 +0,0 @@
-export default class TodoFilter{
-	filter() {
-		const all = document.querySelector(".filters a.all");
-		const active = document.querySelector(".filters a.active");
-		const completed = document.querySelector(".filters a.completed");
-
-		const filters = document.querySelector(".filters");
-	};
-};
