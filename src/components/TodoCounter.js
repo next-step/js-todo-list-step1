ca38f9b9ad40fc3295bcdfc73a95abae537330f3
wonsunil <seonil6352@gmail.com>
2020-11-01 03:53:09 +0900

    Simplify template

--- src/components/TodoCounter.js
+++ src/components/TodoCounter.js
@@ -4,20 +4,7 @@ export default class TodoCount extends Component {
 	template() {
 		return `
 		<div class="count-container">
-			<span class="todo-count">총 <strong>${this.$props.todoCount}</strong> 개</span>
-			<ul class="filters">
-				<li>
-					<a class="all selected" href="#">전체보기</a>
-				</li>
-				<li>
-					<a class="active" href="#active">해야할 일</a>
-				</li>
-				<li>
-					<a class="completed" href="#completed">완료한 일</a>
-				</li>
-			</ul>
-		</div>
-		</main>
+			<span class="todo-count">총 <strong>${this.$props.todoCount()}</strong> 개</span>
 		`;
 	};
 };
