979de505419311b67c40a3156492fdd54c2d6f8b
김휘겸 <breakstorm00@gmail.com>
2020-11-01 08:18:25 +0900

    PR피드백8. 개발 되지 못한 부분 발견

--- js/TodoList/TodoList.js
+++ js/TodoList/TodoList.js
@@ -1,3 +1,5 @@
+import { ENTER, ESC } from './KeyVariable.js'
+
 class TodoList {
     constructor (parent) {
         this.$TodoList = document.getElementById('todo-list')
@@ -17,12 +19,19 @@ class TodoList {
         this.$TodoList.addEventListener('dblclick', (e) => {
             const parentElement = e.target.closest('li')
             parentElement.className = 'editing'
+            const editInput = parentElement.querySelector('.edit')
+            editInput.focus()
         })
         this.$TodoList.addEventListener('keyup', (e) => {
-            if ( e.key === 'Enter' ) {
+            if ( e.key === ENTER ) {
                 const parentElement = e.target.closest('li')
                 const value = e.target.value
                 parent.afterUpdateItem(parentElement, value)
+            }
+            if ( e.key === ESC ) {
+                const parentElement = e.target.closest('li')
+                parentElement.className = ''
+                parent.render()
             }
         })
     }
