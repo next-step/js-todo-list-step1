8a147697e025222d32a417a3af941c6d5ba8615d
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2020-11-04 01:20:05 +0900

    todo list에 todoItem을 키보드로 입력하여 추가하기 및 localStorage에 정보 저장해서 새로고침해도 유지하기

---
+++ index.js
@@ -0,0 +1,3 @@
+import App from "./js/App.js";
+
+const app = new App();
---
+++ js/utils/createId.js
@@ -0,0 +1,14 @@
+// 아이디 새로 생성하기 (겹치지 않게)
+// 다른 방법 있는지 생각해보기
+export function createNewId() {
+  console.log("createId.js");
+  const date = new Date();
+  const newId =
+    date.getFullYear().toString() +
+    date.getMonth().toString() +
+    date.getDay().toString() +
+    date.getHours().toString() +
+    date.getMinutes().toString() +
+    date.getMilliseconds().toString();
+  return newId;
+}
