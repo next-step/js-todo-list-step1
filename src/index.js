960d312f9f182cae9499e1b0efc7f2578fe59161
jh <jh.hwang@softwareinlife.com>
2020-11-11 20:18:01 +0900

    CRUD 완성

--- src/index.js
+++ src/index.js
@@ -1,234 +1,65 @@
-import createStore from "./core/store.js";
-import DynamicDom from "./core/index.js";
+import DynamicDom from "./core/DynamicDom.js";
+import useState   from "./core/useState.js";
 
-import {
-  reducer,
-  addTodo, 
-  deleteTodo, 
-  toggleTodoState, 
-  editeTodoState,
-  updateTodoTitle
-} from "./store/index.js";
+import TodoApp from "./components/TodoApp.js";
 
-// const stateAction = new useState();
+import { reducer, addTodo } from "./store/store.js";
 
-function TodoApp(todo) {
+export const app = new DynamicDom(reducer);
 
-  // const [data, setState] = stateAction(1);
-
-  return DynamicDom.createElement("li", {
-    id: todo.id,
-    className: todo.state,
-    onDblclick: () => {
-      console.log(state());
-      init();
-    }
-  },  TodoList({todo}), 
-  TodoInput({todo}))
-}
-
-// function useState(payload) {
-//   const stateList = [];
-
-//   let state = payload;
-
-//   const setState = (payload) => {
-//     stateList.push(payload);
-//     init();
-//   }
-
-
-//   const getState = () => {
-//     let data 
-//     if(stateList) {
-//       data = stateList[0]
-//       stateList.shift()
-//     } else {
-//       data = state
-//     }
-    
-//     return data
-//   }
-
-//   return [getState(), setState]
-// }
-
-
-  
-
-
-function TodoList({todo}) {
-  return DynamicDom.createElement(
-    "div", 
-    {
-      className: "view",
-      id: todo.id,
-      onDblclick: ()=> {
-        console.log("click")
-        console.log(todo)
-        changeEditMode(todo.id)
-      }
-    }, 
-    TodoToggle({todo}),
-    TodoTitle({todo}),
-    TodoButton({id: todo.id})
-  )
-}
-
-function TodoInput({todo}) {
-  return DynamicDom.createElement("input",{
-    className: "edit",
-    value: todo.title,
-    type: "text",
-    onDblclick: (e)=> {
-      changeEditMode(todo.id)
-      e.target.value = todo.title
-    },
-    onKeypress: (e)=>{
-      if(e.keyCode === 13) {
-        updateTodo(e.target.value, todo.id)
-        changeEditMode(todo.id)
-      }
-      console.log(e.target.value)
-    }
-  }, "")
-}
-
-function TodoToggle({todo}) {
-
-  return DynamicDom.createElement("input", {
-    className: "toggle",
-    type: "checkbox",
-    checked: todo.state === "completed" ? true : false,
-    onClick: ()=> {
-      todoToggle(todo.id)
-    }
-  }, "")
-}
-
-function TodoTitle({todo}) {
-  return DynamicDom.createElement("label", null, `${todo.title}`)
-}
-
-function TodoButton({id}) {
-  return DynamicDom.createElement("button", {
-    className: "destroy",
-    id: id,
-    onClick: (e) => {
-      app.dispatch({
-        type: "DELETE_TODO",
-        payload: {
-          id: id
-        }
-      });
-    }
-  }, "")
-}
-
-
-const todoContainer = document.getElementById("todo-list");
-const todoInput = document.querySelector(".new-todo");
-
+const $todoContainer = document.getElementById("todo-list");
+const $todoInput = document.querySelector(".new-todo");
 const $todoCount = document.querySelector(".todo-count").querySelector("strong");
+const $todoFilters = document.querySelector(".filters");
 
-const app = new DynamicDom({
-  todos: [
-    {
-      id: 1,
-      title: "test",
-      state: ""
-    },
-    {
-      id: 2,
-      title: "test",
-      state: ""
-    },
-    {
-      id: 3,
-      title: "test",
-      state: ""
-    },
-    {
-      id: 4,
-      title: "test",
-      state: ""
-    },
-    {
-      id: 5,
-      title: "test",
-      state: ""
-    },
-  ]
-});
+const initData = localStorage.getItem("todo")
+  ? JSON.parse(localStorage.getItem("todo"))
+  : { todos: [] };
+app.setState(initData);
 
 function init() {
-  app.getState().todos.forEach((todo) => {
-    app.addDomList(TodoApp(todo), todo.id);
-  })
-  app.allDomRender(todoContainer);
-  $todoCount.innerHTML = app.getState().todos.length
+  app.getState().todos.forEach((todo, key) => {
+    app.addFiberList(TodoApp(todo, key), key);
+  });
+  app.render($todoContainer);
+  $todoCount.innerHTML = app.getState().todos.length;
 }
 
-// function update() {
-//   const TodoList = app.getState().todos.map((todo) => 
-//     TodoApp(todo)
-//   )
-//   app.compareDom(TodoList, todoContainer)
-// }
+export const todoState = new useState(init);
+
+function addTodoList(e) {
+  if(e.keyCode === 13) {
+    if(e.target.value === "") {
+      window.alert("내용을 입력해주세요.");
+    } else {
+      const id = app.getState().todos[0]? app.getState().todos[app.getState().todos.length-1].id + 1 : 1
+      app.dispatch(addTodo(id, e.target.value));
+      e.target.value = "";
+    }
+  }
+}
+
+function todoFilter(e) {
+  // e.preventDefault()
+  console.log(location.href)
+  if(e.target.tag === "A") {
+
+  }
+}
+
+function domEventListeners() {
+  $todoInput.addEventListener("keypress", addTodoList);
+  $todoFilters.addEventListener("click", todoFilter);
+}
 
 app.subscriber(init);
+
 init();
+domEventListeners();
 
-function addTodo2(e) {
-  if(e.keyCode === 13) {
-    app.dispatch({
-      type:"ADD_TODO",
-      payload: {
-        id: app.getState().todos[0]? app.getState().todos[app.getState().todos.length-1].id + 1 : 1,
-        title: e.target.value
-      }
-    })
-    e.target.value = ""
-  }
-  
-}
 
-function updateTodo(title, id) {
-  console.log("2")
-  app.dispatch({
-    type:"UPDATE_TODO_TITLE",
-    payload: {
-      id,
-      title
-    }
-  })
-}
-
-function todoToggle(id) {
-  console.log("toggle")
-  app.dispatch({
-    type: "TOGGLE_TODO_STATE",
-    payload: {
-      id
-    }
-  })
-}
-
-// state에서 edited는 삭제해야함. 다른 방식을 찾아봐야함. useState? 
-function changeEditMode(id) {
-  app.dispatch({
-    type: "EDITE_TODO_STATE",
-    payload: {
-      id
-    }
-  })
-}
-
-todoInput.addEventListener("keypress",(e) => {addTodo2(e)});
 
 const APIURL = "https://js-todo-list-9ca3a.df.r.appspot.com";
-
-let userList = "";
 
 fetch(`${APIURL}/api/users`)
   .then(data => {
