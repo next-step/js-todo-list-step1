33ae8cebde6e17c4cc8618722d32b75439c8274b
김민성 <46954944+minsung03@users.noreply.github.com>
2020-11-19 21:43:41 +0900

    [4기 minsung03] TodoList with CRUD (#51)
    * refactor : add todo item function
    
    * refactor add script tag
    
    * feat: 수정하기 거의 다 완성되가는중
    
    * refactor : 코드 리펙토링
    
    * refactor : change event and variable names refactoring
    
    * chore: gitignore 추가
    
    * feat: count 기능 작업 완료
    
    * feat: 솔루션 코드 추가

---
+++ js/app.origin.js
@@ -0,0 +1,64 @@
+const todoAppender = document.getElementById("new-todo-title");
+const todoList   = document.getElementById("todo-list");
+const todoCount = document.querySelector(".todo-count strong");
+
+todoAppender.addEventListener("keydown", ({ key, target }) => {
+	
+	if(key !== "Enter" || target.value.trim().length === 0) return;
+	const lastIndex = Math.max(0, ...[ ...todoList.querySelectorAll('[data-index]') ].map(v => v.dataset.index).map(Number)) + 1;
+	todoList.insertAdjacentHTML('beforeend', `
+		<li data-index="${lastIndex}">
+			<div class="view">
+				<input class="toggle" type="checkbox"/>
+				<label class="label">${target.value}</label>
+				<button class="destroy"></button>
+			</div>
+			<input class="edit" value="${target.value}" />
+		</li>
+	`.trim());
+	
+	target.value = "";
+
+	let count = 0;
+	
+	count = lastIndex;
+
+	todoCount.innerHTML = count++;
+
+})
+
+todoList.addEventListener('click', ({ target }) => {
+	if (!target.classList.contains('destroy')) return;
+	const todoItem = target.closest('[data-index]');
+	todoItem.remove();
+	
+})
+
+todoList.addEventListener('change', ({ target }) => {
+	if (!target.classList.contains('toggle')) return;
+	const todoItem = target.closest('[data-index]');
+	if (target.checked){
+		return todoItem.setAttribute("class", "completed");
+	}
+	todoItem.removeAttribute("class");
+})
+
+todoList.addEventListener("dblclick", ({ target }) => {
+	if (!target.classList.contains('label')) return;
+	target.closest('li').setAttribute("class", "editing");
+
+})
+
+todoList.addEventListener("keyup", ({ key, target }) => {
+	if (!target.classList.contains('edit')) return;
+	const todoItem = target.closest('[data-index]');
+	if (key === "Enter") {
+		todoItem.querySelector('.label').innerHTML = target.value;
+	}
+	if (['Enter', 'Escape'].includes(key)){
+		target.value = "";
+		todoItem.classList.remove("editing");
+	}
+
+})
+
