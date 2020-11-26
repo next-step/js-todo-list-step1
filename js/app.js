204c0d40fb435a7d8fba19f84c2a774f85796ad3
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2020-11-26 23:47:30 +0900

    feat : complete 기능 구현

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
-const TODO_TEMPLATE = title => `
-<li>
+const TODO_TEMPLATE = ({ id, text, completed }) => `
+<li class="${completed ? "completed" : ""}" id=${id}>
   <div class="view">
-    <input class="toggle" type="checkbox"/>
-    <label class="label">${ title }</label>
+    <input class="toggle" type="checkbox" ${completed ? "checked" : ""}/>
+    <label class="label">${text}</label>
     <button class="destroy"></button>
   </div>
-  <input class="edit" value="${ title }" />
+  <input class="edit" value="${text}" />
 </li>
-`
+`;
 
 function app() {
   const todos = [];
+  let id = 0;
 
   const $todoApp = document.querySelector(".todoapp");
   const $input = $todoApp.querySelector(".new-todo");
@@ -18,14 +19,29 @@ function app() {
 
   const SUBMIT_KEY = "Enter";
 
-  const addTodo = todo => {
-    todos.push(todo);
+  const handleTodoToggle = (e) => {
+    if (!e.target.classList.contains("toggle")) {
+      return;
+    }
 
-    const newTodoItem = TODO_TEMPLATE(todo);
-    $list.insertAdjacentHTML("beforeend", newTodoItem);
+    const todoId = e.target.closest("li").id;
+    const targetTodo = todos.find((todo) => todo.id === parseInt(todoId));
+
+    targetTodo.completed = !targetTodo.completed;
+    renderTodo();
   };
 
-  const handleTodoSubmit = e => {
+  const todoObj = (todo) => {
+    return { id: id++, text: todo, completed: false };
+  };
+
+  const addTodo = (todo) => {
+    const newTodo = todoObj(todo);
+    todos.push(newTodo);
+    renderTodo();
+  };
+
+  const handleTodoSubmit = (e) => {
     const todo = $input.value;
     if (e.key === SUBMIT_KEY) {
       addTodo(todo);
@@ -33,6 +49,12 @@ function app() {
     }
   };
 
+  const renderTodo = () => {
+    const allTodo = todos.map(TODO_TEMPLATE).join("");
+    $list.innerHTML = allTodo;
+  };
+
   $todoApp.addEventListener("keypress", handleTodoSubmit);
+  $list.addEventListener("click", handleTodoToggle);
 }
 new app();
