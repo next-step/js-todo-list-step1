73d8d858853a22cff4820deb9fd21e53077f6a5f
hwookim <gusdn9754@gmail.com>
2020-11-26 22:28:02 +0900

    todoItem을 키보드로 입력하여 추가하기

--- js/app.js
+++ js/app.js
@@ -1,15 +1,35 @@
+const TODO_TEMPLATE = title => `
+<li>
+  <div class="view">
+    <input class="toggle" type="checkbox"/>
+    <label class="label">${ title }</label>
+    <button class="destroy"></button>
+  </div>
+  <input class="edit" value="${ title }" />
+</li>
+`
+
 function app() {
+  const todos = [];
+
   const $todoApp = document.querySelector(".todoapp");
   const $input = $todoApp.querySelector(".new-todo");
+  const $list = $todoApp.querySelector(".todo-list");
 
   const SUBMIT_KEY = "Enter";
 
-  const addTodo = todo => {};
+  const addTodo = todo => {
+    todos.push(todo);
+
+    const newTodoItem = TODO_TEMPLATE(todo);
+    $list.insertAdjacentHTML("beforeend", newTodoItem);
+  };
 
   const handleTodoSubmit = e => {
     const todo = $input.value;
-    if (e.key === SUMBIT_KEY) {
+    if (e.key === SUBMIT_KEY) {
       addTodo(todo);
+      $input.value = "";
     }
   };
 
