30d4f68c8e136acb855bbf7388015638e02825d8
hwookim <gusdn9754@gmail.com>
2020-11-29 20:30:44 +0900

    refactor: storage.initStorage() 삭제

--- js/storage.js
+++ js/storage.js
@@ -1,25 +1,14 @@
 import { STORAGE_NAME } from "./constants.js";
 
 const storage = (() => {
-  const store = [];
-
-  const initStorage = () => {
-    const stored = localStorage.getItem(STORAGE_NAME);
-    if (!stored) return;
-
-    const storedItems = JSON.parse(stored);
-    storedItems.forEach((item) => store.push(item));
-  };
-
   const getStorage = () => {
-    return store;
+    const storedItems = localStorage.getItem(STORAGE_NAME);
+    return JSON.parse(storedItems);
   };
 
   const setStorage = (todos) => {
     localStorage.setItem(STORAGE_NAME, JSON.stringify(todos));
   };
-
-  initStorage();
 
   return {
     getStorage,
