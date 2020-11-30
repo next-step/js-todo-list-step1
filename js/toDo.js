eb4d2eeccf183de1cb75c66e596377005b31531c
lee pro <leepro225@gmail.com>
2020-11-30 23:00:08 +0900

    store 및 로컬스토리지 CRUD 기능 추가

--- js/toDo.js
+++ js/toDo.js
@@ -1,4 +1,7 @@
-let count = 0;
+const store = {
+    filtered: 'all',
+    list : []
+}
 
 // target element
 const userInputArea = document.getElementById('new-todo-title');
@@ -20,14 +23,24 @@ userInputArea.addEventListener('keydown', function(e) {
 toDoUlElement.addEventListener('click', function(e) {
     const type = e.target.type;
     if (type === 'checkbox') {
-        // store에 값 저장하기
-        e.target.closest('li').classList.toggle('completed')
+        if (e.target.checked) {
+            e.target.closest('li').classList.add('completed');
+            store.list.map(value => {if (value.id === Number(e.target.closest('li').id)) {value.status = 'completed';}});
+        } else {
+            e.target.closest('li').classList.remove('completed');
+            store.list.map(value => {if (value.id === Number(e.target.closest('li').id)) {value.status = '';}});
+        }
+        showSelected(store.filtered);
     } else if (type === 'submit') {
-        // store에 값 삭제하기
         this.removeChild(e.target.closest('li'));
-        count -= 1;
-        renewalCount();
+        store.list.map((value, idx) =>{
+            if (value.id === Number(e.target.closest('li').id)) {
+                store.list.splice(idx, 1);
+            }
+        });
     }
+    localStorage.setItem('list', JSON.stringify(store.list));
+    renewalCount(store.filtered);
 });
 toDoUlElement.addEventListener('dblclick', function(e) {
     if (e.target.tagName === 'LABEL') {
@@ -37,9 +50,14 @@ toDoUlElement.addEventListener('dblclick', function(e) {
 });
 toDoUlElement.addEventListener('keydown', function(e) {
     if (e.key === 'Enter') {
-        // 값을 저장하기
+        if (e.target.value.replace(/\s| /gi, "").length ===0) {
+        clearInput(e);   
+        return
+       }
         e.target.closest('li').classList.remove('editing');
         e.target.closest('li').querySelector('label').innerHTML = e.target.value;
+        store.list.map(value => {if (value.id === Number(e.target.closest('li').id)) {value.value = e.target.value;}});
+        localStorage.setItem('list', JSON.stringify(store.list));
     } else if (e.key === 'Escape') {
         e.target.closest('li').classList.remove('editing');
     }
@@ -51,16 +69,19 @@ filterUlElement.addEventListener('click', function(e) {
     }
     e.target.classList.add('selected');
     if (e.target.classList.value.includes('active')) {
-        showSelected('active')
+        showSelected('active');
+        store.filtered = 'active';
     } else if (e.target.classList.value.includes('completed')) {
-        showSelected('completed')
+        showSelected('completed');
+        store.filtered = 'completed';
     } else if (e.target.classList.value.includes('all')) {
-        showSelected('all')
+        showSelected('all');
+        store.filtered = 'all';
     }
 });
 
 // view
-const toDoListLi = ({value, addClass}) => {
+const toDoListLi = ({id ,value, status}) => {
         const liElement = document.createElement('li');
         const divElement = document.createElement('div');
         const inputElement = document.createElement('input');
@@ -68,8 +89,11 @@ const toDoListLi = ({value, addClass}) => {
         const buttonElement = document.createElement('button');
         const editInputElement = document.createElement('input');
 
-        if (addClass !== '') {
-            liElement.setAttribute('class', addClass);
+        if (status === 'completed') {
+            liElement.setAttribute('class', status);
+            inputElement.setAttribute('checked', 'checked');
+        } else if (status === 'hidden') {
+            liElement.setAttribute('class', status);
         }
         divElement.setAttribute('class', 'view');
         inputElement.setAttribute('class', 'toggle');
@@ -77,6 +101,7 @@ const toDoListLi = ({value, addClass}) => {
         labelElement.setAttribute('class', 'label');
         buttonElement.setAttribute('class', 'destroy');
         editInputElement.setAttribute('class', 'edit');
+        liElement.setAttribute('id', id);
 
         divElement.appendChild(inputElement);
         divElement.appendChild(labelElement);
@@ -90,49 +115,67 @@ const toDoListLi = ({value, addClass}) => {
 
 // methods
 const createToDoItem = (value) => {
-    let addClass = '';
+    let status = '';
     const filterChildren = filterUlElement.children[2].children[0].className;
-    if (filterChildren.indexOf('selected') !== -1) {addClass = 'hidden'}
-    toDoUlElement.appendChild(toDoListLi({value, addClass}));
-    count += 1;
-    renewalCount();
+    if (filterChildren.indexOf('selected') !== -1) {status = 'hidden'}
+    const id = +new Date();
+    toDoUlElement.appendChild(toDoListLi({id, value, status}));
+    store.list.push({id, status: '', value});
+    localStorage.setItem('list', JSON.stringify(store.list));
+    renewalCount(store.filtered);
 }
 
 const clearInput = (e) => {
     e.target.value = '';
 }
 const renewalCount = (param) => {
-    toDoCount.innerText = param;
+    if (param === 'all') {
+        toDoCount.innerText = store.list.length;
+    } else if (param === 'completed') {
+        toDoCount.innerText = store.list.filter(value => {return value.status === param}).length;
+    } else if (param === 'active'){
+        toDoCount.innerText = store.list.filter(value => {return value.status !== 'completed'}).length;
+    }
+    
 }
 const showSelected = (param) => {
     const toDoItems = toDoUlElement.children;
-    let selectedCount = 0;
     if (param === 'completed') {
         for (let i = 0; toDoItems.length > i; i++) {
             if (toDoItems[i].className.includes(param)) {
                 toDoItems[i].classList.remove('hidden');
-                console.log(i, '완료된 것')
-                selectedCount += 1;
             } else {
             toDoItems[i].classList.add('hidden');
             }
-            renewalCount(selectedCount);
         }
+        renewalCount('completed');
     } else if (param === 'active') {
         for (let i = 0; toDoUlElement.children.length > i; i++) {
         if (toDoItems[i].className.includes('completed')) {
             toDoItems[i].classList.add('hidden');
             } else {
             toDoItems[i].classList.remove('hidden');
-            selectedCount += 1;
             }
         }
-        renewalCount(selectedCount);
+        renewalCount('active');
     } else {
         for (let i = 0; toDoItems.length > i; i++) {
             toDoItems[i].classList.remove('hidden');
         }
-        renewalCount(count);
+        renewalCount('all');
     }
 }
 
+
+window.onload = () => {
+    const list  = localStorage.getItem('list');
+    if (list === null) {return}
+    const listArr = JSON.parse(list);
+    if (listArr.length> 0) {
+        for (let i = 0; listArr.length > i; i++) {
+            store.list.push(listArr[i]);
+            toDoUlElement.appendChild(toDoListLi({id: listArr[i].id, value : listArr[i].value, status : listArr[i].status}));
+        }
+    }
+    renewalCount('all')
+}
