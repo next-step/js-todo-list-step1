9816d31ae1c75474e2f1b5a0e55e14369cbf745c
genie <wlsgml5628@gmail.com>
2020-11-30 22:28:50 +0900

    todo-item 필터 기능 추가 & 리팩토링

--- js/TodoList.js
+++ js/TodoList.js
@@ -1,7 +1,5 @@
-const $todoTitle = document.getElementById('new-todo-title');
-const $todoList = document.getElementById('todo-list');
-
-$todoTitle.addEventListener("keyup", onAddTodoItem);
+// const $todoList = document.getElementById('todo-list');
+// const $todoFilter = document.querySelector('.filters');
 
 const ENTER_KEY = "Enter";
 const ESC_KEY = "Escape";
@@ -19,6 +17,7 @@ function removeTodoItem(event) {
     const target = event.target;
     const list = target.parentElement.parentElement;
     $todoList.removeChild(list);
+    updateCount();
   }else{
       return;
   }
@@ -49,37 +48,5 @@ function closeEditor(event) {
   } else if (event.key == ESC_KEY) {
     list.classList.remove('editing');
     list.removeChild(target);
-  }
-}
-
-function onAddTodoItem(event) {
-  const todoTitle = event.target.value;
-  if(event.key === ENTER_KEY && todoTitle !== "") {
-    // enter 키 입력 시 리스트 추가
-    const div = document.createElement('div');
-    div.className = "view";
-
-    const list = document.createElement('li');
-    const input = document.createElement('input');
-    input.className = "toggle";
-    input.type = "checkbox";
-    input.addEventListener("click", toggleItem);
-
-    const label = document.createElement('label');
-    label.className = "label";
-    label.innerText = todoTitle;
-    label.addEventListener("dblclick", showEditor); // todo list를 더블클릭했을 때 input 모드로 변경
-    
-    const button = document.createElement('button');
-    button.className = "destroy";
-    button.addEventListener("click", removeTodoItem);
-
-    div.appendChild(input);
-    div.appendChild(label);
-    div.appendChild(button);
-    list.appendChild(div);
-    $todoList.appendChild(list);
-
-    event.target.value = "";
   }
 }
