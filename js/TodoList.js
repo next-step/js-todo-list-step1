25464e4bee5a67ee12a267727cfa6beb77fa7227
genie <wlsgml5628@gmail.com>
2020-12-01 00:59:49 +0900

    localStorage 저장 기능 추가

--- js/TodoList.js
+++ js/TodoList.js
@@ -1,15 +1,17 @@
 // const $todoList = document.getElementById('todo-list');
 // const $todoFilter = document.querySelector('.filters');
-
+const todos = [];
 const ENTER_KEY = "Enter";
 const ESC_KEY = "Escape";
 
 function toggleItem(event) {
+  //filterTodos(event);
   const target = event.target;
   const list = target.parentElement.parentElement;
   list.classList.toggle('completed');
   event.target.removeEventListener("dblclick", showEditor);
-  target.classList.toggle('checked');  
+  target.classList.toggle('checked');
+  
 }
 
 function removeTodoItem(event) {
