e047a7d9348777b21b8b9188e6d6b164e128d607
realhee <wlsgml5628@gmail.com>
2020-12-01 10:48:11 +0900

    editing 기능 & todo-item 삭제 전 alert 기능 추가(BOM)

--- js/TodoList.js
+++ js/TodoList.js
@@ -4,19 +4,53 @@ const $todoList = document.getElementById('todo-list');
 $todoTitle.addEventListener("keyup", onAddTodoItem);
 
 const ENTER_KEY = "Enter";
+const ESC_KEY = "Escape";
 
 function toggleItem(event) {
     const target = event.target;
     const list = target.parentElement.parentElement;
     list.classList.toggle('completed');
+    event.target.removeEventListener("dblclick", showEditor);
     target.classList.toggle('checked');  
   }
   
-  function removeTodoItem(event) {
+function removeTodoItem(event) {
+if (confirm("정말 삭제하시겠습니까?")){
     const target = event.target;
     const list = target.parentElement.parentElement;
     $todoList.removeChild(list);
-  }
+    }else{
+        return;
+    }
+}
+    
+function showEditor(event) {
+    const div = event.target.parentElement;
+    const list = div.parentElement;
+    list.classList.add('editing');
+
+    // editor 생성
+    const todoInput = document.createElement('input');
+    todoInput.className = "edit";
+    todoInput.value = list.textContent;
+    list.appendChild(todoInput);
+
+    todoInput.addEventListener("keyup", closeEditor);
+}
+
+function closeEditor(event) {
+    const target = event.target; // edit
+    const list = target.parentElement; // editing
+
+    if (event.key === ENTER_KEY) {
+    list.querySelector('label').textContent = target.value;
+    list.classList.remove('editing');
+    list.removeChild(target);
+    } else if (event.key == ESC_KEY) {
+    list.classList.remove('editing');
+    list.removeChild(target);
+    }
+}
   
 function onAddTodoItem(event) {
   const todoTitle = event.target.value;
@@ -35,6 +69,8 @@ function onAddTodoItem(event) {
     const label = document.createElement('label');
     label.className = "label";
     label.innerText = todoTitle;
+
+    label.addEventListener("dblclick", showEditor); // todo list를 더블클릭했을 때 input 모드로 변경
 
     const button = document.createElement('button');
     button.className = "destroy";
