8d736523c9f1c7c8ecaccdad40c919abd6431cd9
seojihwan <seojihwan213@naver.com>
2020-12-02 09:38:58 +0900

    refactor: TodoList 컴포넌트로 바꾸기

---
+++ src/components/TodoList.js
@@ -0,0 +1,28 @@
+export const TodoList = ({ title, isCompleted, editing, idx }) => {
+  const li = document.createElement('li');
+  const view = document.createElement('div');
+  const toggle = document.createElement('input');
+  const label = document.createElement('label');
+  const destroy = document.createElement('button');
+  const edit = document.createElement('input');
+
+  view.classList.add('view');
+  toggle.classList.add('toggle');
+  label.classList.add('label');
+  destroy.classList.add('destroy');
+  edit.classList.add('edit');
+  toggle.setAttribute('type', 'checkbox');
+  label.innerText = title;
+  edit.setAttribute('value', title);
+  li.setAttribute('key', idx);
+  if (isCompleted) {
+    li.classList.add('completed');
+    toggle.setAttribute('checked', true);
+  }
+  if (editing) {
+    li.classList.add('editing');
+  }
+  view.append(toggle, label, destroy);
+  li.append(view, edit);
+  return li;
+};
