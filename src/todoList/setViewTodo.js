49d97da0aa2ac09ac14712cb6cde3da9011b4962
yujo <yujo@student.42seoul.kr>
2020-12-05 16:43:32 +0900

    feat: todo list의 하단 버튼 클릭을 통해 상태에 맞는 아이템만 보이게 하는 기능 구현

--- src/todoList/setViewTodo.js
+++ src/todoList/setViewTodo.js
@@ -1,3 +1,45 @@
+import { countTodoItem } from './countTodoItem.js';
+
+const viewAll = () => {
+  document
+    .querySelectorAll('.todo-list > li')
+    .forEach((li) => (li.style.display = 'inline'));
+};
+
+const viewActive = () => {
+  document.querySelectorAll('.todo-list > li').forEach((li) => {
+    if (li.className === '') {
+      li.style.display = 'inline';
+    }
+  });
+};
+
+const viewCompleted = () => {
+  document.querySelectorAll('.todo-list > li').forEach((li) => {
+    if (li.className === 'completed') {
+      li.style.display = 'inline';
+    }
+  });
+};
+
+const resetScreen = () => {
+  document.querySelectorAll('a').forEach((a) => a.classList.remove('selected'));
+  document
+    .querySelectorAll('.todo-list > li')
+    .forEach((li) => (li.style.display = 'none'));
+};
+
 export const setViewTodo = (target) => {
-  console.log(target);
+  resetScreen();
+  if (target.className === 'all') {
+    viewAll();
+  }
+  if (target.className === 'active') {
+    viewActive();
+  }
+  if (target.className === 'completed') {
+    viewCompleted();
+  }
+  countTodoItem();
+  target.classList.add('selected');
 };
