45bdf7e4696069d603f8765f791957eb1695d588
sudalqueen <abbey17@cnu.ac.kr>
2020-12-05 19:23:09 +0900

    modify: TodoList의 li에 class 추가 및 css 수정

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,6 @@
 export default function TodoItem(todo) {
     return `
-        <li data-id="${todo.id}" ${todo.editing ? 'editing' : ''}>
+        <li data-id="${todo.id}" class=${todo.editing ? 'editing' : ''} ${todo.complete ? 'completed' : ''}>
                 <input type="checkbox" 
                         ${todo.complete ? 'checked' : ''}
                         data-action="toggle"/>
