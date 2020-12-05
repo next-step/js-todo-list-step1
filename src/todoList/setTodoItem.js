570bd2fc2985085c709ebf17d2b1ef62db45d765
yujo <yujo@student.42seoul.kr>
2020-12-05 15:43:26 +0900

    refactor: 구현 요구사항에 맞게 input 태그에 checked속성 추가

--- src/todoList/setTodoItem.js
+++ src/todoList/setTodoItem.js
@@ -1,10 +1,16 @@
 export const changCompletedTodoItem = (todoItem) => {
+  const $checkbox = todoItem.querySelector('input');
+
   if (todoItem.className === '') {
+    $checkbox.setAttribute('checked', true);
     return todoItem.setAttribute('class', 'completed');
   }
+  $checkbox.removeAttribute('checked');
   return todoItem.removeAttribute('class');
 };
 
 export const removeTodoItem = (todoItem) => {
   return todoItem.remove();
 };
+
+//, input 태그에 checked 속성 추가
