ada606585cc7ac34cc3898e3bf32e355e441c8ec
yujo <yujo@student.42seoul.kr>
2020-12-06 15:36:00 +0900

    refactor: 가독성을 위해 import 구문과 함수 사이 개행 추가

--- src/event/triggerKeyupEvent.js
+++ src/event/triggerKeyupEvent.js
@@ -1,4 +1,5 @@
 import { addTodoItem } from '../todoList/addTodoItem.js';
+
 export const triggerKeyupEvent = ({ key, target }) => {
   if (key === 'Enter' && target.value.length) {
     addTodoItem(target.value);
