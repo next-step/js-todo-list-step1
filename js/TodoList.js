58c66e8dc5734d975694c56e0a018c729ba76aa5
genie <wlsgml5628@gmail.com>
2021-01-19 20:10:51 +0900

    [5기 realhee] TodoList with CRUD (#85)
    * [1주차] todo list에 아이템 추가
    
    * todo-item 토글 기능 추가
    
    * todo-item 삭제 기능 추가
    
    * editing 기능 & todo-item 삭제 전 alert 기능 추가(BOM)
    
    * count 기능 추가 & 리팩토링
    
    * todo-item filtering 기능 추가
    
    * localStorage 저장 기능 추가
    
    * markdown 수정
    
    * localStorage 삭제 기능 & 중복 작업 추가 시 alert 노출

---
+++ js/TodoList.js
@@ -0,0 +1,61 @@
+// const $todoList = document.getElementById('todo-list');
+// const $todoFilter = document.querySelector('.filters');
+
+const ENTER_KEY = "Enter";
+const ESC_KEY = "Escape";
+
+function toggleItem(event) {
+    //filterTodos(event);
+    const target = event.target;
+    const list = target.parentElement.parentElement;
+    list.classList.toggle('completed');
+    event.target.removeEventListener("dblclick", showEditor);
+    target.classList.toggle('checked');
+}
+  
+function removeTodoItem(event) {
+    if (confirm("정말 삭제하시겠습니까?")){
+      const target = event.target;
+      const list = target.parentElement.parentElement;
+      $todoList.removeChild(list);
+
+      // remove item from localStorage    
+      const cleanTodos = todos.filter(function(toDo) {
+        return toDo.label !== list.textContent;
+      });
+      todos = cleanTodos;
+      localStorage.setItem(TODO_LS,JSON.stringify(todos));
+
+      updateCount();
+    }else{
+        return;
+    }
+}
+    
+function showEditor(event) {
+    const div = event.target.parentElement;
+    const list = div.parentElement;
+    list.classList.add('editing');
+
+    // editor 생성
+    const todoInput = document.createElement('input');
+    todoInput.className = "edit";
+    todoInput.value = list.textContent;
+    list.appendChild(todoInput);
+
+    todoInput.addEventListener("keyup", closeEditor);
+}
+
+function closeEditor(event) {
+    const target = event.target; // edit
+    const list = target.parentElement; // editing
+
+    if (event.key === ENTER_KEY) {
+    list.querySelector('label').textContent = target.value;
+    list.classList.remove('editing');
+    list.removeChild(target);
+    } else if (event.key == ESC_KEY) {
+    list.classList.remove('editing');
+    list.removeChild(target);
+    }
+}
