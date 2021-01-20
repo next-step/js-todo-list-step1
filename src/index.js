e202d39a2fcf7e24e5611c37396a499c8ffbd0f5
yujo <yujo@student.42seoul.kr>
2021-01-20 20:51:39 +0900

    feat: 새로운 todo를 추가하는 기능

--- src/index.js
+++ src/index.js
@@ -1,1 +1,73 @@
-console.log('tst');
+import { ALL, ACTIVE, COMPLETED } from './constant/state.js';
+
+export default class App {
+  constructor() {
+    this.todos = [
+      { id: String(Date.now()), title: 'dummy data', complete: false },
+    ];
+    this.selected = ALL;
+    this.$todoList = document.querySelector('#todo-list');
+    this.$newTodoTitle = document.querySelector('#new-todo-title');
+
+    this.$newTodoTitle.addEventListener('keyup', this.addTodo);
+    this.$todoList.addEventListener('dblclick', this.editTodo);
+    this.$todoList.addEventListener('click', this.changeTodo);
+  }
+
+  todoTemplate = (todo) => {
+    return `<li id=${todo.id} class=${todo.completed && 'completed'} >
+                <div class="view">
+                    <input class="toggle" type="checkbox" id=${todo.id} />
+                    <label class="label">${todo.title}</label>
+                    <button class="destroy" id=${todo.id}></button>
+                </div>
+                <input class="edit" value="${todo.title}" />
+            </li>`;
+  };
+
+  loadTodo = () => {
+    console.log('init');
+    this.$todoList.innerHTML = '';
+    this.todos.forEach((todo) => {
+      this.$todoList.insertAdjacentHTML('beforeend', this.todoTemplate(todo));
+    });
+  };
+
+  addTodo = ({ target, key }) => {
+    if (key === 'Enter' && target.value) {
+      this.todos.push({
+        id: String(Date.now()),
+        title: target.value,
+        completed: false,
+      });
+      target.value = '';
+      this.loadTodo();
+    }
+  };
+
+  editTodo = ({ target }) => {
+    if (target.className === 'label') {
+      console.log(target.closest('li'));
+      target.closest('li').classList.add('editing');
+    }
+  };
+
+  changeTodo = ({ target }) => {
+    if (target.className === 'toggle') {
+      this.todos.map((todo) => {
+        if (todo.id === target.id) {
+          todo.completed = !todo.completed;
+        }
+      });
+      console.log(this.todos);
+
+      this.loadTodo();
+    } else if (target.className === 'destroy') {
+      console.log('삭제');
+    }
+  };
+}
+
+window.onload = () => {
+  new App().loadTodo();
+};
