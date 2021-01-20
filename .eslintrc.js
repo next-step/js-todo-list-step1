b8a7f74ccb03b6fb414e28c8f68ec8dcf9acffa8
hwookim <gusdn9754@gmail.com>
2021-01-20 15:49:07 +0900

    chore: npm, eslint, prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    semi: "error",
+  },
+};
