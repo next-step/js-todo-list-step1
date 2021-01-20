aa52f754905c3bad1f23502711dfc76ba70767f0
SoHyun <pje1740@gmail.com>
2021-01-20 22:03:37 +0900

    [Feat] :zap:️ add todoItem edit event #6
    더블클릭 시 할일 수정창으로 교체
    엔터로 수정 확정, esc로 취소

--- scripts/components/todoList.js
+++ scripts/components/todoList.js
@@ -8,6 +8,7 @@ class TodoList {
   init() {
     this.addEventCheckToggle();
     this.addEventDeleteTodoItem();
+    this.addEventEditTodoItem();
   }
 
   addNewTodoItem(taskTitle) {
@@ -16,8 +17,10 @@ class TodoList {
 
   addEventCheckToggle() {
     this.$todoList.addEventListener('change', (e) => {
-      const closestLi = e.target.closest('li');
-      closestLi.classList.toggle('completed');
+      if (e.target.type === 'checkbox') {
+        const closestLi = e.target.closest('li');
+        closestLi.classList.toggle('completed');
+      }
     });
   }
 
@@ -26,6 +29,37 @@ class TodoList {
       if (e.target.classList.contains('destroy')) {
         const closestLi = e.target.closest('li');
         closestLi.remove();
+      }
+    });
+  }
+
+  addEventEditTodoItem() {
+    this.$todoList.addEventListener('dblclick', (e) => {
+      const closestLi = e.target.closest('li');
+      const inputEdit = closestLi.querySelector('input.edit');
+      inputEdit.style.display = 'block';
+      inputEdit.focus();
+      // 캐럿 위치 끝으로 이동
+      inputEdit.setSelectionRange(inputEdit.value.length, inputEdit.value.length);
+      closestLi.querySelector('div.view').style.display = 'none';
+    });
+
+    this.$todoList.addEventListener('keyup', (e) => {
+      const closestLi = e.target.closest('li');
+
+      function switchBackToView() {
+        closestLi.querySelector('div.view').style.display = 'block';
+        closestLi.querySelector('input.edit').style.display = 'none';
+      }
+
+      if (e.target.classList.contains('edit')) {
+        if (e.key === 'Enter' && e.target.value.trim() !== '') {
+          switchBackToView();
+          closestLi.querySelector('label').innerHTML = e.target.value;
+        } else if (e.key === 'Escape') {
+          switchBackToView();
+          e.target.value = closestLi.querySelector('label').innerHTML;
+        }
       }
     });
   }
