7abcecaf296d88d672eeb61ffe1fc039abe40fa8
SoHyun <pje1740@gmail.com>
2021-01-20 21:00:14 +0900

    [Feat] :zap:️ add event to toggle class completed #4
    체크박스 클릭 시 completed class가 toggle 되도록 이벤트 추가

--- scripts/components/todoList.js
+++ scripts/components/todoList.js
@@ -5,11 +5,23 @@ class TodoList {
     this.$todoList = document.querySelector('#todo-list');
   }
 
+  init() {
+    this.addEventCheckToggle();
+  }
+
   addNewTodoItem(taskTitle) {
     this.$todoList.innerHTML += new TodoItem(taskTitle).render();
+  }
+
+  addEventCheckToggle() {
+    this.$todoList.addEventListener('change', (e) => {
+      const closestLi = e.target.closest('li');
+      closestLi.classList.toggle('completed');
+    });
   }
 }
 
 const $todoList = new TodoList();
+$todoList.init();
 
 export default $todoList;
