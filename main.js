4349a1a9bd8a7d5b5ffff50dfa96b53175aef100
jjunyjjuny <jjuny546@gmail.com>
2021-01-21 22:22:13 +0900

    Add checkout event

--- main.js
+++ main.js
@@ -1,41 +1,44 @@
-const test = "test";
-const $ul = document.querySelector('#todo-list');
+const $ul = document.querySelector("#todo-list");
 
-const $input = document.querySelector('#new-todo-title');
-$input.addEventListener('keypress', (e) => {
-    console.log(e.keyCode)
-    if (e.keyCode === 13) {
-        console.log('keycode')
-        const inputVal = e.target.value;
-        $ul.appendChild(createTodoItem(inputVal));
-    }
-})
+const $input = document.querySelector("#new-todo-title");
+$input.addEventListener("keypress", (e) => {
+  if (e.keyCode === 13) {
+    const inputVal = e.target.value;
+    $ul.appendChild(createTodoItem(inputVal));
+  }
+});
 
 function createTodoItem(inputVal) {
-    const $li = document.createElement('li');
-    const $div = document.createElement('div');
-    const $input = document.createElement('input');
-    const $label = document.createElement('label');
-    const $button = document.createElement('button');
+  const $li = document.createElement("li");
+  const $div = document.createElement("div");
+  const $input = document.createElement("input");
+  const $label = document.createElement("label");
+  const $button = document.createElement("button");
 
-    const $inputOther = document.createElement('input');
+  const $inputOther = document.createElement("input");
+  $inputOther.classList.add("edit");
 
+  $input.classList.add("toggle");
+  $input.addEventListener("click", () => {
+    $li.classList.toggle("completed");
+    $input.classList.toggle("checked");
+  });
 
-    $label.classList.add('label');
-    $button.classList.add('destroy');
-    $div.classList.add('view');
+  $label.classList.add("label");
+  $label.innerText = inputVal;
 
-    $input.type = 'checkbox';
+  $button.classList.add("destroy");
+  $div.classList.add("view");
 
+  $input.type = "checkbox";
 
-    $div.appendChild($input);
-    $div.appendChild($label);
-    $div.appendChild($button);
+  $div.appendChild($input);
+  $div.appendChild($label);
+  $div.appendChild($button);
 
-    $li.appendChild($div);
+  $li.appendChild($div);
 
-    $li.appendChild($inputOther);
+  $li.appendChild($inputOther);
 
-    return $li;
+  return $li;
 }
-
