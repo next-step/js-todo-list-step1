e352d62923d58886743856552dbf012b1eaabf96
skid901 <tlrudejr1993@naver.com>
2021-01-22 21:29:26 +0900

    feat: add event for adding item

--- js/TodoInput.js
+++ js/TodoInput.js
@@ -1,1 +1,10 @@
-export default function TodoInput(inputEl, todoApp) {}
+export default function TodoInput(inputEl, todoApp) {
+  inputEl.addEventListener("keypress", (event) => {
+    const value = inputEl.value.trimEnd();
+    if (event.code === "Enter" && value) {
+      inputEl.dispatchEvent(new CompositionEvent("compositionend"));
+      todoApp.addItem(value);
+      inputEl.value = "";
+    }
+  });
+}
