f8ac1c54286e7d1374ed5c91bf3b1e1f9c64206a
yujo <yujo@student.42seoul.kr>
2021-01-22 11:48:51 +0900

    refactor: 아이템을 수정하는 모듈 함수 분리

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -1,33 +1,35 @@
 export default class EditTodo {
   constructor($todoList, loadTodo) {
-    $todoList.addEventListener('dblclick', ({ target }) => {
-      this.editTodo({ target }, loadTodo);
-    });
+    this.loadTodo = loadTodo;
+
+    $todoList.addEventListener('dblclick', this.editTodo);
   }
 
-  editTodo = ({ target }, loadTodo) => {
-    this.todos = JSON.parse(localStorage.getItem('todos'));
-    const updatedTodoItem = (id, value) => {
-      this.todos.map((todo) => {
-        if (todo.id === id) {
-          todo.title = value;
-        }
-      });
-      localStorage.setItem('todos', JSON.stringify(this.todos));
-      loadTodo();
-    };
-
-    const edit = ({ target, key }) => {
-      if (key === 'Enter') {
-        updatedTodoItem(target.closest('li').id, target.value);
-      } else if (key === 'Escape') {
-        target.closest('li').classList.remove('editing');
+  updateTodoItem = (listId, value) => {
+    this.todos.map((todo) => {
+      if (todo.id === listId) {
+        todo.title = value;
       }
-    };
+    });
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+    this.loadTodo();
+  };
+
+  editTodoItem = ({ target, key }) => {
+    switch (key) {
+      case 'Enter':
+        return this.updateTodoItem(target.closest('li').id, target.value);
+      case 'Escape':
+        return target.closest('li').classList.remove('editing');
+    }
+  };
+
+  editTodo = ({ target }) => {
+    this.todos = JSON.parse(localStorage.getItem('todos'));
 
     if (target.className === 'label') {
       target.closest('li').classList.add('editing');
-      target.closest('li').addEventListener('keyup', edit);
+      target.closest('li').addEventListener('keyup', this.editTodoItem);
     }
   };
 }
