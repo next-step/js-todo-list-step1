79bde34d1ea80c13acd131b97cf98bf7880594c3
skid901 <tlrudejr1993@naver.com>
2021-01-23 03:40:45 +0900

    feat: add event for deleting item

--- js/TodoInput.js
+++ js/TodoInput.js
@@ -1,4 +1,6 @@
 export default function TodoInput(inputEl, todoApp) {
+  this.focus = () => inputEl.focus();
+
   inputEl.addEventListener("keypress", (event) => {
     const value = inputEl.value.trimEnd();
     if (event.code === "Enter" && value) {
--- js/TodoList.js
+++ js/TodoList.js
@@ -1,6 +1,6 @@
 const renderTodoItem = ({ id, value, completed }) => `
-  <li class="${completed ? "completed" : ""}">
-    <div class="view" data-item-id="${id}">
+  <li class="${completed ? "completed" : ""}" data-id="${id}">
+    <div class="view">
       <input class="toggle" type="checkbox" ${completed ? "checked" : ""}>
       <label class="label">${value}</label>
       <button class="destroy"></button>
@@ -9,7 +9,19 @@ const renderTodoItem = ({ id, value, completed }) => `
 `;
 
 export default function TodoList(listEl, todoApp) {
+  this.deleteItem = (event) => {
+    const itemEl = event.target.parentElement.parentElement;
+    todoApp.deleteItem(itemEl.dataset.id);
+    todoApp.todoInput.focus();
+  };
+
   this.render = (items) => {
     listEl.innerHTML = items.map(renderTodoItem).join("");
   };
+
+  listEl.addEventListener("click", (event) => {
+    if (event.target.classList.contains("destroy")) {
+      return this.deleteItem(event);
+    }
+  });
 }
