5e1151501daf9a80b17fefba580040eac6fc519b
skid901 <tlrudejr1993@naver.com>
2021-01-23 07:48:49 +0900

    refactor: separate the todo addition logic from the event logic

--- js/TodoInput.js
+++ js/TodoInput.js
@@ -1,4 +1,10 @@
 export default function TodoInput(inputEl, todoApp) {
+  this.addTodo = (value) => {
+    inputEl.dispatchEvent(new CompositionEvent("compositionend"));
+    todoApp.addTodo(value);
+    inputEl.value = "";
+  };
+
   this.focus = () => inputEl.focus();
 
   this.render = () => {
@@ -10,9 +16,7 @@ export default function TodoInput(inputEl, todoApp) {
   inputEl.addEventListener("keypress", (event) => {
     const value = inputEl.value.trim();
     if (event.code === "Enter" && value) {
-      inputEl.dispatchEvent(new CompositionEvent("compositionend"));
-      todoApp.addTodo(value);
-      inputEl.value = "";
+      this.addTodo(value);
     }
   });
 }
