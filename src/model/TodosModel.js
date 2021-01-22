e6c16e8a5fb551943e9e9caacb1f59e83b82a3e1
SoHyun <pje1740@gmail.com>
2021-01-22 20:16:51 +0900

    [Feat] :zap:️ create TodosModel.js #8
    할일 데이터의 CRUD를 관리하는 모델 클래스 및 객체 생성

---
+++ src/model/TodosModel.js
@@ -0,0 +1,50 @@
+import TodoItem from '../components/TodoItem.js';
+
+/*
+localStorage의 todos 프로퍼티에 할일 목록을 아래와 같은 형태의 객체로 관리
+{
+  data: {title, index, isCompleted}[],
+  autoindex: number
+}
+*/
+
+class TodosData {
+  constructor() {
+    // 로컬 스토리지가 비어있다면
+    if (!localStorage.todos) {
+      this.data = {};
+      this.autoindex = 0;
+      this.saveToLocalStorage();
+    } else {
+      const currStorage = JSON.parse(localStorage.todos);
+      this.data = currStorage.data;
+      this.autoindex = Number(currStorage.autoindex);
+    }
+  }
+
+  get datas() {
+    return this.data;
+  }
+
+  saveToLocalStorage() {
+    localStorage.todos = JSON.stringify({ data: this.data, autoindex: this.autoindex });
+  }
+
+  createTodo(todoTitle) {
+    this.data[this.autoindex] = new TodoItem(todoTitle, this.autoindex);
+    this.autoindex++;
+    this.saveToLocalStorage();
+  }
+
+  updateTodo(index, isCompleted) {
+    this.data.get(index).isCompleted = isCompleted;
+  }
+
+  deleteTodo(index) {
+    this.data.delete(index);
+  }
+}
+
+const todosData = new TodosData();
+
+export default todosData;
