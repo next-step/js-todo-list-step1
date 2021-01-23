37e6992ab35e1a05bf1d7a4a5e05c1190f21b26e
domuk-k <dannyworks102@gmail.com>
2021-01-23 21:53:29 +0900

    chore: setting for further feature

--- js/types/constants.js
+++ js/types/constants.js
@@ -1,5 +1,11 @@
-export const BUTTON_NAMES = new Map([
+export const FILTER_NAMES = new Map([
   ["all", "전체보기"],
   ["active", "남은 투두"],
   ["completed", "완료한 투두"]
 ]);
+
+export const FILTER_ENUM = {
+  ALL: "all",
+  ACTIVE: "active",
+  COMPLETED: "completed"
+};
