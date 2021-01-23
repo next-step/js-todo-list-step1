2af11498acc08af3ea2d09ab9665774615320f8a
domuk-k <dannyworks102@gmail.com>
2021-01-23 21:52:48 +0900

    feat: CRUD for todos

--- js/App.js
+++ js/App.js
@@ -6,28 +6,75 @@ import { Todo } from "./types/index.js";
 
 class App extends Reilly.Component {
   state = {
-    todos: [],
+    todos: [
+      { id: "234", content: "Typescript", completed: false },
+      { id: "r4564", content: "React", completed: true }
+    ],
     mode: "all"
   };
 
   constructor(props) {
     super(props);
     this.addTodo = this.addTodo.bind(this);
+    this.toggleTodo = this.toggleTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
+    this.changeMode = this.changeMode.bind(this);
+    this.updateTodo = this.updateTodo.bind(this);
   }
 
   addTodo(e) {
     e.preventDefault();
-    let content = e.target.elements["new-todo"].value;
+    let content = e.target.elements["new-todo"].value.trim();
+    if (!content) return;
     this.setState({ todos: [new Todo(content), ...this.state.todos] });
   }
 
+  toggleTodo(e) {
+    e.stopPropagation();
+    if (!e.target.matches(".toggle")) return;
+    const targetId = e.path.find((elm) => elm.matches("li")).id;
+    this.setState({
+      todos: this.state.todos.map((todo) =>
+        todo.id !== targetId ? todo : { ...todo, completed: !todo.completed }
+      )
+    });
+  }
+
+  removeTodo(e) {
+    e.stopPropagation();
+    if (!e.target.matches(".destroy")) return;
+    if (!confirm("destroy this for real?")) return;
+    const targetId = e.path.find((elm) => elm.nodeName === "LI").id;
+    this.setState({
+      todos: this.state.todos.filter((todo) => todo.id !== targetId)
+    });
+  }
+
+  changeMode(e) {
+    this.setState({
+      mode: e.target.classList[0]
+    });
+  }
+
+  updateTodo(e) {
+    console.log(e.target);
+  }
+
   render() {
+    const { todos, mode } = this.state;
+
     return createElement(
       "div",
       { className: "todoapp" },
       createElement(Title, null, "todos"),
       createElement(TodoForm, { onsubmit: this.addTodo }),
-      createElement(Main, { todoState: this.state })
+      createElement(Main, {
+        todos,
+        mode,
+        onToggle: this.toggleTodo,
+        onRemove: this.removeTodo,
+        onModeChange: this.changeMode
+      })
     );
   }
 }
