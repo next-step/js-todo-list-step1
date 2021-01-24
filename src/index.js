ed924e9805ebcf16b0aad574cd68bc799a6e05ca
khw970421 <khw970421@kakao.com>
2021-01-24 16:39:07 +0900

    1/24 commit 최대한 전역변수 적게사용 이벤트 핸들러 끼리 정리 및 함수끼리 정리

--- src/index.js
+++ src/index.js
@@ -1,91 +1,155 @@
-let new_todo = document.getElementById('new-todo-title');
-let answer;
+let new_todo = document.querySelector('.new-todo');
+let count = 0 ;     //할 일 갯수 세는 용
+
+// 기존 저장된 것들이 있으면 이를 불러오는 함수 진행
+document.body.onload = function(){
+    for(let i=0;i<localStorage.length;i++)
+    {
+        let key = localStorage.key(i);
+        let item = localStorage.getItem(key);
+        admin(key,item);
+    }
+}
 
 new_todo.addEventListener("keyup", function(event) {
-    // Enter키를 누를시 실행
-    if (event.keyCode === 13) {
-        answer = confirm('등록 하시겠습니까?') == true ? admin(new_todo.value) : false;
+    // Enter키를 누를시 실행하는데 공백일경우 alert 함수 실행
+    if (event.key === 'Enter') {
+        (/[\S]/gi.test(new_todo.value)==true && check_overlap(new_todo.value) == true) ? admin(new_todo.value,'F') : alert('공백 혹은 같은 이름의 할일을 입력했습니다.');
         //입력한 할일 추가시 할일 공백으로 만들기
         new_todo.value = '';
     }
 });         // 내용을 입력하고 'Enter'를 누르면 comfirm이 나오고 맞다면 admin함수 실행
 
-let count = 0 ;     //할 일 갯수 세는 용
-let todo_count = document.querySelector('.todo-count');
+function click_set() {
+//전체보기 버튼 클릭시 모든 내용 display
+    document.querySelector('.all').addEventListener('click', function () {
+        document.querySelectorAll('.todo-list > li').forEach(x => x.style.display = '');
+        /* 아래코드도 가능
+       let child = document.querySelector('.todo-list')
+       child.childNodes.forEach(x=> x.style.display = '');
+         */
+    })
 
-// ul태그 추가
-let ul = document.createElement('ul');
-// main 태그를 기준으로 바로위에다가 ul 엘리먼트를 추가
-document.querySelector('main').insertAdjacentElement("beforebegin", ul);
+//해야할 일 버튼 클릭시 class가 completed인 것들은 display가 none이 되도록
+    document.querySelector('.active').addEventListener('click', function () {
+        document.querySelectorAll('.todo-list > li').forEach(x => x.classList.contains('active') == true ? x.style.display = '' : x.style.display = 'none');
+        /* 아래코드도 가능
+        let child = document.querySelector('.todo-list')
+        child.childNodes.forEach(x=>x.classList.contains('active') == true ? x.style.display='' : x.style.display = 'none');
+        */
+    })
 
-function admin(value) {
+//완료한 일 클릭시 class가 active인 것들은 display가 none이 되도록
+    document.querySelector('.completed').addEventListener('click', function () {
+        document.querySelectorAll('.todo-list > li').forEach(x => x.classList.contains('completed') == true ? x.style.display = '' : x.style.display = 'none')
+        /* 아래코드도 가능
+        let child = document.querySelector('.todo-list')
+        child.childNodes.forEach(x=>x.classList.contains('completed') == true ?  x.style.display='' : x.style.display = 'none')
+        */
+    })
+}
+
+function check_overlap(value){
+    for(let i=0;i<localStorage.length;i++)
+    {
+        if(value == localStorage.key(i))
+            return false;
+    }
+    return true;
+}
+
+function admin(value,item) {
+
     //태그들과 class 속성 부여 후 연결
-
     let li = document.createElement('li');
     let div = document.createElement('div');
     let input = document.createElement('input');
     let button = document.createElement('button');
     let label = document.createElement('label');
 
-    ul.append(li);
     li.append(div);
     div.append(input);
     div.append(label);
     div.append(button);
 
-    ul.classList.add('todo');
-    ul.setAttribute('id','todo');
     li.classList.add('active')
-    div.classList.add('new-todo');
+    div.classList.add('view');
     input.classList.add('toggle');
     input.setAttribute('type', 'checkbox');
     label.classList.add(`label`);
-    button.classList.add(`destroy${count++}`);
+    button.classList.add(`destroy`);
 
-    //li앞에 문자 생략
-    li.style.listStyleType = "None";
     //입력 값을 label에 추가 및 버튼에 X 추가
-    label.innerHTML = value;
-    button.innerHTML = 'X';
+    label.innerText = value;
+
+    //localStorage의 value(item)이 T이면 completed한 상태 및 체크상태 유지(아닐경우 기본 active와 체크상태 X이다)
+    if(item == 'T')
+    {
+        li.setAttribute('class' , 'completed')
+        input.setAttribute('checked','true')
+    }
+    //  item이 F이면 active 상태로 처음 작성시 item == 'F' 일때의 localStorage 저장 역할
+    else
+        localStorage.setItem(label.innerText,'F');
 
     //버튼 클릭시 가장가까운 li태그 삭제 및, 해야할 count 값 줄이기
     button.addEventListener('click', function (event) {
         event.target.closest('li').remove();
-        todo_count.childNodes[1].innerHTML = --count;
+        document.querySelector('.todo-count > strong').innerText = --count;
+        localStorage.removeItem(label.innerText);
     })
 
     // 체크박스 클릭시 li태그에 class속성 추가 및 text에 중간작대기 생성 (클릭 취소하면 class속성 completed 추가 및 text원래대로)
-    // input의 부모태그 아래의 [1] 위치에 text가 존재하므로 이를 진행
+    // 체크박스 클릭 혹은 클릭 취소 시  localStorage 속성도 변화시킨다.
     input.addEventListener('click', function (event) {
         if (input.checked == true) {
             event.target.closest('li').setAttribute('class','completed');
-            event.target.parentNode.childNodes[1].style.textDecoration = 'line-through';
+            localStorage.setItem(label.innerText,'T');
         } else {
             event.target.closest('li').setAttribute('class','active');
-            event.target.parentNode.childNodes[1].style.textDecoration = 'none';
+            localStorage.setItem(label.innerText,'F');
         }
     })
 
+    //label 더블클릭시 input형태로 변경
+    label.addEventListener('dblclick',function(){
+        // 기존의 class인 li_class와 input태그인 input_tag 설정
+        let li_class = li.getAttribute('class');
+        let input_tag  = document.createElement('input');
+
+        li.setAttribute('class','editing');
+        input_tag.setAttribute('class','edit');
+        li.insertAdjacentElement("beforeend", input_tag);
+
+        //input에서 Enter 클릭시 적용/ Esc 클릭시 취 소적용
+        input_tag.addEventListener('keydown', function (e) {
+            if (e.key === 'Enter' && /[\S]/gi.test(input_tag.value)==true && check_overlap(input_tag.value) == true) {
+                //기존 localItem 제거, 새로운 localItem 추가 및 값 변경과 기본값으로 속성 변경 및 click_set 다시 설정(변화 후 바로 적용되게)
+                localStorage.removeItem(label.innerText);
+                localStorage.setItem(input_tag.value,'F');
+                label.innerText = input_tag.value;
+                input.removeAttribute('checked')
+                input_tag.remove();
+                li.setAttribute('class', 'active');
+                click_set();
+            }
+            else if (e.key === 'Escape') {
+                // li class가 editing에서 active로 다시 원래로 바꾸기 및 input_tag 제거
+                input_tag.remove();
+                li.setAttribute('class', li_class);     //기존의 class로 바꾸기
+            }
+            else if(e.key === 'Enter' && /[\S]/gi.test(input_tag.value)==false){
+                alert('공백 입력 금지')
+            }
+            else if(e.key === 'Enter' && check_overlap(input_tag.value) == false){
+                alert('이미 존재하는 할일 입력 금지')
+            }
+        });
+    })
     //strong태그 안에 있는 count 값 적용시키기
-    todo_count.childNodes[1].innerHTML = count;
+    document.querySelector('.todo-count > strong').innerText = ++count;
+    //해당 이벤트핸들러 및 모든 걸 끝낸 후 li태그를 가진 내용을 ul태그 안에 넣기
+    document.querySelector('.todo-list').insertAdjacentElement("beforeend", li);
 }
 
-//전체보기 버튼 클릭시 모든 내용 display
-document.querySelector('.all').addEventListener('click',function(){
-    let child = document.querySelector('.todo')
-    child.childNodes.forEach(x=> x.style.display = '');
-})
-
-//해야할 일 버튼 클릭시 class가 completed인 것들은 display가 none이 되도록
-document.querySelector('.active').addEventListener('click',function(){
-    let child = document.querySelector('.todo')
-    child.childNodes.forEach(x=>x.classList!='active' ? x.style.display = 'none' : x.style.display='')
-})
-
-//완료한 일 클릭시 class가 active인 것들은 display가 none이 되도록
-document.querySelector('.completed').addEventListener('click',function(){
-    let child = document.querySelector('.todo')
-    child.childNodes.forEach(x=>x.classList!='completed' ? x.style.display = 'none' : x.style.display='')
-})
-
-
+click_set();
