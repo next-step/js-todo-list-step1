564acb104e65f826c99f443a841af154f0c7821e
skid901 <tlrudejr1993@naver.com>
2021-01-25 04:47:24 +0900

    refactor: separate state logic from event listener

--- js/TodoCount.js
+++ js/TodoCount.js
@@ -3,23 +3,26 @@ export default function TodoCount(countContainerEl, todoApp) {
   const filtersEl = countContainerEl.querySelector(".filters");
   const filterEls = filtersEl.querySelectorAll("a");
 
-  this.render = function (todos) {
-    countEl.innerHTML = `총 <strong>${todos.length}</strong> 개`;
-  };
-
-  filtersEl.addEventListener("click", (event) => {
-    if (event.target.nodeName !== "A") return;
-
+  this.setFilter = (targetEl) => {
     filterEls.forEach((el) => {
       const { classList } = el;
       classList.remove("selected");
-      if (el !== event.target) return;
+      if (el !== targetEl) return;
 
       classList.add("selected");
       todoApp.setFilter(
         classList.contains("all") ? null : classList.contains("completed")
       );
     });
+  };
+
+  this.render = function (todos) {
+    countEl.innerHTML = `총 <strong>${todos.length}</strong> 개`;
+  };
+
+  filtersEl.addEventListener("click", (event) => {
+    if (event.target.nodeName !== "A") return;
+    this.setFilter(event.target);
 
     event.preventDefault();
   });
