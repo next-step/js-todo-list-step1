02dc12718eac3a6294b67109314fba7332df4b5b
domuk-k <dannyworks102@gmail.com>
2021-01-25 11:57:25 +0900

    feat: typedef for init Todo and TodoState

--- js/types/constants.js
+++ js/types/constants.js
@@ -1,9 +1,17 @@
+/**
+ * @readonly
+ * @enum {string}
+ */
 export const FILTER_NAMES = new Map([
   ["all", "전체보기"],
   ["active", "남은 투두"],
   ["completed", "완료한 투두"]
 ]);
 
+/**
+ * @readonly
+ * @enum {"all" | "active" | "completed"}
+ */
 export const FILTER_ENUM = {
   ALL: "all",
   ACTIVE: "active",
