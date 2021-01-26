a958f64547c7fed1866b25b465704f630fe7a125
DongHwan Kim <bkuekany@gmail.com>
2021-01-26 11:42:05 +0900

    feat: Add Todo 총 갯수 컴포넌트 추가

---
+++ component/TodoTotalCount.js
@@ -0,0 +1,35 @@
+
+export function TodoTotalCount( $div, context ){
+    const $count = document.querySelector('.todo-count > strong');
+    const $filter = $div.querySelector('.filters');
+
+    this.todoItems = [];
+
+    this.setState = updatedTodoItems => {
+        this.todoItems = updatedTodoItems;
+        this.render(this.todoItems);
+    };
+    
+    this.render = () => {
+        $count.innerText = this.todoItems.length;
+        $filter.innerHTML = renderHTML();
+    };
+
+    $filter.addEventListener('click', e => {
+        const completeState = e.target.innerText;
+        context.filterTodo(completeState);
+    });
+
+    const renderHTML = () => {
+        return `<li>
+                    <a class="all selected" href="/#">전체보기</a>
+                </li>
+                <li>
+                    <a class="active" href="#active">해야할 일</a>
+                </li>
+                <li>
+                    <a class="completed" href="#completed">완료한 일</a>
+                </li>`
+    }
+
+}
