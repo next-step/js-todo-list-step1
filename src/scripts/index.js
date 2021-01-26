32c7e27ea7a9cee1cf66432c6dc254f8a5e0644e
SoHyun <pje1740@gmail.com>
2021-01-26 09:42:35 +0900

    [Feat] :zap:️ add checks for initial hash in the url #11
    새로고침 시 url의 해시가 변경되지 않기 때문에 해당 부분을 감지하여 필터를 적용한 뒤 출력

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,11 @@
 import newTodoInput from '../components/NewTodoInput.js';
 import todoCount from '../components/TodoCount.js';
+import filters from '../components/Filters.js';
 
 newTodoInput.init();
+
+if (window.location.hash === '#active') {
+  filters.showActive();
+} else if (window.location.hash === '#completed') {
+  filters.showCompleted();
+}
