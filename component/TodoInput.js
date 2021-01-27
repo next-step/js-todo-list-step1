210df3089ab2b0c35babe1622d7a5dd9176bf5f3
DongHwan Kim <bkuekany@gmail.com>
2021-01-27 23:31:19 +0900

    event.key 방식 적용

--- component/TodoInput.js
+++ component/TodoInput.js
@@ -2,14 +2,15 @@
 export function TodoInput( context ) {
     const $todoInput = document.querySelector("#new-todo-title");
   
-    $todoInput.addEventListener("keypress", event => this.addTodoItem(event));
   
     this.addTodoItem = event => {
       const $newTodoTarget = event.target;
-      if(event.keyCode === 13){
+      if(event.key === 'Enter'){
          context.saveItem({todoItem: $newTodoTarget.value, completed: false});
          $todoInput.value = '';
       }
     };
+    
+    $todoInput.addEventListener("keypress", this.addTodoItem);
   }
   
