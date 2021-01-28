858029b0b12609a3566757335aa0252c02bf18da
Jiseon Kim <jsid1561@gmail.com>
2021-01-28 11:11:46 +0900

    refactor: className변수를 filterStatus로 대체

--- src/components/todoFilter.js
+++ src/components/todoFilter.js
@@ -8,34 +8,25 @@ export const todoFliter = () => {
 
 export let filterStatus = 'all';
 
-// 클릭하는 필터에 따라 보이는 투두리스트와 count 수가 변경됨
 const filtering = ({target}) => {
 
     const $allTodo= $todoList.querySelectorAll('li');
     const $completedTodo = $todoList.querySelectorAll('.completed')
 
-    // className을 배열로 받아와서 0번째 값을 filterStatus에 넣는 것은 ?
-    const className = target.className;
+    filterStatus = target.classList.item(0);
 
-    if(className === 'all selected'){
+    if(filterStatus === 'all'){
         display($allTodo);
-
-        // 필터 버튼을 클릭했을 때 보이는 todo 수 변경
-        filterStatus = 'all';
-        todoCount(filterStatus);
-    } else if(className === 'active'){
+    } else if(filterStatus === 'active'){
         display($allTodo);
         hide($completedTodo);
-
-        filterStatus = 'active';
-        todoCount(filterStatus);
-    } else if(className === 'completed'){
+    } else if(filterStatus === 'completed'){
         hide($allTodo);
         display($completedTodo);
-
-        filterStatus = 'completed';
-        todoCount(filterStatus);
     }
+
+    // filter적용했을 때 count 수 변경
+    todoCount(filterStatus);
 }
 
 
