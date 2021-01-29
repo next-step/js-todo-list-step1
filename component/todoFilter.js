91e8d72f569d418b2249cd9c68f384e4f4d35d5a
6mn12j <dbwk0404@hanmail.net>
2021-01-29 15:53:55 +0900

    Refactor : 코드리뷰 적용

--- component/todoFilter.js
+++ component/todoFilter.js
@@ -1,71 +1,64 @@
-import { filterChange, filterState, toDos } from "../init.js";
-import {
-  activeEl,
-  completedEl,
-  filterEls,
-  todoListEl,
-  allEl,
-} from "./constant.js";
-import { handleCount } from "./todoCount.js";
-import { addToDos } from "./todoList.js";
+import { filterChange, filterState, toDos } from '../init.js';
+import { activeEl, completedEl, filterEls, todoListEl, allEl } from './constant.js';
+import { handleCount } from './todoCount.js';
+import { addToDos } from './todoList.js';
 
 export const viewAllClick = (event) => {
   event.preventDefault();
-  filterChange("all");
+  filterChange('all');
   filterEls.forEach((el) => {
-    el.classList.remove("selected");
+    el.classList.remove('selected');
   });
-  allEl.classList.add("selected");
+  allEl.classList.add('selected');
 
-  todoListEl.innerHTML = "";
-  toDos.forEach((item) => {
-    addToDos(item);
-  });
+  todoListEl.innerHTML = '';
+
+  toDos.map((item) => addToDos(item));
   handleCount(toDos.length);
 };
 
 export const viewActiveClick = (event) => {
   event.preventDefault();
 
-  filterChange("active");
+  filterChange('active');
 
   filterEls.forEach((el) => {
-    el.classList.remove("selected");
+    el.classList.remove('selected');
   });
-  activeEl.classList.add("selected");
+  activeEl.classList.add('selected');
 
   renderActiveItems();
 };
 
 export const viewCompletedClick = (event) => {
   event.preventDefault();
-  filterChange("completed");
+  filterChange('completed');
   filterEls.forEach((el) => {
-    el.classList.remove("selected");
+    el.classList.remove('selected');
   });
-  completedEl.classList.add("selected");
+  completedEl.classList.add('selected');
 
   renderCompleteItems();
 };
 
 export const renderFromFilter = () => {
-  if (filterState === "completed") renderCompleteItems();
-  else if (filterState === "active") renderActiveItems();
+  if (filterState === 'completed') renderCompleteItems();
+  else if (filterState === 'active') renderActiveItems();
 };
 
 const renderActiveItems = () => {
-  todoListEl.innerHTML = "";
+  todoListEl.innerHTML = '';
   const activeItems = toDos.filter((item) => item.completed === false);
-  activeItems.forEach((item) => {
+  activeItems.map((item) => {
     addToDos(item);
   });
   handleCount(activeItems.length);
 };
 
 const renderCompleteItems = () => {
-  todoListEl.innerHTML = "";
+  todoListEl.innerHTML = '';
   const completedItems = toDos.filter((item) => item.completed === true);
-  completedItems.forEach((item) => {
+  completedItems.map((item) => {
     addToDos(item);
   });
   handleCount(completedItems.length);
