c21c2c393f2b2153337d4761a1a0129450c3f664
khw970421 <khw970421@kakao.com>
2021-01-29 20:54:26 +0900

    1/29 리터럴 템플릿 적용

--- src/index.js
+++ src/index.js
@@ -65,84 +65,76 @@ function check_overlap(value){
 }
 
 function init_Element(value,item) {
-
-    //태그들과 class 속성 부여 후 연결
-    let li = document.createElement('li');
-    let div = document.createElement('div');
-    let input = document.createElement('input');
-    let button = document.createElement('button');
-    let label = document.createElement('label');
-
-    li.append(div);
-    div.append(input);
-    div.append(label);
-    div.append(button);
-
-    li.classList.add('active')
-    div.classList.add('view');
-    input.classList.add('toggle');
-    input.setAttribute('type', 'checkbox');
-    label.classList.add(`label`);
-    button.classList.add(`destroy`);
-
-    //입력 값을 label에 추가 및 버튼에 X 추가
-    label.innerText = value;
-
+    const $todo_list = document.querySelector('.todo-list');
+    const content = (value)=>`
+        <li class="active"> 
+            <div class="view">
+                <input class="toggle" type="checkbox">
+                <label class="label">${value}</label>            
+                <button class="destroy"></button>    
+            </div>
+        </li>
+    `
+    $todo_list.innerHTML += content(value);
     //localStorage의 value(item)이 T이면 completed한 상태 및 체크상태 유지(아닐경우 기본 active와 체크상태 X이다)
     if(item === 'T')
     {
-        li.setAttribute('class' , 'completed')
-        input.setAttribute('checked','true')
+        document.querySelectorAll('.label').forEach($el => {
+           if($el.innerText === value){
+               $el.closest('li').setAttribute('class' , 'completed');
+               $el.closest('div').children[0].setAttribute('checked','true');
+           }
+        })
     }
     //  item이 F이면 active 상태로 처음 작성시 item == 'F' 일때의 localStorage 저장 역할
     else
-        localStorage.setItem(label.innerText,'F');
+        localStorage.setItem(value,'F');
 
     //버튼 클릭시 가장가까운 li태그 삭제 및, 해야할 count 값 줄이기
-    button.addEventListener('click', function (event) {
+    document.querySelectorAll('.destroy').forEach($el => $el.addEventListener('click', function (event) {
         event.target.closest('li').remove();
         document.querySelector('.todo-count > strong').innerText = --count;
-        localStorage.removeItem(label.innerText);
-    })
+        localStorage.removeItem(event.target.closest('div').children[1].innerText);
+    }))
 
     // 체크박스 클릭시 li태그에 class속성 추가 및 text에 중간작대기 생성 (클릭 취소하면 class속성 completed 추가 및 text원래대로)
     // 체크박스 클릭 혹은 클릭 취소 시  localStorage 속성도 변화시킨다.
-    input.addEventListener('click', function (event) {
-        if (input.checked === true) {
+    document.querySelectorAll('.toggle').forEach($el => $el.addEventListener('click', function (event) {
+        if ($el.checked === true) {
             event.target.closest('li').setAttribute('class','completed');
-            localStorage.setItem(label.innerText,'T');
+            localStorage.setItem(event.target.closest('div').children[1].innerText,'T');
         } else {
             event.target.closest('li').setAttribute('class','active');
-            localStorage.setItem(label.innerText,'F');
+            localStorage.setItem(event.target.closest('div').children[1].innerText,'F');
         }
-    })
+    }))
 
     //label 더블클릭시 작동
-    label.addEventListener('dblclick',function(){
+    document.querySelectorAll('.label').forEach($el => $el.addEventListener('dblclick',function(event){
         // 기존의 class인 li_class와 input태그인 input_tag 설정
-        let li_class = li.getAttribute('class');
+        let li_class = event.target.closest('li').getAttribute('class');
         let input_tag  = document.createElement('input');
 
-        li.setAttribute('class','editing');
+        event.target.closest('li').setAttribute('class','editing');
         input_tag.setAttribute('class','edit');
-        li.insertAdjacentElement("beforeend", input_tag);
+        event.target.closest('li').insertAdjacentElement("beforeend", input_tag);
 
         //input에서 Enter 클릭시 적용/ Esc 클릭시 취 소적용
         input_tag.addEventListener('keydown', function (e) {
             if (e.key === 'Enter' && /[\S]/gi.test(input_tag.value)==true && check_overlap(input_tag.value) == true) {
                 //기존 localItem 제거, 새로운 localItem 추가 및 값 변경과 기본값으로 속성 변경 및 click_set 다시 설정(변화 후 바로 적용되게)
-                localStorage.removeItem(label.innerText);
+                localStorage.removeItem(event.target.closest('label').innerText);
                 localStorage.setItem(input_tag.value,'F');
-                label.innerText = input_tag.value;
-                input.removeAttribute('checked')
+                event.target.closest('label').innerText = input_tag.value;
                 input_tag.remove();
-                li.setAttribute('class', 'active');
+                event.target.closest('li').setAttribute('class', 'active');
+                event.target.closest('div').children[0].removeAttribute('checked')
                 click_set();
             }
             else if (e.key === 'Escape') {
                 // li class가 editing에서 active로 다시 원래로 바꾸기 및 input_tag 제거
                 input_tag.remove();
-                li.setAttribute('class', li_class);     //기존의 class로 바꾸기
+                event.target.closest('li').setAttribute('class', li_class);     //기존의 class로 바꾸기
             }
             else if(e.key === 'Enter' && /[\S]/gi.test(input_tag.value)==false){
                 alert('공백 입력 금지')
@@ -151,11 +143,10 @@ function init_Element(value,item) {
                 alert('이미 존재하는 할일 입력 금지')
             }
         });
-    })
+    }))
     //strong태그 안에 있는 count 값 적용시키기
     document.querySelector('.todo-count > strong').innerText = ++count;
     //해당 이벤트핸들러 및 모든 걸 끝낸 후 li태그를 가진 내용을 ul태그 안에 넣기
-    document.querySelector('.todo-list').insertAdjacentElement("beforeend", li);
 }
 
 //처음 시작시 이벤트 핸들러 적용
