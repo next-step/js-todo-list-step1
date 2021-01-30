efd2123e31ce5c8814afb3c1ba9c804adbd5131d
younoah <kr_uno_10@naver.com>
2021-01-30 19:24:14 +0900

    add function of editTodoItem

--- js/todo.js
+++ js/todo.js
@@ -1,22 +1,29 @@
 'ues strict';
 const todoInput = document.querySelector('.new-todo');
 const todoList = document.querySelector('.todo-list');
-const filter = document.querySelector('.filters');
+const filters = document.querySelector('.filters');
+const nowFilter = document.querySelector('.filters .selected');
 
 const filterNames = ['all', 'active', 'completed'];
 
 function init() {
-  onClickFilter();
+  showCountItems(filters.querySelector('.all'));
   todoInput.addEventListener('keyup', event => {
     addTodoItem(event);
   });
   todoList.addEventListener('click', event => {
     toggleTodoItem(event);
     deleteTodoItem(event);
-    onClickFilter(event);
   });
-  filter.addEventListener('click', event => {
-    onClickFilter(event);
+  todoList.addEventListener('dblclick', event => {
+    console.log('dblClick');
+    toggleEditTodoItem(event);
+  });
+  todoList.addEventListener('keyup', event => {
+    completeEditTodoItem(event);
+  });
+  filters.addEventListener('click', event => {
+    showCountItems(event.target);
   });
 }
 
@@ -26,17 +33,19 @@ function addTodoItem(event) {
     return;
   }
   console.log('addTodoItem() called');
-  todoList.append(createTodoTemplete(todoTitle));
+  todoList.append(createTodoItemTemplate(todoTitle));
   event.target.value = '';
+  showCountItems(filters.querySelector('.all'));
 }
 
-function createTodoTemplete(title) {
-  console.log('createTodoTemplete() called');
+function createTodoItemTemplate(title) {
+  console.log('createTodoItemTemplate() called');
   const li = document.createElement('li');
   const div = document.createElement('div');
   const input = document.createElement('input');
   const label = document.createElement('label');
   const button = document.createElement('button');
+  const editInput = document.createElement('input');
 
   // set elements
   div.setAttribute('class', 'view');
@@ -45,12 +54,15 @@ function createTodoTemplete(title) {
   label.setAttribute('class', 'label');
   label.innerHTML = `${title}`;
   button.setAttribute('class', 'destroy');
+  editInput.className = 'edit';
+  editInput.setAttribute('value', title);
 
   // combine elements
-  div.appendChild(input);
-  div.appendChild(label);
-  div.appendChild(button);
+  div.append(input);
+  div.append(label);
+  div.append(button);
   li.appendChild(div);
+  li.append(editInput);
   return li;
 }
 
@@ -63,6 +75,7 @@ function deleteTodoItem(event) {
   const todoItem = event.target.closest('li');
 
   todoItem.remove();
+  showCountItems(filters.querySelector('.all'));
 }
 
 function toggleTodoItem(event) {
@@ -76,45 +89,91 @@ function toggleTodoItem(event) {
   li.classList.toggle('completed');
 }
 
-function onClickFilter(event) {
-  const filterName = event === undefined ? 'all' : event.target.classList[0];
-  const targetFilter =
-    event === undefined ? document.querySelector('.all') : event.target;
+function showCountItems(target) {
+  const filterName = target.classList[0];
+
   if (!filterNames.includes(filterName)) {
     return;
   }
-  console.log('onClickFilter() called');
+
+  console.log('showCountItems() called');
   const todoCountText = document.querySelector('.todo-count>strong');
-  const totalNumber = document.querySelectorAll('.todo-list li').length;
-  const completedNumber = document.querySelectorAll('.todo-list li.completed')
-    .length;
+
   switch (filterName) {
     case 'all':
+      const totalNumber = document.querySelectorAll('.todo-list li').length;
       console.log('all');
-      clearFilter();
-      targetFilter.classList.add('selected');
+      removeHidden();
+      clearFilters();
+      target.classList.add('selected');
       todoCountText.innerHTML = totalNumber;
       break;
     case 'active':
+      const activeNumber = document.querySelectorAll(
+        '.todo-list li:not(.completed)'
+      ).length;
       console.log('active');
-      clearFilter();
-      targetFilter.classList.add('selected');
-      todoCountText.innerHTML = totalNumber - completedNumber;
+      removeHidden();
+      clearFilters();
+      addHidden('.completed');
+      target.classList.add('selected');
+      todoCountText.innerHTML = activeNumber;
       break;
     case 'completed':
+      const completedNumber = document.querySelectorAll(
+        '.todo-list li.completed'
+      ).length;
       console.log('completed');
-      clearFilter();
-      targetFilter.classList.add('selected');
+      removeHidden();
+      addHidden(`li:not(.completed)`);
+      clearFilters();
+      target.classList.add('selected');
       todoCountText.innerHTML = completedNumber;
       break;
   }
 }
 
-function clearFilter() {
+function clearFilters() {
   const filters = document.querySelectorAll('.filters a');
   for (const filter of filters) {
     filter.classList.remove('selected');
   }
+}
+
+function removeHidden() {
+  const hiddenItems = document.querySelectorAll('.hidden');
+  for (const item of hiddenItems) {
+    item.classList.remove('hidden');
+  }
+}
+
+function addHidden(selector) {
+  const items = document.querySelectorAll(`.todo-list ${selector}`);
+  for (const item of items) {
+    item.classList.add('hidden');
+  }
+}
+
+function toggleEditTodoItem(event, target) {
+  if (event.target.className !== 'label') {
+    return;
+  }
+  console.log('toggleEditTodoItem() called');
+  const todoItem = event.target.parentElement.parentElement;
+  todoItem.classList.toggle('editing');
+}
+
+function completeEditTodoItem(event) {
+  if (event.key !== 'Enter') {
+    return;
+  }
+  console.log('completeEditTodoItem() called');
+  const todoItem = document.querySelector('.editing');
+  const todoTitle = todoItem.querySelector('.edit').value;
+  const todoLabel = todoItem.querySelector('.label');
+  todoLabel.innerHTML = todoTitle;
+  console.log(todoItem);
+  todoItem.classList.toggle('editing');
 }
 
 init();
