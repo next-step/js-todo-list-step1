c6ddda1adb2250652c2887e9aab8587dde94bdeb
Yeewon <yes3427@kookmin.ac.kr>
2021-02-01 01:42:58 +0900

    review#11 : remove unnecessary if statements

--- js/ControlTodoItem.js
+++ js/ControlTodoItem.js
@@ -5,24 +5,20 @@ export function initControlTodoItem(){
 }
 
 //완료한 일로 check, 할 일 삭제 : edit모드에서는 작동X
-function onToggleTodoItem({target, key}) {
+function onToggleTodoItem({target}) {
     const li = target.closest("li");
 
-    //할 일을 체크하거나 해제
-    if(li.classList.value === "" || li.classList.value === "completed"){
-        //완료된 일 
-        if(target && target.nodeName === 'INPUT'){ 
-            target.closest("li").classList.toggle("completed");
-        }
-        
-        //삭제
-        if(target && target.nodeName === 'BUTTON'){
-            var returnValue = confirm('정말로 삭제하시겠습니까?');
-            if(returnValue === true){
-                li.parentNode.removeChild(li);
-            }
-            document.querySelector("strong").innerText = document.querySelectorAll("#todo-list li").length;
-        }
+    //완료된 일 
+    if(target && target.nodeName === 'INPUT'){ 
+        target.closest("li").classList.toggle("completed");
+    }
 
+    //삭제
+    if(target && target.nodeName === 'BUTTON'){
+        var returnValue = confirm('정말로 삭제하시겠습니까?');
+        if(returnValue === true){
+            li.parentNode.removeChild(li);
+        }
+        document.querySelector("strong").innerText = document.querySelectorAll("#todo-list li").length;
     }
 }
