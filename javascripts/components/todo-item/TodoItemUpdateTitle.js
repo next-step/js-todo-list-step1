64fae1dd0d7cd47d3185981d549219924827f245
YunJin Choi <zzdd1558@gmail.com>
2021-02-26 04:16:30 +0900

    refactor this 제거 및 indent 4 => 2

--- javascripts/components/todo-item/TodoItemUpdateTitle.js
+++ javascripts/components/todo-item/TodoItemUpdateTitle.js
@@ -1,21 +1,21 @@
 import KeyUtils from "../../utils/KeyUtils.js";
 
 export default function TodoItemUpdateTitle({id, title, updateTodo, updateTodoStatus}) {
-    const element = document.createElement("input");
-    element.classList.add("edit");
-    element.value = title;
-    element.addEventListener("keyup", ({keyCode}) => {
+  const element = document.createElement("input");
+  element.classList.add("edit");
+  element.value = title;
+  element.addEventListener("keyup", ({keyCode}) => {
 
-        if (KeyUtils.isEsc(keyCode)) {
-            updateTodoStatus({id, status: false})
-        }
-
-        if (KeyUtils.isEnter(keyCode)) {
-            updateTodo({id, "title": element.value});
-        }
-    })
-
-    return {
-        render : () => element
+    if (KeyUtils.isEsc(keyCode)) {
+      updateTodoStatus({id, status: false})
     }
+
+    if (KeyUtils.isEnter(keyCode)) {
+      updateTodo({id, "title": element.value});
+    }
+  })
+
+  return {
+    render: () => element
+  }
 }
