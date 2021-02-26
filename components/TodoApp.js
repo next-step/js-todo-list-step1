8eaf235d8f8ced9f044cfb110ab0492316d9ceb2
nakzyu <nakzyu@gmail.com>
2021-02-26 23:59:16 +0900

    Items의 상태가 변경되었을때 TodoItem컴포넌트를 동적으로 생성,삭제

--- components/TodoApp.js
+++ components/TodoApp.js
@@ -1,50 +1,113 @@
 import Item from "../models/Item.js";
 
 export default class ToDoApp {
-  todoItems = [];
+  items = [];
+  entrustedComponents = [];
 
   constructor() {
     new TodoInput(this.onAdd.bind(this));
+    this.entrustedComponents.push(new TodoList(this.onRemove.bind(this)));
   }
 
-  setState(todoItems) {
-    this.todoItems = todoItems;
+  notify() {
+    this.entrustedComponents.forEach((component) =>
+      component.render(this.items)
+    );
+  }
+
+  setState(items) {
+    this.items = items;
+    console.log(this.items);
+    this.notify();
   }
 
   onAdd(item) {
-    const newState = [item, ...this.todoItems];
-    this.setState(newState);
-    console.log(this.todoItems);
+    const itemAddedState = [item, ...this.items];
+    this.setState(itemAddedState);
   }
 
-  onRemove() {}
+  onRemove(id) {
+    console.log("remove");
+    const targetDeletedState = this.items.filter((item) => item.id !== id);
+    this.setState(targetDeletedState);
+  }
 }
 
 class TodoInput {
   $todoInput;
   onAdd;
+  idCounter = 0;
 
   constructor(onAdd) {
     this.$todoInput = document.querySelector("#new-todo-title");
     this.onAdd = onAdd;
-
-    this.init();
+    this.$todoInput.addEventListener("keypress", this.onKeypress.bind(this));
   }
 
-  init() {
-    this.$todoInput.addEventListener("keypress", this.onKeypress.bind(this));
+  genTodoItemId() {
+    return `_${this.idCounter++}`;
   }
 
   onKeypress(event) {
     if (event.key === KEYS.ENTER) {
-      this.onAdd(new Item(event.target.value, false));
+      this.onAdd(new Item(this.genTodoItemId(), event.target.value, false));
     }
   }
 }
 
-class TodoList {}
+class TodoList {
+  $todoList;
+  onRemove;
 
-class TodoItem {}
+  constructor(onRemove) {
+    this.$todoList = document.querySelector("#todo-list");
+    this.onRemove = onRemove;
+  }
+
+  render(items) {
+    this.$todoList.innerHTML = [];
+    items.map((item) =>
+      this.$todoList.appendChild(new TodoItem(item, this.onRemove).render())
+    );
+  }
+}
+
+class TodoItem {
+  item;
+  onRemove;
+
+  constructor(item, onRemove) {
+    this.item = item;
+    this.onRemove = onRemove;
+  }
+
+  makeTemplate(title) {
+    const $li = document.createElement("li");
+
+    $li.innerHTML = `
+           <div class="view">
+              <input class="toggle" type="checkbox"/>
+              <label class="label">${title}</label>
+              <button class="destroy"></button>
+            </div>
+            <input class="edit" value="${title}" />
+        `;
+
+    const $destroyBtn = $li.querySelector(".destroy");
+
+    $destroyBtn.addEventListener("click", this.onDeleteButtonClick.bind(this));
+
+    return $li;
+  }
+
+  onDeleteButtonClick() {
+    this.onRemove(this.item.id);
+  }
+
+  render() {
+    return this.makeTemplate(this.item.title);
+  }
+}
 
 const KEYS = {
   ENTER: "Enter",
--- models/Item.js
+++ models/Item.js
@@ -1,8 +1,10 @@
 export default class Item {
+  id;
   title;
   isCompleted;
 
-  constructor(title, isCompleted) {
+  constructor(id, title, isCompleted) {
+    this.id = id;
     this.title = title;
     this.isCompleted = isCompleted;
   }
