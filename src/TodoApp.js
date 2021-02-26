a84890fa297693024e039f195544c38a65286d3f
younoah <kr_uno_10@naver.com>
2021-02-27 00:29:48 +0900

    refactor : TodoApp 모듈화, index.js 추가

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -4,25 +4,23 @@ import * as Utils from './utils.js';
 import TodoInput from './TodoInput.js';
 import TodoList from './TodoList.js';
 import TodoFilter from './TodoFilter.js';
-
-const todoInput = new TodoInput();
-const todoList = new TodoList();
-const todoFilter = new TodoFilter();
-
-const init = () => {
-  todoList.load();
-  todoInput.setKeyUpListener(event => {
-    const text = event.target.value;
-    const id = Utils.uuid();
-    todoList.addNewItem(text, id);
-    todoList.render();
-    todoList.save();
-  });
-  todoFilter.setClickListener(event => {
-    const targetBtn = event.target;
-    todoList.filterOption = targetBtn.classList[0];
-    todoList.render();
-  });
-};
-
-init();
+export default class TodoApp {
+  constructor() {
+    this.todoInput = new TodoInput();
+    this.todoList = new TodoList();
+    this.todoFilter = new TodoFilter();
+    this.todoList.load();
+    this.todoInput.setKeyUpListener(event => {
+      const text = event.target.value;
+      const id = Utils.uuid();
+      this.todoList.addNewItem(text, id);
+      this.todoList.render();
+      this.todoList.save();
+    });
+    this.todoFilter.setClickListener(event => {
+      const targetBtn = event.target;
+      this.todoList.filterOption = targetBtn.classList[0];
+      this.todoList.render();
+    });
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+import TodoApp from './TodoApp.js';
+
+new TodoApp();
