da100f241d809ba9ee2afcabb032ad7e209dd375
ordinCode <ysh1346@naver.com>
2021-02-27 12:20:25 +0900

    feat: 편집 기능 추가

--- js/controller/TodoController.js
+++ js/controller/TodoController.js
@@ -19,19 +19,19 @@ function TodoController() {
   };
 
   const onClickTodoList = ({ target }) => {
-    const $todoItem = target.closest(".todo-item");
+    const $todoItem = findItem(target);
     if (isToggleButton(target)) {
       this.todoAppService.toggle($todoItem);
       return;
     }
     if (isDestroyButton(target)) {
       this.todoAppService.destroy($todoItem);
-      return;
-    }
-    if (isNotComplete(target)) {
-      this.todoAppService.onEdit($todoItem);
     }
   };
+
+  function findItem(target) {
+    return target.closest(".todo-item");
+  }
 
   function isToggleButton(target) {
     return target.classList.contains("toggle");
@@ -41,8 +41,19 @@ function TodoController() {
     return target.classList.contains("destroy");
   }
 
+  const onDoubleClickTodoList = ({ target }) => {
+    if (isToggleButton(target)) {
+      return;
+    }
+
+    const $todoItem = findItem(target);
+    if (isNotComplete(target)) {
+      this.todoAppService.onEdit($todoItem);
+    }
+  };
+
   function isNotComplete(target) {
-    const $todoItem = target.closest(".todo-item");
+    const $todoItem = findItem(target);
     return !$todoItem.classList.contains("completed");
   }
 
@@ -53,7 +64,7 @@ function TodoController() {
     }
 
     if (event.key === "Escape") {
-      offEdit(event)
+      offEdit(event);
     }
   };
 
@@ -65,7 +76,7 @@ function TodoController() {
 
   const offEdit = (event) => {
     const $todoItem = event.target.closest(".todo-item");
-
+    $todoItem.classList.remove("editing");
   };
 
   this.clear = function () {
@@ -74,7 +85,8 @@ function TodoController() {
 
   this.init = function () {
     $newTodoTitle.addEventListener("keyup", onKeyupTodoTitle);
-    $todoList.addEventListener("dblclick", onClickTodoList);
+    $todoList.addEventListener("click", onClickTodoList);
+    $todoList.addEventListener("dblclick", onDoubleClickTodoList);
     $todoList.addEventListener("keyup", onKeyupTodoItem);
   };
 }
