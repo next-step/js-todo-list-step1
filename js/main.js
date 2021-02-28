bcea550c4d125183b1eabcb4f5aa52e01921938a
Jay <jay@myfair.co>
2021-03-01 00:45:47 +0900

    완료한 todoItem에 completed class 추가

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 function todoList (){
     const $input = document.querySelector('#new-todo-title');
     const $todoList = document.querySelector('#todo-list');
-    $input.addEventListener('keypress', (event) => {
-        if(event.target.value != ''  && event.key === 'Enter'){
-            addTodo (event);
-        }
-    });
     function addTodo (event){
         const title = event.target.value;
         const newTodoItem = document.createElement('li');
@@ -19,6 +14,23 @@ function todoList (){
                 <input class="edit" value="${title}" />
             `
         this.event.target.value = ''  ;
+        const checkBtn = document.querySelector('.toggle');
+        checkBtn.addEventListener('click', checkComplete => {
+            if(checkBtn.checked){
+                newTodoItem.classList.add('completed');
+            } else {
+                newTodoItem.classList.remove('completed');
+            }
+        });
+        
     }
+  
+    $input.addEventListener('keypress', (event) => {
+        if(event.target.value != ''  && event.key === 'Enter'){
+            addTodo (event);
+        }
+    });
+  
 }
 const todoApp = new todoList();
+// - [ ] todo list의 체크박스를 클릭하여 complete 상태로 변경. (li tag 에 completed class 추가, input 태그에 checked 속성 추가) 
