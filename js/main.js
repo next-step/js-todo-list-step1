19e5aa5fd22835f451efeb84b381b865fb671a49
Jay <jay@myfair.co>
2021-02-28 21:37:45 +0900

    feat: todolist에 item 추가 기능 구현

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function todoList (){
+    const $input = document.querySelector('#new-todo-title');
+    const $todoList = document.querySelector('#todo-list');
+    $input.addEventListener('keypress', (event) => {
+        if(event.key === 'Enter'){
+            addTodo (event);
+        }
+    });
+    function addTodo (event){
+        const title = event.target.value;
+        const newTodoItem = document.createElement('li');
+        $todoList.appendChild(newTodoItem);
+            newTodoItem.innerHTML=`
+                <div class="view">
+                    <input class="toggle" type="checkbox"/>
+                    <label class="label">${title}</label>
+                    <button class="destroy"></button>
+                </div>
+                <input class="edit" value="${title}" />
+            `
+        this.event.target.value = ''  ;
+    }
+}
+const todoApp = new todoList();
