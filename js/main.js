f24a44bdf860bef2f749dbefaf797500b36ac0d2
Jay <jay@myfair.co>
2021-03-01 14:33:02 +0900

    더블클릭시 input 모드로 변경

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 function todoList (){
     const $input = document.querySelector('#new-todo-title');
     const $todoList = document.querySelector('#todo-list');
+    const $todoCount = document.querySelector('.todo-count');
+    let numTodo;
     function addTodo (event){
-        const title = event.target.value;
+        let title = event.target.value;
         const newTodoItem = document.createElement('li');
         $todoList.appendChild(newTodoItem);
             newTodoItem.innerHTML=`
@@ -24,16 +26,29 @@ function todoList (){
                 newTodoItem.classList.remove('completed');
             }
         });
-        $deleteBtn.addEventListener('click', (event) => {
+        $deleteBtn.addEventListener('click', () => {
            $todoList.removeChild(newTodoItem);
+           countTodo ();
         });
-        newTodoItem.addEventListener('click', (event) => {
-            // console.log('dfs');
-           console.log( event.target);
+        newTodoItem.addEventListener('click', () => {
             newTodoItem.classList.add('editing');
          });
+         newTodoItem.addEventListener('keyup', (event) => {
+            const editInputElem = newTodoItem.querySelector(".edit")
+            const titleLabelElem =  newTodoItem.querySelector(".label")
+            if (event.key === 'Escape'){
+                newTodoItem.classList.remove('editing');
+            } 
+            if (event.key === 'Enter'){
+                titleLabelElem.innerText = editInputElem.value;
+            }
+         });
+         countTodo ();
     }
-  
+    function countTodo (){
+        let numTodo = $todoList.childElementCount;
+        $todoCount.querySelector('strong').innerText= numTodo;
+     }
     $input.addEventListener('keypress', (event) => {
         if(event.target.value != ''  && event.key === 'Enter'){
             addTodo (event);
