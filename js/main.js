6dc589f0e99de33385a8e4cd0c2f9dd3fe9fa285
Jay <jay@myfair.co>
2021-03-04 22:28:31 +0900

    refactor :

--- js/main.js
+++ js/main.js
@@ -2,11 +2,24 @@ function todoList() {
   const $input = document.querySelector("#new-todo-title");
   const $todoList = document.querySelector("#todo-list");
   const $todoCount = document.querySelector(".todo-count");
-  let numTodo;
+  const title = [];
 
-  function addTodo() {
+  function addTodo(event) {
     if (event.target.value != "" && event.key === "Enter") {
+      title.push(event.target.value);
+      makeLI();
     }
+  }
+
+  function makeLI() {
+    let htmlliElement = document.createElement("li");
+    htmlliElement.innerHTML=" <div class=\"view\">\n" +
+        "      <input class=\"toggle\" type=\"checkbox\"/>\n" +
+        "      <label class=\"label\">새로운 타이틀</label>\n" +
+        "      <button class=\"destroy\"></button>\n" +
+        "    </div>\n" +
+        "    <input class=\"edit\" value=\"새로운 타이틀\" />"
+    $todoList.appendChild();
   }
 
   function countTodo() {
@@ -14,14 +27,15 @@ function todoList() {
     $todoCount.querySelector("strong").innerText = numTodo;
   }
 
-  $todoList.addEventListener("click", (event) => {
+  function deleteItem(event) {
     const target = event.target;
     if (target.classList.contains("destroy")) {
       $todoList.removeChild(newTodoItem);
       countTodo();
     }
-  });
+  }
 
+  $todoList.addEventListener("click", deleteItem);
   $input.addEventListener("keypress", addTodo);
 }
 const todoApp = new todoList();
