3f94df626161cf4f33bcbdb03ba90129147e6566
sanam <simian114@gmail.com>
2021-04-07 18:36:34 +0900

    feat: db에 save 메서드 추가 db 에 저장할 때 해당 db에만 저장하는게 아니라 localStroage에도 저장하게 만들어줬다. 그리고 id 값을 자동으로 부여하도록 만들었다.

--- src/javascript/db.js
+++ src/javascript/db.js
@@ -1,6 +1,26 @@
 export default class DB {
-  constructor() {
-    this.storage = localStorage;
-    console.log('DB was created');
+  constructor(userName) {
+    // NOTE: todo 에는 id, completed여부, content 가 들어온다.
+    this._id = 0;
+    this.userName = userName;
+    const temp = localStorage[this.userName];
+    this.todos = temp ? JSON.parse(temp) : new Array();
+  }
+
+  save(item) {
+    this.increaseId();
+    item.id = this.getId();
+    this.todos.push(item);
+    localStorage[this.userName] = JSON.stringify(this.todos);
+    console.log(localStorage[this.userName]);
+    return item;
+  }
+
+  increaseId() {
+    this._id++;
+  }
+
+  getId() {
+    return this._id;
   }
 }
