0869bc11709786f439b3813186b02d68329e6f77
sanam <simian114@gmail.com>
2021-04-07 18:40:07 +0900

    feat: create 메서드 추가 들어온 데이터의 valid 를 체크하고 db에 저장한다.

--- src/javascript/model/model.js
+++ src/javascript/model/model.js
@@ -1,8 +1,16 @@
-import DB from '../db.js';
-
 export default class Model {
-  constructor() {
-    this.stroage = new DB();
-    console.log('Model was created');
+  constructor(storages) {
+    this.stroages = storages;
+  }
+
+  async create(value, userName) {
+    if (value.length === 0) {
+      throw new RangeError('value is empty!!');
+    }
+    const storage = this.stroages[userName];
+    return storage.save({
+      content: value,
+      completed: false,
+    });
   }
 }
