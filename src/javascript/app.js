2033a7af93eb274965177b1ea5c5c7d06a223e40
sanam <simian114@gmail.com>
2021-04-07 18:08:54 +0900

    refactor: storages를 model이 아닌 app에서 관리하도록 수정 기존에는 model 안에서 단 하나의 storage 만을 만들었었는데 이렇게 될 경우 user가 여러 명인 경우에 대해서는 대응하지 못한다. 따라서 app 객체가 생성됨과 동시에 localStorage 에서 userNames 키를 이용해 userNames 를 가져오고 해당 값들을 이용해 DOM 요소를 조작, 및 stroage 를 만들어서 관리하게한다.

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,13 +1,21 @@
 import Model from './model/model.js';
 import View from './view/view.js';
 import Controller from './controller/controller.js';
+import DB from './db.js';
 
+// NOTE: userNames 는 이후의 과제를 고려해서 작성해둠.
+// TODO: 이후 userNames 는 localStorages에서 `userNames` 키로 가져오기
 class App {
   constructor() {
-    this.model = new Model();
+    this.storages = {};
+    this.userNames = ['default', 'test', 'test2'];
+    this.userNames.forEach((name) => {
+      this.storages[name] = new DB(name);
+    });
+    this.model = new Model(this.storages);
     this.view = new View();
     this.controller = new Controller(this.model, this.view);
   }
 }
 
-new App();
+window.todo = new App();
