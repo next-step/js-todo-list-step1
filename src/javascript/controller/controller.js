797bc55ac154e72a5ba73ea96823cb5799142d64
sanam <simian114@gmail.com>
2021-04-07 18:33:36 +0900

    feat: addItem 이 메서드의 역할은 2가지다 1. model에게 item 을 추가하라고 알려주고 2. view에게 화면을 업데이트 할 것을 말해준다.
    view 에 렌더링 되는건 model 에 성공적으로 추가가 되었을 경우이므로
    프로미스를 이용해서 성공한 경우에만 then 을 이용해서 view 의 render
    메서드를 호출해준다.

--- src/javascript/controller/controller.js
+++ src/javascript/controller/controller.js
@@ -2,6 +2,21 @@ export default class Controller {
   constructor(model, view) {
     this.model = model;
     this.view = view;
-    console.log('controller was created');
+
+    this.view.setEventListener('add', (value) => {
+      this.addItem(value);
+    });
+  }
+
+  addItem(value) {
+    this.model
+      .create(value, this.view.currentUser)
+      .then((item) => {
+        this.view.renderTodo(item);
+        // TODO: input box 클리어하자.
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   }
 }
