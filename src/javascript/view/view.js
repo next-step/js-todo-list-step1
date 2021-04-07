07057c40137d3df7ff6f35c062cfe6d0cdf8593e
sanam <simian114@gmail.com>
2021-04-07 18:29:02 +0900

    feat: add 이벤트 핸들러 등록 사용자가 enter 를 입력하면 컨트롤러의 생성자에서 호출했었던 setEventListener 으로 view 의 input 에 걸어 놓았던 이벤트를 잡고 처리할 수 있게 됐다. 엔터가 들어오면 입력된 값을 가지고 등록된 callBack 함수를 호출한다. 들어온 callback 함수는 controller 의 addItem 함수다.

--- src/javascript/view/view.js
+++ src/javascript/view/view.js
@@ -1,5 +1,44 @@
+/* 
+  NOTE: 현재는 currentUser 를 default로 바로 등록하고 있지만,
+        이후에는 DOM 요소로 user를 선택할 수 있게 수정해야한다.
+*/
 export default class View {
   constructor() {
-    console.log('View was created');
+    this.todoList = document.querySelector('#todo-list');
+    this.input = document.querySelector('#new-todo-title');
+    this.currentUser = 'default';
+    console.log(this.todoList);
+  }
+
+  setEventListener(eventName, callback) {
+    switch (eventName) {
+      case 'add':
+        // NOTE: callback == Controller.addItem
+        this.input.addEventListener('keypress', (event) => {
+          if (event.key !== 'Enter') {
+            return;
+          } else if (event.key === 'ESC') {
+            // TODO: clear input box
+            return;
+          }
+          callback(this.input.value);
+        });
+        break;
+      default:
+        console.log('eventName is not handling');
+    }
+  }
+
+  renderTodo(item) {
+    console.log(this);
+    let li = `<li data-id=${item.id}>
+                <div class="view">
+                  <input class="toggle" type="checkbox"/>
+                  <label class="label">${item.content}</label>
+                  <button class="destroy"></button>
+                </div>
+                <input class="edit" value="새로운 타이틀" />
+              </li>`;
+    this.todoList.insertAdjacentHTML('beforeend', li);
   }
 }
