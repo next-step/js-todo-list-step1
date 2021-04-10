a27d5d36fa9da2eada26bc070de7aa44af0273d4
sanam <simian114@gmail.com>
2021-04-10 18:23:14 +0900

    refactor: id 동작 수정 이제 db 자체에서는 todos 배열을 관리하지 않기 때문에 id의 동작 방식을 수정했어야했음. 따라서 save 될 때 마다 + 1을 하는게 아닌 save 의 인자로 들어오는 모든 todos 의 length 를 id 로 세팅하게 만들었다.

--- src/javascript/db.js
+++ src/javascript/db.js
@@ -3,7 +3,6 @@ export default class DB {
     // NOTE: todo 에는 id, completed여부, content 가 들어온다.
     this.userName = userName;
     const temp = localStorage[this.userName];
-    console.log(temp);
     this.todos = temp ? JSON.parse(temp) : new Array();
     this.setId(this.todos.length);
   }
@@ -16,17 +15,8 @@ export default class DB {
   }
 
   save(todos) {
+    this.setId(todos.length);
     localStorage[this.userName] = JSON.stringify(todos);
-  }
-
-  // NOTE: 인자로 id 를 받는게 아니라 todo 자체를 받고 하는게 더 좋지 않을까?
-  // NOTE: 여기서 에러가 발생하는 경우가 있을까? 굳이 async 를 사용하는게 좋은건가?
-  remove(todos) {
-    localStorage[this.userName] = todos;
-  }
-
-  increaseId() {
-    this._id++;
   }
 
   setId(num) {
@@ -37,7 +27,7 @@ export default class DB {
     return this._id;
   }
 
-  getTodos() {
-    return this.todos.filter((todo) => !todo.removed);
+  increaseId() {
+    this._id++;
   }
 }
