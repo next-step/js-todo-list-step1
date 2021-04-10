9b8cdc345ea07937970e6ca6556e7cdc1a3ae4ae
sanam <simian114@gmail.com>
2021-04-10 17:20:14 +0900

    feat: model 에 상태관리를 위한 todos 변수를 추가

--- src/javascript/model/model.js
+++ src/javascript/model/model.js
@@ -1,13 +1,25 @@
 export default class Model {
   constructor(storages) {
-    this.stroages = storages;
+    // NOTE: this.storages 는 실제 DB
+    // NOTE: this.todos 는 Model 에서 관리하는 데이터.
+    // NOTE: 앞으로 이 데이터에 변화가 생기면 그 변화를 storages 에 저장하고,
+    // NOTE: 뷰를 변화시키는 로직을 실행하면 된다.
+    // NOTE: 그리고 이전에는 storages 에서 모든 로직을 다 수행하고 있었는데
+    // NOTE: 그 로직 전부를 여기에서 실행하고 db는 정말 데이터를 저장하는 용도로만 사용하자.
+    this.storages = storages;
+    this.todos = {};
+    for (let userName in storages) {
+      this.todos[userName] = storages[userName].todos;
+      console.log(userName);
+      console.log(this.todos[userName]);
+    }
   }
 
   async create(value, userName) {
     if (value.length === 0) {
       throw new RangeError('value is empty!!');
     }
-    const storage = this.stroages[userName];
+    const storage = this.storages[userName];
     return storage.save({
       content: value,
       completed: false,
@@ -15,22 +27,22 @@ export default class Model {
   }
 
   getTodosOf(userName) {
-    const storage = this.stroages[userName];
+    const storage = this.storages[userName];
     return storage.getTodos();
   }
 
   async remove(todoId, userName) {
-    const storage = this.stroages[userName];
+    const storage = this.storages[userName];
     return storage.remove(todoId);
   }
 
   async updateStatus(todoId, userName) {
-    const storage = this.stroages[userName];
+    const storage = this.storages[userName];
     return storage.updateStatus(todoId);
   }
 
   async updateContent(todoId, content, userName) {
-    const storage = this.stroages[userName];
+    const storage = this.storages[userName];
     return storage.updateContent(todoId, content);
   }
 }
