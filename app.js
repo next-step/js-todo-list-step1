a5777b22d38c3b87bc7e3d7f735d1c001d9ff433
kkang <kkang@student.42seoul.kr>
2021-04-11 22:48:04 +0900

    App() 함수를 만들어 refactor 진행
    - 루트역할을 할 App 함수 생성

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 /* eslint-disable import/extensions */
 import { count, filters, todoList, newTodoTitle } from './constant.js';
 
-const todos = [];
+function App() {
+  const todos = [];
 
-function handleKeyup(event) {
-  if (event.keyCode === 13 && event.target.value !== '') {
-    todos.push({
-      name: event.target.value,
-      completed: false,
-      key: Date.now()
-    });
-    event.target.value = '';
+  function handleKeyup(event) {
+    if (event.keyCode === 13 && event.target.value !== '') {
+      todos.push({
+        name: event.target.value,
+        completed: false,
+        key: Date.now()
+      });
+      event.target.value = '';
+    }
   }
+
+  function init() {
+    // loading
+
+    // eventHandler
+    newTodoTitle.addEventListener('keyup', handleKeyup);
+  }
+  init();
 }
 
-function init() {
-  // loading
-
-  // eventHandler
-  newTodoTitle.addEventListener('keyup', handleKeyup);
-}
-init();
+App();
