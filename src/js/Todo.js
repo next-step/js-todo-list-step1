fa6bc88b9f87c1594fe273b2de7b3aae49a05c61
bbbnam <bbbnam@naver.com>
2021-04-12 02:03:00 +0900

    feat : 클래스 메소드화 및 기본 요구사항 구현

--- src/js/Todo.js
+++ src/js/Todo.js
@@ -1,91 +1,57 @@
 class Todo {
-    constructor(todo) {
-        const todoList = document.createElement('li');
-        const view = document.createElement('div');
-        const toggle = document.createElement('input');
-        const label = document.createElement('label');
-        const labelTest = document.createTextNode(todo);
-        const destroy = document.createElement('button');
-        const edit = document.createElement('input');
-        let isDone = false;
-
-        view.className = 'view';
-        toggle.className = 'toggle';
-        label.className = 'label';
-        destroy.className = 'destroy';
-        edit.className = 'edit';
-        toggle.setAttribute("type", "checkbox");
-        label.append(labelTest);
-        edit.setAttribute("value", todo);
-        view.append(toggle,label,destroy);
-        
-        todoList.append(view, edit);
-        this._todoList = todoList;
+    constructor() {
+        this._todoItems = [];
+        this._todoInput = document.getElementById('new-todo-title');
+        this._todoList = document.getElementById('todo-list');
+        this._todoFilter = document.querySelector('.filters');
+        this._todoCount = document.querySelector('.todo-count').firstElementChild;
     }
 
-    get makeTodoList() {
-        return this._todoList;
+    add(todo) {
+        this._isDone = false;
+        this._todoLi = document.createElement('li');
+        this._view = document.createElement('div');
+        this._toggle = document.createElement('input');
+        this._label = document.createElement('label');
+        this._labelTest = document.createTextNode(todo);
+        this._destroy = document.createElement('button');
+        this._edit = document.createElement('input');
+        this._view.className = 'view';
+        this._toggle.className = 'toggle';
+        this._label.className = 'label';
+        this._destroy.className = 'destroy';
+        this._edit.className = 'edit';
+        this._toggle.setAttribute("type", "checkbox");
+        this._label.append(this._labelTest);
+        this._edit.setAttribute("value", todo);
+        this._view.append(this._toggle, this._label, this._destroy);
+
+        this._todoLi.append(this._view, this._edit);
+        this._todoList.append(this._todoLi);
+        this._todoCount.innerText = this.todoCount;
+        this._todoItems.push(this._todoLi);
     }
-}
 
-const addTask = (todo) => {
-    const newTodo = new Todo(todo);
-    const todoList = document.getElementById('todo-list');
-    countTodoList(todoList, 'add');
-    todoList.append(newTodo.makeTodoList);
-};
-
-const editTask = ({target, key},labelArea, originalValue) => {
-    switch(key) {
-        case 'Enter' :
-            labelArea.innerText = target.value;
-            return stopEditing(target);
-        case 'Escape' :
-            target.value = originalValue;
-            return stopEditing(target);
+    destroy(target, li) {
+        target.removeChild(li);
+        this._todoCount.innerText = this.todoCount;
+        this.updateItems();
     }
-};
 
-const destroyTask = (target, li) => {
-    const todoList = document.getElementById('todo-list');
-    countTodoList(todoList, 'destroy');
-    target.removeChild(li);
-}
-
-const countTodoList = (todoList, act) => {
-    const todoCountHtml = document.querySelector('.todo-count').firstElementChild;
-    let todoCount = 0;
-    switch(act) {
-        case 'add' :
-            todoCount = todoList.childNodes.length + 1;
-            todoCountHtml.innerText = todoCount;
-            return;
-        case 'destroy' :
-            todoCount = todoList.childNodes.length - 1;
-            todoCountHtml.innerText = todoCount;
-            return;
+    edit({target, key},labelArea, originalValue) {
+        switch(key) {
+            case 'Enter' :
+                labelArea.innerText = target.value;
+                target.closest('li').removeAttribute('class');
+                return;
+            case 'Escape' :
+                target.value = originalValue;
+                target.closest('li').removeAttribute('class');
+                return;
+        }
     }
-}
 
-const stopEditing = (target) => {
-    target.closest('li').removeAttribute('class');
-}
-
-const todoInput = document.getElementById('new-todo-title');
-const todoListUl = document.querySelector('.todo-list'); 
-
-todoInput.addEventListener('keydown', function() {
-    if (window.event.keyCode == 13) {
-        addTask(this.value);
-        this.value = '';
-    }
-});
-
-todoListUl.addEventListener('click', function(event) {
-    const toggleCheck = event.target.closest('.toggle');
-    const destroyButton = event.target.closest('.destroy');
-    const li = event.target.closest('li');
-    if (toggleCheck) {
+    changeTodoState(li, toggleCheck) {
         if (li.className == '') {
             li.classList.add('completed');
             toggleCheck.setAttribute('checked', '');
@@ -93,18 +59,114 @@ todoListUl.addEventListener('click', function(event) {
             li.removeAttribute('class');
             toggleCheck.removeAttribute('checked');
         }
+        this.updateItems();
     }
-    if (destroyButton) {
-        destroyTask(this, li);
+    
+    updateItems() {
+        this._todoItems = [];
+        this._todoList.childNodes.forEach(element => {
+            this._todoItems.push(element);
+        });
+    }
+
+    initTodoList() {
+        while (this._todoList.firstChild) {
+            this._todoList.removeChild(this._todoList.firstChild);
+        }
+    }
+
+    allList() {
+        this.initTodoList();
+        this._todoItems.forEach(element=> {
+            this._todoList.append(element);
+        });
+    }
+
+    activeList() {
+        this.initTodoList();
+        this._todoItems.forEach(element=> {
+            if (element.className != 'completed') {
+                this._todoList.append(element);
+            }
+        });
+    }
+
+    completedList() {
+        this.initTodoList();
+        this._todoItems.forEach(element=> {
+            if (element.className == 'completed') {
+                this._todoList.append(element);
+            }
+        });
+    }
+
+    get todoCount() {
+        return this._todoList.childNodes.length;
+    }
+
+    get todoInput() {
+        return this._todoInput;
+    }
+
+    get todoList() {
+        return this._todoList;
+    }
+
+    get todoFilter() {
+        return this._todoFilter;
+    }
+}
+const todoApp = new Todo();
+
+todoApp.todoInput.addEventListener('keydown', function() {
+    if (window.event.keyCode == 13) {
+        todoApp.add(this.value);
+        this.value = '';
     }
 });
 
-todoListUl.addEventListener('dblclick', function(event) {
+todoApp.todoList.addEventListener('click', function(event) {
+    const toggleCheck = event.target.closest('.toggle');
+    const destroyButton = event.target.closest('.destroy');
+    const li = event.target.closest('li');
+    if (toggleCheck) {
+        todoApp.changeTodoState(li, toggleCheck);
+    }
+    if (destroyButton) {
+        todoApp.destroy(this, li);
+    }
+});
+
+todoApp.todoList.addEventListener('dblclick', function(event) {
     const labelArea = event.target.closest('.label');
     const li = event.target.closest('li');
     if (!labelArea) return;
 
     const originalValue = labelArea.innerText;
     li.classList.add('editing');
-    li.addEventListener('keyup', ({target, key}) => editTask({target, key},labelArea, originalValue))
+    li.addEventListener('keyup', ({target, key}) => todoApp.edit({target, key},labelArea, originalValue))
 });
+
+todoApp.todoFilter.addEventListener('click', function(event) {
+    switch(event.target.id) {
+        case 'all' :
+            todoApp.allList();
+            initClassList(event, this);
+            return;
+        case 'active' :
+            todoApp.activeList();
+            initClassList(event, this);
+            return;
+        case 'completed' :
+            todoApp.completedList();
+            initClassList(event, this);
+            return;
+    }
+})
+
+const initClassList = (event, target) => {
+    for (let item of target.children) {
+        item.children[0].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+}
