cf60854d89967206ea84789b93f413923f5fdc7d
sangwon <junothyme@gmail.com>
2021-04-11 12:17:45 +0900

    feat: todo list의 item 개수를 리스트 하단에 보여주기

--- src/component/TodoApp.js
+++ src/component/TodoApp.js
@@ -1,17 +1,21 @@
 import TodoInput from "./TodoInput.js";
 import TodoItemModel from "./model/TodoItemModel.js";
 import TodoList from "./TodoList.js";
+import TodoCount from "./TodoCount.js";
 
 function TodoApp() {
 	this.todoItems = [];
+	const listTarget = document.querySelector("#todo-list");
+	const todoList = new TodoList({ target: listTarget, status: null, complete: null });
+
+	const countTarget = document.querySelector(".todo-count strong");
+	const todoCount = new TodoCount({ target: countTarget });
 
 	this.setState = (updatedItems) => {
 		this.todoItems = updatedItems;
 		todoList.setState(this.todoItems);
+		todoCount.setState(this.todoItems);
 	};
-
-	const target = document.querySelector("#todo-list");
-	const todoList = new TodoList({ target });
 
 	new TodoInput({
 		onAdd: (contents) => {
