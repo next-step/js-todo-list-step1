7cead3a46af48c31552b8d086ff75dfce9529380
JooJaeSeong <joojae26@gmail.com>
2021-04-12 19:38:19 +0900

    Use 'change event' instead of 'keydown event' - "Enter" 입력 조건, String 빈 값 확인 조건 제거

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,32 @@
-// input 가져오기
 const todoTitleInput = document.getElementById("new-todo-title");
 const todoList = document.getElementById("todo-list");
-// Enter key Event 설정
 
 const enterTitle = (event) => {
-  if (event.key === "Enter") {
-    const inputText = todoTitleInput.value;
-    if (inputText !== "") {
-      const list = document.createElement("li");
-      const view = document.createElement("div");
-      const checkBox = document.createElement("input");
-      const label = document.createElement("label");
-      const finBtn = document.createElement("button");
-      const edit = document.createElement("input");
+  const inputText = todoTitleInput.value;
+  const list = document.createElement("li");
+  const view = document.createElement("div");
+  const checkBox = document.createElement("input");
+  const label = document.createElement("label");
+  const finBtn = document.createElement("button");
+  const edit = document.createElement("input");
 
-      todoList.appendChild(list);
-      list.appendChild(view);
-      list.appendChild(edit);
-      view.appendChild(checkBox);
-      view.appendChild(label);
-      view.appendChild(finBtn);
+  todoList.appendChild(list);
+  list.appendChild(view);
+  list.appendChild(edit);
+  view.appendChild(checkBox);
+  view.appendChild(label);
+  view.appendChild(finBtn);
 
-      list.classList.add("false");
-      view.classList.add("view");
-      edit.classList.add("edit");
-      checkBox.classList.add("toggle");
-      checkBox.type = "checkbox";
-      label.classList.add("label");
-      finBtn.classList.add("destroy");
+  list.classList.add("false");
+  view.classList.add("view");
+  edit.classList.add("edit");
+  checkBox.classList.add("toggle");
+  checkBox.type = "checkbox";
+  label.classList.add("label");
+  finBtn.classList.add("destroy");
 
-      label.textContent = inputText;
-      todoTitleInput.value = "";
-    }
-  }
+  label.textContent = inputText;
+  todoTitleInput.value = "";
 };
 
-todoTitleInput.addEventListener("keydown", enterTitle);
+todoTitleInput.addEventListener("change", enterTitle);
