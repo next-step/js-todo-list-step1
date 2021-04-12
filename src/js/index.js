69879844c2be1133d8895e0a96008cb7262256c2
JooJaeSeong <joojae26@gmail.com>
2021-04-12 22:20:22 +0900

    Fix : Reuse keydown event & Add checkbox toggle function - change event는 focusing의 문제로 제외함 - keydown event가 한국어 입력 시 두 번 발생하는 문제로 event.isComposing을 false 설정 후 해결 - checkbox click 여부에 따라 list class명, checkbox input checked 변경

--- src/js/index.js
+++ src/js/index.js
@@ -2,31 +2,48 @@ const todoTitleInput = document.getElementById("new-todo-title");
 const todoList = document.getElementById("todo-list");
 
 const enterTitle = (event) => {
-  const inputText = todoTitleInput.value;
-  const list = document.createElement("li");
-  const view = document.createElement("div");
-  const checkBox = document.createElement("input");
-  const label = document.createElement("label");
-  const finBtn = document.createElement("button");
-  const edit = document.createElement("input");
+  if (!event.isComposing && event.key === "Enter") {
+    const inputText = todoTitleInput.value;
+    if (inputText !== "") {
+      const list = document.createElement("li");
+      const view = document.createElement("div");
+      const checkBox = document.createElement("input");
+      const label = document.createElement("label");
+      const finBtn = document.createElement("button");
+      const edit = document.createElement("input");
 
-  todoList.appendChild(list);
-  list.appendChild(view);
-  list.appendChild(edit);
-  view.appendChild(checkBox);
-  view.appendChild(label);
-  view.appendChild(finBtn);
+      todoList.appendChild(list);
+      list.appendChild(view);
+      list.appendChild(edit);
+      view.appendChild(checkBox);
+      view.appendChild(label);
+      view.appendChild(finBtn);
 
-  list.classList.add("false");
-  view.classList.add("view");
-  edit.classList.add("edit");
-  checkBox.classList.add("toggle");
-  checkBox.type = "checkbox";
-  label.classList.add("label");
-  finBtn.classList.add("destroy");
+      list.className = "false";
+      view.className = "view";
+      edit.className = "edit";
+      checkBox.className = "toggle";
+      checkBox.type = "checkbox";
+      label.className = "label";
+      finBtn.className = "destroy";
 
-  label.textContent = inputText;
-  todoTitleInput.value = "";
+      label.textContent = inputText;
+      todoTitleInput.value = "";
+
+      checkBox.addEventListener("click", checkFin);
+
+      function checkFin(event) {
+        console.dir(checkBox.checked);
+        if (list.className === "false") {
+          list.className = "completed";
+          checkBox.setAttribute("checked", true);
+        } else {
+          list.className = "false";
+          checkBox.setAttribute("checked", false);
+        }
+      }
+    }
+  }
 };
 
-todoTitleInput.addEventListener("change", enterTitle);
+todoTitleInput.addEventListener("keydown", enterTitle);
