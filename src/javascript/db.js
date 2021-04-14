388a7be8fa2ebcc670a6ba6583e3a29de7fef834
sanam <simian114@gmail.com>
2021-04-14 23:08:43 +0900

    refactor: ?? 을 이용한 db 데이터 초기화

--- src/javascript/db.js
+++ src/javascript/db.js
@@ -2,8 +2,7 @@ export default class DB {
   constructor(userName) {
     // NOTE: todo 에는 id, completed여부, content 가 들어온다.
     this.userName = userName;
-    const temp = localStorage[this.userName];
-    this.todos = temp ? JSON.parse(temp) : new Array();
+    this.todos = JSON.parse(localStorage.getItem(this.userName)) ?? [];
     this._setId(this.todos.length);
   }
 
