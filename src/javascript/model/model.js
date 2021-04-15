7966a46a133ca3d27e5b879ededec63bac9d7c47
sanam <simian114@gmail.com>
2021-04-15 13:57:57 +0900

    refactor: remove, update 중복을 _update 메서드로 분리

--- src/javascript/model/model.js
+++ src/javascript/model/model.js
@@ -1,4 +1,5 @@
 import { ERROR_MESSAGE } from '../utils/constants.js';
+import { hasKey } from '../utils/utils.js';
 export default class Model {
   constructor(storages) {
     this._storages = storages;
@@ -24,39 +25,28 @@ export default class Model {
   }
 
   async remove(todoId, userName) {
-    this._setCurrentStorage(userName);
-    const targetTodo = this._findTodoById(todoId, userName);
-    if (!targetTodo) {
-      throw new Error(ERROR_MESSAGE.UNEXPECTED);
-    }
-    targetTodo.removed = true;
-    this._currentStorage.save(this._todos[userName]);
-    return targetTodo;
+    return this._update({
+      todoId,
+      userName,
+      cmd: 'remove',
+    });
   }
 
   async updateStatus(todoId, userName) {
-    this._setCurrentStorage(userName);
-    const targetTodo = this._findTodoById(todoId, userName);
-    if (!targetTodo) {
-      throw new Error(ERROR_MESSAGE.UNEXPECTED);
-    }
-    targetTodo.completed = !targetTodo.completed;
-    this._currentStorage.save(this._todos[userName]);
-    return targetTodo;
+    return this._update({
+      todoId,
+      userName,
+      cmd: 'updateStatus',
+    });
   }
 
   async updateContent(todoId, content, userName) {
-    this._setCurrentStorage(userName);
-    const targetTodo = this._findTodoById(todoId, userName);
-    if (!targetTodo) {
-      throw new Error(ERROR_MESSAGE.UNEXPECTED);
-    }
-    if (!content.length) {
-      throw targetTodo;
-    }
-    targetTodo.content = content;
-    this._currentStorage.save(this._todos[userName]);
-    return targetTodo;
+    return this._update({
+      todoId,
+      userName,
+      content,
+      cmd: 'updateContent',
+    });
   }
 
   getTodosOf(userName) {
@@ -69,5 +59,24 @@ export default class Model {
 
   _findTodoById(id, userName) {
     return this._todos[userName].find((todo) => todo.id === id);
+  }
+
+  _update(params) {
+    const targetTodo = this._findTodoById(params.todoId, params.userName);
+    if (!targetTodo) {
+      throw new Error(ERROR_MESSAGE.UNEXPECTED);
+    }
+    if (hasKey(params, 'content') && !params.content) {
+      throw targetTodo;
+    }
+    const obj = {
+      remove: () => (targetTodo.removed = true),
+      updateStatus: () => (targetTodo.completed = !targetTodo.completed),
+      updateContent: () => (targetTodo.content = params.content),
+    };
+    obj[params.cmd]();
+    this._setCurrentStorage(params.userName);
+    this._currentStorage.save(this._todos[params.userName]);
+    return targetTodo;
   }
 }
