6f3759b5a5f79e2fe40daeae4172d7ea04f5d2ac
Heejay Kong <heejay.dev@gmail.com>
2021-05-24 15:25:28 +0900

    [8기 heejaykong] TodoList with CRUD (#169)
    * initialize
    
    * func: add todo, delete todo, checkbox toggle
    
    * func: add todo-edit-function when double clicked
    
    * func: add localStorage CRUD && count
    
    * func: add 'completed' attribute to Todo && remember completed when painted
    
    * 1차 마무리
    
    * fix: refactor(replace deprecated codes && duplicated codes

---
+++ index.js
@@ -0,0 +1,123 @@
+const $ = (selector) => document.querySelector(selector);
+const $todoInput = $('.new-todo');
+const $todoCount = $('.todo-count strong');
+const $activeTodosBtn = $('.active');
+const $completedTodosBtn = $('.completed');
+
+let todos = [];
+
+const paintTodo = (todoObj) => {
+  // 그리기
+  const ul = document.querySelector('.todo-list');
+  const li = document.createElement('li');
+  li.id = todoObj.id;
+  li.innerHTML = `
+    <div class="view">
+      <input class="toggle" type="checkbox"/>
+      <label class="label">${todoObj.title}</label>
+      <button class="destroy"></button>
+    </div>
+    <input class="edit" value="${todoObj.title}" />`
+  ul.appendChild(li);
+  const toggleBtn = li.querySelector('input');
+  const delBtn = li.querySelector('button');
+  const label = li.querySelector('label');
+  // 완료여부 상태대로 그리기
+  toggleBtn.checked = (todos.find(todo => todo.id === (li.id))).completed;
+  toggleBtn.checked? li.classList.add('completed') : li.classList.remove('completed');
+  // 이벤트리스너들 부착
+  toggleBtn.addEventListener('click', handleToggleClick);
+  delBtn.addEventListener('click', handleDelBtnClick);
+  label.addEventListener('dblclick', handleEditDoubleClick)
+}
+
+const createTodo = (inputValue) => {
+  const todo = {
+    id: Math.random().toString(36).substr(2, 9),
+    title: inputValue,
+    completed: false
+  };
+  todos.push(todo);
+  saveTodos();
+  paintTodo(todo);
+  countTodos();
+}
+
+const handleSubmit = (e) => {
+  if ($todoInput.value && e.key === 'Enter'){
+    createTodo($todoInput.value)
+    $todoInput.value = '';
+  }
+}
+
+const paintAsToggled = (targetLi, isCompleted) => {
+  isCompleted? targetLi.setAttribute('checked', 'on') : targetLi.removeAttribute('checked');
+  targetLi.classList.toggle('completed');
+}
+
+const handleToggleClick = (e) => {
+  const targetLi = e.target.closest('li');
+  const clickedTodoObj = todos.find(todo => todo.id === (targetLi.id));
+  const isCompleted = clickedTodoObj.completed;
+  paintAsToggled(targetLi, isCompleted);
+  // 토글결과 localStorage에도 저장
+  clickedTodoObj.completed = !clickedTodoObj.completed;
+  saveTodos();
+}
+
+const handleDelBtnClick = (e) => {
+  const targetLi = e.target.closest('li');
+  targetLi.parentNode.removeChild(targetLi);
+  const id = e.target.closest('li').id;
+  const result = todos.filter(todo => todo.id !== (id));
+  todos = result;
+  saveTodos();
+  countTodos();
+}
+
+const handleEditDoubleClick = (e) => {
+  const targetLabel = e.target;
+  const targetLi = e.target.closest('li');
+  const editInput = targetLabel.parentNode.nextElementSibling;
+  targetLi.classList.add('editing');
+  // 수정하다가 esc와 enter 누르는 것에 따라 로직처리
+  editInput.addEventListener('keyup', (e2) => {
+    if (e2.key === 'Escape') {
+      targetLi.classList.remove('editing');
+      editInput.value = targetLabel.innerHTML;
+    }
+    if (e2.key === 'Enter'){
+      targetLi.classList.remove('editing');
+      targetLabel.innerHTML = editInput.value;
+      const theObj = todos.find(todo => todo.id === (targetLi.id));
+      theObj.title = editInput.value;
+      saveTodos();
+    }
+  });
+}
+
+const countTodos = () => {
+  $todoCount.innerHTML = todos.length;
+}
+
+const saveTodos = () => {
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+const loadTodos = () => {
+  const currentTodos = localStorage.getItem('todos');
+  if (currentTodos) {
+    const parsedTodos = JSON.parse(currentTodos);
+    todos = parsedTodos;
+    parsedTodos.map(todo => paintTodo(todo));
+  }
+  countTodos();
+}
+
+const init = () => {
+  loadTodos();
+  $todoInput.addEventListener('keyup', handleSubmit); //인풋 입력하고 엔터 누를 시 submit
+  // $activeTodosBtn.addEventListener('click', () => console.log('hey')); //마지막 요구사항 미완
+}
+
+init();
