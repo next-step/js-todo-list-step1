dd5a449b0ee5edf70ab47bff71e37aa8a7d9ccb6
Kkan9ma <kkan9ma@gmail.com>
2021-05-24 15:18:43 +0900

    [8기 강경만] TodoList with CRUD (#160)
    * 개발 환경 세팅
    
    - ESLint, Prettier, node_modules
    
    * Input 창에서 발생하는 이벤트핸들러 구현
    
    - 사용자의 enter 입력을 기준으로 분기를 나누었음.
    
    * [enter] 키 입력 시 todos가 저장되도록 구현
    
    사용자가 엔터를 누를 때 todos 배열에 입력값이 저장되도록 구현
    
    * App() 함수를 만들어 refactor 진행
    
    - 루트역할을 할 App 함수 생성
    
    * [enter] 키 입력 시 todoitem이 todo list에 추가되도록 구현
    
    - 입력한 값을 템플릿에 반영하여 목록에 렌더링되도록 구현
    
    * Refactor handleKeyup function
    
    - 조건문을 변경하여 예외 상황 발생 시 바로 함수를 종료하도록 조정
    
    * Refactor handleKeyup function
    
    - 요소 추가 부분 addTodo 함수로 분리
    
    * Todo가localstorage에 담기도록 saveTodo함수 구현
    
    localstorage 기능을 이용하여 입력한 todo값이 저장되도록 구현
    
    * todos가 알맞게 초기화되도록 구현
    
    - 로드될 때 localstorage에 값이 있는지 확인하여 [] 또는 입력되어 있는 값이 반영되도록 함
    
    * loadTodos 생성
    
    - 현 상태를 로드해주는 함수 생성
    
    * Todo를 출력해주는 함수 추가
    
    - 현재 todo 상태를 출력해주는 함수 구현
    - 이에 따라 loadTodos를 addTodo 내에도 추가하였음
    
    * eraseTodo 함수 구현
    
    - 출력 시 중복되는 부분을 지워주는 함수 추가
    
    * handleInputClick 구현
    
    - input 클릭 시 알맞게 취소선 효과가 나도록 구현
    
    * Input 버튼 클릭 시 localstorage에 반영되도록 구현
    
    - localstorage setitem을 통해 알맞게 값을 변경하였음
    
    * Delete 버튼 효과가 localstorage에 반영되도록 구현
    
    - filter, localstorage를 이용하여 삭제 효과를 구현하였음
    
    * DOM 요소 제거 추가
    
    * count 개수가 보이도록 구현
    
    * Filter를 클릭했을 때 눌린 부분이 강조되도록 변경
    
    * Fix error
    
    - querySelector가 null의 값을 가져올 수도 있는 에러 상황에 대한 처리
    
    * 함수 위치 정리
    
    * 설정 파일 추가
    
    * 비정상적인 TODO 리스트 상태관리 로직 수정
    
    - load시 localstorage 현재 상태를 기준으로 렌더링하도록 구현
    
    * Count 값이 정상적으로 출력되도록 수정
    
    * 일부 오류 수정
    
    - loadTodos가 반복적으로 호출되던 부분 삭제
    - 함수명 변경 등
    
    * ESLint에 맞게 수정
    
    * dblclick eventListener 작성
    
    * editTodos 함수 작성
    
    - 인풋창을 두번 클릭할 시 edit 모드로 진입하도록 구현
    - edit 모드에서 발생할 2가지 event를 핸들링할 함수 구조 작성
    
    * edit 모드 입력 로직 작성
    
    - edit 모드에서 입력할 시 값이 변경된 값으로 반영되도록 로직 작성
    - fix to do: edit에서 this 사용 필요-> 전체 구조 개편 요구
    
    * 전체구조 리팩토링
    
    - this 사용을 위해 각 함수를 객체 내 메소드로 변경하였음.
    - fix to do -> edit 모드 비정상적인 동작
    
    * 함수명 변경: handleInputClick -> handleToggleClick
    
    * keycode 13을 상수 ENTER로 변경
    
    * Edit 오류 fix, 상수 추가
    
    - edit 시 오류가 나던 querySelector 부분을 수정
    - ESC 코드 상수화
    
    * Fix: 잘못된 이벤트 기재 부분을 수정
    
    - dbclick 이라 되어 있는 부분을 dblclick으로 수정
    
    * Fix: 메소드 이름 수정, 완료 todo 반영 안 되던 부분 수정
    
    * Refactor: for문을 forEach로 수정
    
    * Refactor: classList.add, delete 메소드를 toggle로 변경, for문 수정
    
    * Refactor: for문 구조를 바꾸며 실행 부분을 새로운 메소드로 분리
    
    - reflectInputEditing 메소드 생성
    
    * Fix: for문 -> for of로 수정
    
    * Fix: .keyCode를 .key로 변경
    
    * Refactor: 불필요한 return 제거
    
    * Fix: 함수 이름 변경

---
+++ app.js
@@ -0,0 +1,193 @@
+/* eslint-disable import/extensions */
+import {
+  count,
+  filters,
+  todoList,
+  newTodoTitle,
+  ENTER,
+  ESC
+} from './constant.js';
+
+const App = {
+  todos: [],
+
+  makeListElement(todo) {
+    const type = todo.completed === true ? `"checkbox" checked` : 'checkbox';
+    const li = `<li class="${
+      todo.completed === true ? 'completed' : 'false'
+    }" id=${todo.id}>
+		<div class="view">
+		<input class="toggle" type=${type} id=${todo.id} />
+		<label class="label">${todo.value}</label>
+		<button class="destroy" id=${todo.id}></button>
+		</div>
+		<input class="edit" value='${todo.value}' />
+		</li>`;
+
+    return li;
+  },
+
+  renderByFilter(todo) {
+    let li;
+    const selected = document.querySelector('.selected');
+
+    if (
+      selected.classList.contains('all') ||
+      (selected.classList.contains('active') && todo.completed === false) ||
+      (selected.classList.contains('completed') && todo.completed === true)
+    ) {
+      li = App.makeListElement(todo);
+      todoList.insertAdjacentHTML('beforeend', li);
+    }
+  },
+
+  eraseTodo() {
+    const list = todoList.querySelectorAll('li');
+
+    for (const listItem of list) {
+      todoList.removeChild(listItem);
+    }
+  },
+
+  reflectInputEditing(todosArrayElement, todosArray, li) {
+    if (todosArrayElement.id !== Number(li.id)) return;
+    todosArrayElement.value = li.querySelector('.edit').value;
+    localStorage.setItem('todos', JSON.stringify(todosArray));
+    App.todos = todosArray;
+    li.classList.remove('editing');
+    App.render();
+  },
+
+  handleInputEditing(event) {
+    if (event.key !== ENTER && event.key !== ESC) return;
+    const { target } = event;
+    const li = target.closest('li');
+    const todosArray = JSON.parse(localStorage.getItem('todos'));
+
+    if (event.key === ESC) {
+      li.classList.remove('editing');
+      App.render();
+      return;
+    }
+    if (event.key === ENTER) {
+      for (const todosArrayElement of todosArray) {
+        App.reflectInputEditing(todosArrayElement, todosArray, li);
+      }
+    }
+  },
+
+  handleDblclickTodoList(event) {
+    const { target } = event;
+    const li = target.closest('li');
+    li.classList.add('editing');
+    const input = li.querySelector('.edit');
+    input.addEventListener('keyup', App.handleInputEditing);
+  },
+
+  render() {
+    App.eraseTodo();
+
+    const todosArray = JSON.parse(localStorage.getItem('todos'));
+    todosArray.forEach(todoItem => todoItem && App.renderByFilter(todoItem));
+
+    count.innerText = todoList.querySelectorAll('li').length;
+    const listItems = todoList.querySelectorAll('li');
+    for (const listItem of listItems) {
+      listItem.addEventListener('dblclick', App.handleDblclickTodoList);
+    }
+  },
+
+  saveTodo(todos) {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  },
+
+  addTodo(event) {
+    App.todos.push({
+      value: event.target.value,
+      completed: false,
+      id: Date.now(),
+      editing: ''
+    });
+    App.saveTodo(App.todos);
+    App.render();
+    event.target.value = '';
+  },
+
+  handleKeyup(event) {
+    if (
+      event.target.id !== 'new-todo-title' ||
+      event.key !== ENTER ||
+      event.target.value === ''
+    )
+      return;
+    App.addTodo(event);
+  },
+
+  handleToggleClick(target) {
+    const li = target.closest('li');
+    const todosArray = JSON.parse(localStorage.getItem('todos'));
+
+    target.getAttribute('checked') === null
+      ? target.setAttribute('checked', '')
+      : target.removeAttribute('checked');
+    li.classList.toggle('completed');
+    todosArray.forEach(todo => {
+      if (todo.id === Number(li.id)) {
+        todo.completed = !todo.completed;
+      }
+    });
+    localStorage.setItem('todos', JSON.stringify(todosArray));
+    App.todos = todosArray;
+    App.render();
+  },
+
+  handleDestroyButtonClick(target) {
+    const li = target.closest('li');
+    const deleteResult = App.todos.filter(todo => {
+      return todo.id !== parseInt(li.id, 10);
+    });
+    App.todos = deleteResult;
+    localStorage.setItem('todos', JSON.stringify(App.todos));
+    li.remove();
+    App.render();
+  },
+
+  handleClickTodoList(event) {
+    const { target } = event;
+
+    if (target.className === 'toggle') {
+      App.handleToggleClick(target);
+    }
+    if (target.className === 'destroy') {
+      App.handleDestroyButtonClick(target);
+    }
+  },
+
+  handleClickFilters(event) {
+    const condition = filters.querySelector('.selected');
+
+    if (condition) {
+      condition.classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+    App.render();
+  },
+
+  init() {
+    App.todos =
+      localStorage.getItem('todos') === null
+        ? []
+        : JSON.parse(localStorage.getItem('todos'));
+    App.render();
+
+    newTodoTitle.addEventListener('keyup', App.handleKeyup);
+    todoList.addEventListener('click', App.handleClickTodoList);
+    filters.addEventListener('click', App.handleClickFilters);
+    const listItems = todoList.querySelectorAll('li');
+    for (const listItem of listItems) {
+      listItem.addEventListener('dblclick', App.handleDblclickTodoList);
+    }
+  }
+};
+
+App.init();
---
+++ constant.js
@@ -0,0 +1,7 @@
+const count = document.querySelector('strong');
+const filters = document.querySelector('.filters');
+const todoList = document.querySelector('#todo-list');
+const newTodoTitle = document.querySelector('#new-todo-title');
+const ENTER = 'Enter';
+const ESC = 'Escape';
+export { count, filters, todoList, newTodoTitle, ENTER, ESC };
