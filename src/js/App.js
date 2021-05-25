11e127c6d3357ef26e36bcbc5a1326dedfa037bc
jspark940321 <jspark940321@gmail.com>
2021-05-25 23:16:54 +0900

    feat: +, - 구현
    +, - 및 테스트

--- src/js/App.js
+++ src/js/App.js
@@ -1,1 +1,51 @@
-alert('Hello');
+// 부모 컴포넌트
+function TodoApp() {
+  this.todoItems = [];
+
+  this.setState = updatedItems => {
+    this.todoItems = updatedItems;
+    todoList.setState(this.todoItems);
+  };
+
+  new TodoInput({
+    onAdd: contents => {
+      const newTodoItem = new TodoItem(contents);
+      this.todoItems.push(newTodoItem);
+      this.setState(this.todoItems);
+    }
+  });
+}
+TodoApp();
+alert();
+// 입력 받는 컴포넌트
+function TodoInput({ onAdd }) {
+  const $todoInput = document.querySelector("#new-todo-title");
+
+  $todoInput.addEventListener("keydown", event => this.addTodoItem(event));
+
+  this.addTodoItem = event => {
+    const $newTodoTarget = event.target;
+    if (this.isValid(event, $newTodoTarget.value)) {
+      onAdd($newTodoTarget.value);
+      $newTodoTarget.value = "";
+    }
+  };
+
+  this.isValid = (event, value) => {
+	  if(value == "") return false;
+	  return true;
+  }
+}
+
+// todoList 보여주는 컴포넌트
+function TodoList() {
+  this.setState = updatedTodoItems => {
+	this.todoItems = updatedTodoItems;
+    this.render(this.todoItems);
+  };
+
+  this.render = items => {
+    const template = items.map(todoItemTemplate);
+    this.$todoList.innerHTML = template.join("");
+  };
+}
