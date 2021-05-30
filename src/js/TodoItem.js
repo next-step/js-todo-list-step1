48a8e1d412a053deb69c43121d9811179b77f544
jspark940321 <jspark940321@gmail.com>
2021-05-30 21:58:23 +0900

    feat: LocalStorage 저장기능 구현

--- src/js/TodoItem.js
+++ src/js/TodoItem.js
@@ -2,6 +2,11 @@ export let todoList = [];
 export let listStatus = "all";
 export let is_complete = true;
 
+export function TodoItem() {
+  todoList = JSON.parse(localStorage.getItem("todoList"));
+  if (todoList == undefined) todoList = [];
+}
+
 export const addList = (value) => {
   const id = todoList.length == 0 ? 0 : todoList[todoList.length - 1].id + 1;
   todoList.push({
@@ -9,27 +14,35 @@ export const addList = (value) => {
     title: value,
     complete: false,
   });
+  saveLocalStroge();
 };
 export const changeComplete = (id) => {
   todoList.forEach((l) => {
     if (l.id == id) {
       l.complete = !l.complete;
+      saveLocalStroge();
       return;
     }
   });
 };
 export const deleteItem = (id) => {
   todoList = todoList.filter((td) => td.id != id);
+  saveLocalStroge();
 };
 
 export const modifyItem = (id, title) => {
   todoList.map((td) => {
     if (td.id == id) td.title = title;
   });
+  saveLocalStroge();
 };
 
 export const setStatus = (status) => {
   listStatus = status;
   if (listStatus == "active") is_complete = false;
   if (listStatus == "completed") is_complete = true;
+};
+
+export const saveLocalStroge = () => {
+  localStorage.setItem("todoList", JSON.stringify(todoList));
 };
