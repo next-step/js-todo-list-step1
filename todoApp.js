381961ec3a146e2fb881fcb2de232b2602a50b77
hysimok <jeonghy95@gmail.com>
2021-06-01 11:42:14 +0900

    set index number to todoItems

--- todoApp.js
+++ todoApp.js
@@ -7,8 +7,9 @@ import { generateTodos } from "./src/utils/util.js";
 export default function TodoApp() {
 	console.log('reload');
 
-	// LocalStorage에 할 일 목록이 있다면 가져오기
 	this.todoItems = generateTodos(JSON.parse(localStorage.getItem("items")));
+	this.todoId = JSON.parse(localStorage.getItem("id")) ?? 0;
+	console.log(this.todoId);
 
 	// todoList 컴포넌트 생성
 	this.todoList = new TodoList(this);
@@ -27,7 +28,7 @@ export default function TodoApp() {
 	// 학습 자료에 있는 코드는 add 함수를 TodoInput 컴포넌트에 넘겨줘서 TodoInput 인스턴스 안에서 실행하는데
 	// TodoInput 인스턴스 내에서 더 많은 함수를 실행해야할 수 도 있는 상황을 대비해 (확장성 고려) this 를 넘겨준다. 
 	this.add = contents => {
-		const newTodoItem = new TodoItem(0, contents);
+		const newTodoItem = new TodoItem(this.todoId++, contents);
 		this.todoItems.push(newTodoItem);
 		this.setState();
 	}
