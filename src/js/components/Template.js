c55a9340be21db72ead751cea7410ead9340428a
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-06-03 12:26:04 +0900

    # refactor : 중복 제거 & 가독성 증가 작업 완료

--- src/js/components/Template.js
+++ src/js/components/Template.js
@@ -1,23 +1,14 @@
 export const TodoItemTemplate = (todoItem) => {
-  if (todoItem.isCompleted()) {
-    return `<li id="${todoItem.id}" class="${todoItem.status}">
+  return `<li id="${todoItem.id}" class="${todoItem.status}">
         <div class="view">
-          <input class="toggle" type="checkbox" checked>
-          <label class="label">${todoItem.content}</label>
-          <button class="destroy"></button>
-        </div>
-        <input class="edit" value="completed now ${todoItem.content}">
-      </li>`;
-  } else {
-    return `<li id="${todoItem.id}" class="${todoItem.status}">
-        <div class="view">
-          <input class="toggle" type="checkbox">
+         <input class="toggle" type="checkbox" ${
+           todoItem.isCompleted() ? "checked" : ""
+         }>
           <label class="label">${todoItem.content}</label>
           <button class="destroy"></button>
         </div>
         <input class="edit" value="${todoItem.content}">
       </li>`;
-  }
 };
 
 export const TodoCountTemplate = (todoItems) => {
--- src/js/components/TodoInput.js
+++ src/js/components/TodoInput.js
@@ -2,16 +2,18 @@ import { $ } from "../utils/querySelector.js";
 import { EVENT_TYPE, KEY_TYPE } from "../utils/Constants.js";
 
 export function TodoInput({ onAdd }) {
-  const $todoInput = document.querySelector("#new-todo-title");
+  const $todoInput = $("#new-todo-title");
 
   $todoInput.addEventListener("keyup", (event) => this.addTodoItem(event));
 
   this.addTodoItem = (event) => {
-    let current_input = $todoInput.value;
-    if (this.isValid(event, current_input)) {
-      event.preventDefault();
-      onAdd(current_input);
-      $todoInput.value = "";
+    if (event.key === "Enter") {
+      let current_input = $todoInput.value;
+      if (this.isValid(event, current_input)) {
+        event.preventDefault();
+        onAdd(current_input);
+        $todoInput.value = "";
+      }
     }
   };
 
