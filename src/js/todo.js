f99113a549c0e0a20357d7483641beeb053b1ca8
unknown <melon9751@naver.com>
2021-06-04 20:41:56 +0900

    변수명, 목록 관리를 배열로 개선하라

--- src/js/todo.js
+++ src/js/todo.js
@@ -7,7 +7,9 @@ const $filtersToDo = document.querySelector(".active");
 const $filtersCompleted = document.querySelector(".completed");
 
 let uniqueNumber = 0;
-const TODO_LS = "todos";
+let toDosAll = [];
+let toDosActive = [];
+let toDosCompleted = [];
 
 function handleEnter(event, inputValue) {
     if(event.key === 'Enter') {
@@ -19,70 +21,81 @@ function handleEnter(event, inputValue) {
 function addToDo(text) {
     const idValue = ++uniqueNumber;
 
-    const li = document.createElement("li");
-    li.id = idValue;
-    li.addEventListener("dblclick", editToDo);
+    const $li = document.createElement("li");
+    $li.id = idValue;
+    $li.addEventListener("dblclick", editToDo);
     
-    const div = document.createElement("div");
-    div.classList.add("view");
+    const $div = document.createElement("div");
+    $div.classList.add("view");
     
-    const inputToggle = document.createElement("input");
-    inputToggle.classList.add("toggle");
-    inputToggle.type = "checkbox";
-    inputToggle.id = idValue;
-    inputToggle.addEventListener("click", checkToDo);
+    const $inputToggle = document.createElement("input");
+    $inputToggle.classList.add("toggle");
+    $inputToggle.type = "checkbox";
+    $inputToggle.id = idValue;
+    $inputToggle.addEventListener("click", checkToDo);
 
-    const label = document.createElement("label");
-    label.classList.add("label");
-    label.innerHTML = text;
+    const $label = document.createElement("label");
+    $label.classList.add("label");
+    $label.innerHTML = text;
 
-    const delBtn = document.createElement("button");
-    delBtn.classList.add("destroy");
-    delBtn.id = idValue;
-    delBtn.addEventListener("click", deleteToDo);
+    const $delBtn = document.createElement("button");
+    $delBtn.classList.add("destroy");
+    $delBtn.id = idValue;
+    $delBtn.addEventListener("click", deleteToDo);
 
-    const inputEdit = document.createElement("input");
-    inputEdit.classList.add("edit");
-    inputEdit.value = text;
+    const $inputEdit = document.createElement("input");
+    $inputEdit.classList.add("edit");
+    $inputEdit.value = text;
 
-    li.appendChild(div);
-    div.appendChild(inputToggle);
-    div.appendChild(label);
-    div.appendChild(delBtn);
-    li.appendChild(inputEdit);
+    $li.appendChild($div);
+    $div.appendChild($inputToggle);
+    $div.appendChild($label);
+    $div.appendChild($delBtn);
+    $li.appendChild($inputEdit);
 
-    $toDoList.appendChild(li);
+    $toDoList.appendChild($li);
+    toDosAll.push($li);
+    toDosActive.push($li);
     showNumbers();
 }
 
 function checkToDo(event) {
-    const inputToggle = event.target;
-    const checkedState = inputToggle.checked;
+    const $inputToggle = event.target;
+    const checkedState = $inputToggle.checked;
     if(checkedState) {
-        inputToggle.checked = true;
+        $inputToggle.checked = true;
     } else {
-        inputToggle.checked = false;
+        $inputToggle.checked = false;
     }
 
-    const li = inputToggle.parentNode.parentNode;
-    const completedState = li.classList;
-    if(completedState.contains("completed")) {
-        li.classList.remove("completed");
-        li.classList.add("false");
-    } else {
-        li.classList.remove("false");
-        li.classList.add("completed");
+    const $li = $inputToggle.parentNode.parentNode;
+    const completedState = $li.classList;
+    if(!completedState.contains("completed")) { //할 일에서 완료로 변경
+        toDosCompleted.push($li);
+        toDosActive = toDosActive.filter(todo => todo.id != $li.id);
+        $li.classList.remove("false");
+        $li.classList.add("completed");
+    } else { //완료에서 할 일로 변경
+        toDosCompleted = toDosCompleted.filter(todo => todo.id != $li.id);
+        toDosActive.push($li);
+        $li.classList.remove("completed");
+        $li.classList.add("false");
     }
 }
 
 function deleteToDo(event) {
-    const deleteBtn = event.target;
-    const idValue = deleteBtn.id;
-    const ul = document.querySelector("#todo-list");
-    const liSize = ul.childNodes.length;
+    const $deleteBtn = event.target;
+    const idValue = $deleteBtn.id;
+    const $ul = document.querySelector("#todo-list");
+    const $li = $ul.childNodes;
+    const liSize = $ul.childNodes.length;
+    
     for(let i = 0; i < liSize; i++) {
-        if(ul.childNodes[i].id == idValue) {
-            ul.removeChild(ul.childNodes[i]);
+        if($li[i].id == idValue) {
+            $ul.removeChild($li[i]);
+            toDosCompleted = toDosCompleted.filter(todo => todo.id != idValue);
+            toDosActive = toDosActive.filter(todo => todo.id != idValue);
+            toDosAll = toDosAll.filter(todo => todo.id != idValue);
             break;
         }
     }
@@ -90,89 +103,77 @@ function deleteToDo(event) {
 }
 
 function editToDo(event) {
-    const label = event.target;
-    const li = label.parentNode.parentNode;
-    li.classList.remove("false");
-    li.classList.add("editing");
-    li.addEventListener("keydown", finishEditToDo);
+    const $label = event.target;
+    const $li = $label.parentNode.parentNode;
+    $li.classList.remove("false");
+    $li.classList.add("editing");
+    $li.addEventListener("keydown", finishEditToDo);
 }
+
 
 function finishEditToDo(event) { 
     if(event.key === 'Enter') {
-        const inputEdit = event.target;
-        const editText = inputEdit.value;
+        const $inputEdit = event.target;
+        const editText = $inputEdit.value;
 
-        const label = inputEdit.previousSibling.childNodes[1];
-        label.innerHTML = editText;
-        const li = inputEdit.parentNode;
-        li.classList.remove("editing");
-        li.classList.add("false");
+        const $label = $inputEdit.previousSibling.childNodes[1];
+        $label.innerHTML = editText;
+        const $li = $inputEdit.parentNode;
+        $li.classList.remove("editing");
+        $li.classList.add("false");
     }
 
     if(event.key === 'Escape') {
-        const inputEdit = event.target;
-        const li = inputEdit.parentNode;
-        li.classList.remove("editing");
-        li.classList.add("false");
+        const $inputEdit = event.target;
+        const $li = $inputEdit.parentNode;
+        $li.classList.remove("editing");
+        $li.classList.add("false");
     }
 }
 
 function changeFilter(event) {
-    const target = event.target;
-    const liList = $filters.getElementsByTagName("li");
-    for(let i=0 ; i<liList.length; i++) {
-        if(liList[i].firstElementChild.classList.contains("selected")) {
-            liList[i].firstElementChild.classList.remove("selected");
+    const $target = event.target;
+    const $liList = $filters.getElementsByTagName("li");
+    for(let i=0 ; i<$liList.length; i++) {
+        if($liList[i].firstElementChild.classList.contains("selected")) {
+            $liList[i].firstElementChild.classList.remove("selected");
             break;
         }
     }
-    target.classList.add("selected");
+    $target.classList.add("selected");
 
-    const targetClassList = target.classList;
-    const liToDoList = $toDoList.getElementsByTagName("li");
+    const targetClassList = $target.classList;
+    const $liToDoList = $toDoList.getElementsByTagName("li");
+
+    const liToDoListLength = $liToDoList.length;
+    for(let i=0; i<liToDoListLength; i++) {
+        $toDoList.removeChild($toDoList.firstChild);
+    }
+
     if(targetClassList.contains("all")) {
-        for(let i=0; i<liToDoList.length; i++) {
-            liToDoList[i].style.display = "block";   
-        }
+        toDosAll.forEach(todo => $toDoList.appendChild(todo));
     }
     
     if(targetClassList.contains("active")) {
-        for(let i=0; i<liToDoList.length; i++) {
-            const liClassListName = liToDoList[i].classList;
-            if(!liClassListName.contains("completed")) {
-                liToDoList[i].style.display = "block";
-            } else {
-                liToDoList[i].style.display = "none";
-            }
-        }
+        toDosActive.forEach(todo => $toDoList.appendChild(todo));
     }
     
     if(targetClassList.contains("completed")) {
-        for(let i=0; i<liToDoList.length; i++) {
-            const liClassListName = liToDoList[i].classList;
-            if(liClassListName.contains("completed")) {
-                liToDoList[i].style.display = "block";
-            } else {
-                liToDoList[i].style.display = "none";
-            }
-        }
+        toDosCompleted.forEach(todo => $toDoList.appendChild(todo));
     }
     showNumbers();
 }
 
 function showNumbers() {
-    let numbers = 0;
-    const liList = $toDoList.childNodes;
-    for(let i=0; i<liList.length; i++) {
-        if(liList[i].style.display === 'block' || liList[i].style.display === '') {
-            numbers++;
-        }
-    }
     const strong = $toDoCount.firstChild.nextSibling;
-    strong.innerHTML = numbers;
+    strong.innerHTML = $toDoList.childNodes.length;
 }
 
-$inputToDo.addEventListener("keydown", event => handleEnter(event, $inputToDo.value));
-$filtersAll.addEventListener("click", changeFilter);
-$filtersToDo.addEventListener("click", changeFilter);
-$filtersCompleted.addEventListener("click", changeFilter);
+function init() {
+    $inputToDo.addEventListener("keydown", event => handleEnter(event, $inputToDo.value));
+    $filtersAll.addEventListener("click", changeFilter);
+    $filtersToDo.addEventListener("click", changeFilter);
+    $filtersCompleted.addEventListener("click", changeFilter);
+}
+
+init(); 
