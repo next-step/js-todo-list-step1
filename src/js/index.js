04df5e3dd82df6303f0ca1340b4204e902b9ebe0
BoMee-Yoon <primavera0331@naver.com>
2021-06-05 13:47:39 +0900

    Delete all files

--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-import App from './app.js';
-
-const app = App();
-
-document.addEventListener('DOMContentLoaded', () => app.init());
-document.addEventListener('beforeunload', () => app.destroy());
--- src/js/utils/helper.js
+++
@@ -1,9 +0,0 @@
-export const stopEditing = (target) => {
-  const $item = getTodoItemElement(target);
-  removeClassName($item, 'editing');
-}
-
-export const startEditing = (target) => {
-  const $item = getTodoItemElement(target);
-  addClassName($item, 'editing')
-}
