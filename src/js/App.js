c4ed76d4ffa053eb839465950e830a1bdeea765f
BoMee-Yoon <primavera0331@naver.com>
2021-06-05 13:59:40 +0900

    Add TodoApp

---
+++ src/js/App.js
@@ -0,0 +1,54 @@
+import { TodoInput } from "./components/TodoInput";
+
+class NewTodoItem {
+  constructor(text) {
+    return {
+      completed: false,
+      text,
+    }
+  } 
+}
+
+class App {
+  constructor(TodoList, TodoCount) {
+    this.TodoList = TodoList;
+    this.TodoCount = TodoCount;
+
+    this.items = [];
+
+    this.init();
+  }
+
+  init() {
+    this.setState([]);
+    this.setEvent();
+  }
+
+  get count() {
+    return this.items.length
+  }
+
+  setState(updatedItems) {
+    this.items = updatedItems.map((item, id) => ({ ...item, id }));
+    // this.TodoList.setState(this.items);
+    // this.TodoCount.setState(this.count);
+  }
+
+  setEvent() {
+    this.addTodo();
+  }
+
+  addTodo() {
+    new TodoInput({
+      add: text => {
+        const newItem = new NewTodoItem(text);
+        this.items.push(newItem);
+        this.setState(this.items);
+      }
+    })
+  }
+}
+
+export default function() {
+  return new App();
+}
---
+++ src/js/components/TodoInput.js
@@ -0,0 +1,5 @@
+export class TodoInput {
+  constructor(add) {
+    this._add = add;
+  }
+}
