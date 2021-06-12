745f1fcefdf11ce8ef8be7eb6e89c97a4dcbc3d4
thdwlsgus0 <thdwlsgus0@nts-corp.com>
2021-06-12 17:03:09 +0900

    build - style 설정을 위한 style-loader 추가

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import render from './components/render.js';
 import './utils/addEvent.js';
 import { getTodoItem, getItemFilter } from './store/store.js';
+import css from './css/style.css';
 // 초기 렌더링
 render({ todoItem: getTodoItem(), option: getItemFilter() });
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,14 @@ const path = require('path');
 module.exports = {
   entry: './src/app.js',
   output: { filename: 'bundle.js', path: path.resolve(__dirname, 'dist') },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   devServer: {
     contentBase: __dirname + '/dist/',
     host: 'localhost',
