1f04215780b1341c3aa8b540598cbe397f0bd664
janghu <janghu@event-us.kr>
2021-07-07 15:19:33 +0900

    feat: todo data 형식 변경

--- src/components/NewTodoInput.js
+++ src/components/NewTodoInput.js
@@ -1,8 +1,6 @@
 import Component from '../core/component.js';
 
 export default class NewTodoInput extends Component {
-  render() {}
-
   bindEvents() {
     this.$target.addEventListener('keyup', ({ key }) => {
       if (key !== 'Enter') return;
@@ -14,7 +12,11 @@ export default class NewTodoInput extends Component {
 
   addTodo(todo) {
     const todoList = this.props.todoList.get();
-    todoList.push(todo);
+    todoList.push({
+      id: Date.now(),
+      todo,
+      checked: false,
+    });
     this.props.todoList.set(todoList);
   }
 }
