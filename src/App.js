d69a1504f2ab60db40798e9d014cec5451b4dd67
kimjanghu <kjh921030@gmail.com>
2021-07-09 23:50:30 +0900

    feat: todo 수정 및 총 갯수 표시 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import NewTodoInput from './components/NewTodoInput.js';
+import TodoCount from './components/TodoCount.js';
 import TodoList from './components/TodoList.js';
 import Component from './core/component.js';
 import State from './core/State.js';
@@ -18,14 +19,20 @@ export default class App extends Component {
       todoList: this.todoList,
       onSubmitTodo: this.mountTodoList.bind(this),
     });
+
+    this.todoCountView = new TodoCount($('.count-container'), {
+      todoList: this.todoList,
+    });
   }
 
   mountTodoList() {
-    new TodoList($('#todo-list'), {
+    this.todoListView = new TodoList($('#todo-list'), {
       todoList: this.todoList,
       checkTodo: this.checkTodo.bind(this),
       deleteTodo: this.deleteTodo.bind(this),
+      editTodo: this.editTodo.bind(this),
     });
+    this.todoCountView.render();
   }
 
   checkTodo(id) {
@@ -42,6 +49,17 @@ export default class App extends Component {
   deleteTodo(id) {
     const todoList = this.todoList.get().filter((todo) => todo.id !== id);
     this.todoList.set([...todoList]);
-    console.log(this.todoList);
+    this.todoCountView.render();
+  }
+
+  editTodo(id, value) {
+    this.todoList.get().map((todo) => {
+      if (todo.id === id) {
+        todo.todo = value;
+        return todo;
+      }
+      return todo;
+    });
+    this.todoListView.render();
   }
 }
