12a311c1b835cc32ee764352413fb257fb5dec46
kimjanghu <kjh921030@gmail.com>
2021-07-09 23:50:04 +0900

    feat: todo 수정

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -12,13 +12,13 @@ export default class TodoList extends Component {
 
   renderTodo({ id, todo }) {
     return `
-      <li class="todo" id=${id}>
+      <li class="todo" data-id=${id}>
         <div class="view">
-          <input class="toggle" type="checkbox" id=${id} />
+          <input class="toggle" type="checkbox" data-id=${id} />
           <label class="label">${todo}</label>
-          <button class="destroy" id=${id}></button>
+          <button class="destroy" data-id=${id}></button>
         </div>
-        <input class="edit" value="${todo}" />
+        <input class="edit" value="${todo}" data-id=${id} />
       </li>
     `;
   }
@@ -27,16 +27,41 @@ export default class TodoList extends Component {
     $$('.todo').forEach((item) => {
       item.addEventListener('click', ({ target }) => {
         const classList = target.classList;
-        const checkId = Number(target.id);
+        const targetId = Number(target.dataset.id);
         if (classList.contains(TODO_BUTTONS.TOGGLE)) {
           target.closest('.todo').classList.toggle('completed');
-          this.props.checkTodo(checkId);
+          this.props.checkTodo(targetId);
         }
 
         if (classList.contains(TODO_BUTTONS.DESTROY)) {
           const removeTarget = target.closest('.todo');
           this.$target.removeChild(removeTarget);
-          this.props.deleteTodo(checkId);
+          this.props.deleteTodo(targetId);
+        }
+      });
+
+      item.addEventListener('dblclick', ({ target }) => {
+        const classList = target.classList;
+        if (classList.contains(TODO_BUTTONS.LABEL)) {
+          target.closest('.todo').classList.add('editing');
+          target.closest('.todo').querySelector('.edit').focus();
+        }
+      });
+
+      item.addEventListener('keyup', ({ target, key }) => {
+        const targetId = Number(target.dataset.id);
+        const editTarget = target.closest('.todo');
+        if (key === 'Enter') {
+          editTarget.classList.add('view');
+          editTarget.classList.remove('editing');
+          this.props.editTodo(targetId, target.value);
+          return;
+        }
+
+        if (key === 'Escape') {
+          editTarget.classList.add('view');
+          editTarget.classList.remove('editing');
+          return;
         }
       });
     });
