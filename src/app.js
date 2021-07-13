000c60119825d33576c4d03759eb28d5153e42da
JiEun Lee <jieunlee.me@gmail.com>
2021-07-13 12:04:05 +0900

    refactor: LocalStorage 클래스로 분리

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import TodoApp from './components/TodoApp.js';
+import LocalStorage from './components/LocalStorage.js';
 
+const LS_KEY = 'TODOS';
 window.addEventListener('DOMContentLoaded', () => {
-  const todoApp = new TodoApp();
-  todoApp.init();
+  const storage = new LocalStorage(LS_KEY);
+  const savedData = storage.getItems();
+
+  const todoApp = new TodoApp(storage);
+  todoApp.init(savedData);
 });
